2f0ddbcd38fee7958d068863ad09aca1cb146d45
Kevin van Zonneveld <kevin@vanzonneveld.net>
2015-11-24 15:17:22 +0100

    Structure is directly under core now

--- src/js/plugins/TransloaditBasic.js
+++
@@ -1,9 +0,0 @@
-class TransloaditBasic extends TransloaditPlugin {
-  constructor(core, opts) {
-    super(core, opts);
-    this.type = 'presetter';
-    this.core
-      .use(DragDrop, {modal: true, wait: true})
-      .use(Tus10, {endpoint: 'http://master.tus.io:8080'})
-  }
-}
