fd57e9db0a065c353d121730014521d0ea1b4c21
Artur Paikin <artur@arturpaikin.com>
2015-11-24 15:39:56 +0300

    Updated core/plugin structure

---
+++ src/js/plugins/TransloaditBasic.js
@@ -0,0 +1,9 @@
+class TransloaditBasic extends TransloaditPlugin {
+  constructor(core, opts) {
+    super(core, opts);
+    this.type = 'presetter';
+    this.core
+      .use(DragDrop, {modal: true, wait: true})
+      .use(Tus10, {endpoint: 'http://master.tus.io:8080'})
+  }
+}
