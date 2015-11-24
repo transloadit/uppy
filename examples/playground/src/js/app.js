7ba8df8411a4416646e4985cef90be4c05d9b48d
Artur Paikin <artur@arturpaikin.com>
2015-11-24 15:43:22 +0300

    Attempting to build a playground example with Browserify & Babel

---
+++ examples/playground/src/js/app.js
@@ -0,0 +1,12 @@
+import Transloadit from '../../../src/js/core/Transloadit';
+import DragDrop from '../../../src/js/plugins/DragDrop';
+import Tus10 from '../../../src/js/plugins/Tus10';
+
+const transloadit = new Transloadit({wait: false});
+const files = transloadit
+  .use(DragDrop, {modal: true})
+  .use(Tus10, {endpoint: 'http://master.tus.io:8080'})
+  .run();
+
+console.log('--> Finished transloadit. Final result: ');
+console.dir(files);
