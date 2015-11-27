c95a94cae3a8eb1b42d5b8a6c33abd934fe789db
Artur Paikin <artur@arturpaikin.com>
2015-11-27 16:23:31 +0300

    Resolved a conflict

--- src/plugins/DragDrop.js
+++ src/plugins/DragDrop.js
@@ -1,28 +1,6 @@
-<<<<<<< HEAD
 import { toggleClass } from '../core/Utils';
 import { TransloaditPlugin } from './';
-=======
-import TransloaditPlugin from './TransloaditPlugin';
-console.log('pizza', TransloaditPlugin)
-// This is how we roll $('.element').toggleClass in non-jQuery world
-function toggleClass(el, className) {
-  // console.log(el);
-
-  if (el.classList) {
-    el.classList.toggle(className);
-  } else {
-    var classes = el.className.split(' ');
-    var existingIndex = classes.indexOf(className);
-
-    if (existingIndex >= 0) {
-      classes.splice(existingIndex, 1);
-    } else {
-      classes.push(className);
-      el.className = classes.join(' ');
-    }
-  }
-}
->>>>>>> master
+console.log('pizza', TransloaditPlugin);
 
 export default class DragDrop extends TransloaditPlugin {
   constructor(core, opts) {
--- src/plugins/index.js
+++ src/plugins/index.js
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
 import DragDrop from './DragDrop';
 import TransloaditBasic from './TransloaditBasic';
 import Tus10 from './Tus10';
 import TransloaditPlugin from './TransloaditPlugin';
-=======
-import TransloaditPlugin from './TransloaditPlugin'
-import DragDrop from './DragDrop'
-import TransloaditBasic from './TransloaditBasic'
-import Tus10 from './Tus10'
->>>>>>> master
 
 export default {
   TransloaditPlugin,
