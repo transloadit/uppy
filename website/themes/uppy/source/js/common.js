68774ef15f3ac5ab76b41696e90cd31e27c64d3e
Kevin van Zonneveld <kevin@vanzonneveld.net>
2015-12-03 15:32:05 +0100

    Not using version-select atm

--- website/themes/uppy/source/js/common.js
+++ website/themes/uppy/source/js/common.js
@@ -167,18 +167,19 @@
   e=d.getElementsByTagName(t)[0];s.async=1;s.src=u;e.parentNode.insertBefore(s,e);
   })(window,document,'script','//s.swiftypecdn.com/install/v2/st.js','_st');
 
-  _st('install','HgpxvBc7pUaPUWmG9sgv','2.0.0');
+  // @todo get our own swifttype
+  // _st('install','HgpxvBc7pUaPUWmG9sgv','2.0.0');
 
   // version select
-  document.querySelector('.version-select').addEventListener('change', function (e) {
-    var version = e.target.value
-    if (version.indexOf('1.') !== 0) {
-      version = version.replace('.', '')
-      var section = window.location.pathname.match(/\/(\w+?)\//)[1]
-      window.location.assign('http://' + version + '.uppyjs.io/' + section + '/')
-    } else {
-      // TODO when 1.x is out
-    }
-  })
+  // document.querySelector('.version-select').addEventListener('change', function (e) {
+  //   var version = e.target.value
+  //   if (version.indexOf('1.') !== 0) {
+  //     version = version.replace('.', '')
+  //     var section = window.location.pathname.match(/\/(\w+?)\//)[1]
+  //     window.location.assign('http://' + version + '.uppyjs.io/' + section + '/')
+  //   } else {
+  //     // TODO when 1.x is out
+  //   }
+  // })
 
 })()
