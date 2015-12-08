57c31299a967ebb57e7ddaf278afaeb7bd8aba79
Artur Paikin <artur@arturpaikin.com>
2015-12-08 12:57:15 +0300

    Moved DragDrop to examples folder

---
+++ website/src/examples/dragdrop/src/js/app.js
@@ -0,0 +1,8 @@
+import Uppy from '../../../../src/core';
+import { DragDrop, Tus10 } from '../../../../src/plugins';
+
+const uppy = new Uppy({wait: false});
+const files = uppy
+  .use(DragDrop, {selector: '#upload-target'})
+  .use(Tus10, {endpoint: 'http://master.tus.io:8080'})
+  .run();
