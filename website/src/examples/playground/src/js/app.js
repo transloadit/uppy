924eef06ad07ea6e3bcbcc0071ca812942e117b7
Kevin van Zonneveld <kevin@vanzonneveld.net>
2015-12-11 12:05:49 +0100

    Rename playground->cdn example and make it work

--- website/src/examples/playground/src/js/app.js
+++ website/src/examples/playground/src/js/app.js
@@ -1,8 +1,1 @@
-// import Uppy from 'uppy/core';
-// import { DragDrop, Tus10 } from 'uppy/plugins';
-
-// const uppy = new Uppy({wait: false});
-// const files = uppy
-//   .use(DragDrop, {selector: '#upload-target'})
-//   .use(Tus10, {endpoint: 'http://master.tus.io:8080'})
-//   .run();
+// empty
