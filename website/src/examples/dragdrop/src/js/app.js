79b3693768dc82e8867b02da3d3c1c036215bedf
Artur Paikin <artur@arturpaikin.com>
2015-12-11 14:02:31 +0300

    Fixed example app.js

--- website/src/examples/dragdrop/src/js/app.js
+++ website/src/examples/dragdrop/src/js/app.js
@@ -1,13 +1,8 @@
-<<<<<<< HEAD
-import Uppy from '../../../../../src/core';
-import { DragDrop, Tus10 } from '../../../../../src/plugins';
-=======
-// // import Uppy from '../../../../../../src/core';
-// // import { DragDrop, Tus10 } from '../../../../../../src/plugins';
->>>>>>> origin/master
+import Uppy from 'uppy/core';
+import { DragDrop, Tus10 } from 'uppy/plugins';
 
-// const uppy = new Uppy({wait: false});
-// const files = uppy
-//   .use(DragDrop, {selector: '#upload-target'})
-//   .use(Tus10, {endpoint: 'http://master.tus.io:8080'})
-//   .run();
+const uppy = new Uppy({wait: false});
+const files = uppy
+  .use(DragDrop, {selector: '#upload-target'})
+  .use(Tus10, {endpoint: 'http://master.tus.io:8080'})
+  .run();
