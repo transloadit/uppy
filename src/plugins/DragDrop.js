027e0ca95f8d0cdf34ca290ba83248fa7a5deb09
Kevin van Zonneveld <kevin@vanzonneveld.net>
2015-12-21 11:58:40 +0100

    Break up checkDragDropSupport conditions for readability

--- src/plugins/DragDrop.js
+++ src/plugins/DragDrop.js
@@ -39,9 +39,20 @@ export default class DragDrop extends Plugin {
    */
   checkDragDropSupport() {
     const div = document.createElement('div');
-    return (('draggable' in div) ||
-           ('ondragstart' in div && 'ondrop' in div)) &&
-           'FormData' in window && 'FileReader' in window;
+
+    if (!('draggable' in div) || !('ondragstart' in div && 'ondrop' in div)) {
+      return false;
+    }
+
+    if (!('FormData' in window)) {
+      return false;
+    }
+
+    if (!('FileReader' in window)) {
+      return false;
+    }
+
+    return true;
   }
 
   listenForEvents() {
