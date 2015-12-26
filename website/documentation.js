72558280f2dd125f3297c0b76c1eda1b85d46249
Artur Paikin <artur@arturpaikin.com>
2015-12-26 00:37:39 -0500

    Improved docs generator

--- website/documentation.js
+++ website/documentation.js
@@ -1,26 +1,33 @@
 var documentation = require('documentation');
 var documentationFormatter = documentation.formats['md'];
+var remark = require('remark');
+var inject = require('mdast-util-inject');
+var chalk = require('chalk');
 var fs = require('fs');
 
-var docsFrontmatter = '---\ntype: api\norder: 1\ntitle: "Generated API Docs"\n---\n';
+documentation('../src/index.js', {}, function (err, comments) {
+  if (err) console.log(err);
 
-// documentation('../src/index.js', {}, function (err, comments) {
-//   documentationFormatter(comments, {}, function (err, output) {
-//     var docsWithFrontmatter = docsFrontmatter + output;
-//     // console.log(docsWithFrontmatter);
-//     fs.writeFileSync('src/api/docs.md', docsWithFrontmatter);
-//     console.log('documentation generated');
-//   });
-// });
+  documentationFormatter(comments, {}, function (err, output) {
+    if (err) console.log(err);
 
-var remark = require('remark');
-var inputMarkdownContent = fs.readFileSync('src/api/docs.md', 'utf-8');
-var docjsReadme = require('documentation-readme/lib/plugin');
-remark().use(docjsReadme, {
- section: 'Uppy Core & Plugins', // inject into the ## Usage section of the input doc
- documentationArgs: [ '../src/index.js' ]
-}).process(inputMarkdownContent, function (err, vfile, content) {
-fs.writeFileSync('src/api/docs.md', content);
- // console.log(content);
- console.log('documentation generated');
+    var inputMarkdownContent = remark.parse(fs.readFileSync('src/api/docs.md', 'utf-8'));
+    var newStuff = remark.parse(output);
+    inject('Uppy Core & Plugins', inputMarkdownContent, newStuff);
+
+    fs.writeFileSync('src/api/docs.md', remark.stringify(inputMarkdownContent));
+    console.info(chalk.green('✓ documentation generated'));
+  });
 });
+
+// var remark = require('remark');
+// var inputMarkdownContent = fs.readFileSync('src/api/docs.md', 'utf-8');
+// var docjsReadme = require('documentation-readme/lib/plugin');
+// remark().use(docjsReadme, {
+//  section: 'Uppy Core & Plugins', // inject into the ## Usage section of the input doc
+//  documentationArgs: [ '../src/index.js' ]
+// }).process(inputMarkdownContent, function (err, vfile, content) {
+// fs.writeFileSync('src/api/docs.md', content);
+//  // console.log(content);
+//  console.log('documentation generated');
+// });
