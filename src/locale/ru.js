86b81cc47c941d8dd67bedfff162790d274acd64
Artur Paikin <artur@arturpaikin.com>
2016-01-19 10:05:58 -0500

    ru and en locale additions

--- src/locale/ru.js
+++ src/locale/ru.js
@@ -3,17 +3,16 @@ const ru = {};
 ru.strings = {
   'choose_file': 'Выберите файл',
   'or_drag_drop': 'или перенесите его сюда',
+  'you_have_chosen': 'Вы выбрали: %{file_name}',
   // 'files_chosen': 'выбран %{smart_count} файл |||| выбрано %{smart_count} файла |||| выбрано %{smart_count} файлов'
   'files_chosen': {
-    0: 'выбран %{smart_count} файл',
-    1: 'выбрано %{smart_count} файла',
-    2: 'выбрано %{smart_count} файлов'
+    0: 'Выбран %{smart_count} файл',
+    1: 'Выбрано %{smart_count} файла',
+    2: 'Выбрано %{smart_count} файлов'
   }
 };
 
 ru.pluralize = function (n) {
-  // return n % 10 === 1 && n % 100 !== 11 ? 0 : n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 >= 20) ? 1 : 2;
-
   if (n % 10 === 1 && n % 100 !== 11) {
     return 0;
   }
