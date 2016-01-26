df945e4909a269afbb52208330559f69e44efc15
Harry Hedger <hedgerh@gmail.com>
2016-01-26 00:00:55 -0500

    Authorize template. Modal plugin layout.

---
+++ src/plugins/Modal.js
@@ -0,0 +1,26 @@
+export default class Modal {
+  constructor (core, opts) {
+
+  }
+
+  connect (target) {
+  }
+
+  authenticate () {
+  }
+
+  addFile () {
+
+  }
+
+  getDirectory () {
+  }
+
+  run (results) {
+
+  }
+
+  render (files) {
+
+  }
+}
