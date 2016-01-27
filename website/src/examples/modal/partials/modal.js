e292f81f4934311d6b6a214d9190335bd2f85c34
Harry Hedger <hedgerh@gmail.com>
2016-01-26 22:22:12 -0500

    Fixed formatting

--- website/src/examples/modal/partials/modal.js
+++ website/src/examples/modal/partials/modal.js
@@ -4,7 +4,14 @@ import Browser from './browser'
 export default () => {
   return `
     <section class='Modal'>
-      ${Sidebar({ providers: [{ name: 'Local' }, { name: 'Google Drive' }]})}
+      ${Sidebar({
+        providers: [{
+          name: 'Local'
+        },
+        {
+          name: 'Google Drive'
+        }]
+      })}
       ${Browser({
         files: [{ image: '123.png', name: '123.png' }],
         provider: 'Dropbox',
