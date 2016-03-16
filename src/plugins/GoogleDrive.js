235044d588aa829a29a8b30a5559c906746d28b9
Artur Paikin <artur@arturpaikin.com>
2016-03-16 17:42:42 -0400

    Fix GoogleDrive icon

--- src/plugins/GoogleDrive.js
+++ src/plugins/GoogleDrive.js
@@ -8,7 +8,7 @@ export default class Google extends Plugin {
     this.files = []
     this.name = 'Google Drive'
     this.icon = `
-      <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="#ffffff" fill-rule="evenodd" clip-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="1.414">
+      <svg class="UppyModalTab-icon" width="28" height="28" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
         <path d="M2.955 14.93l2.667-4.62H16l-2.667 4.62H2.955zm2.378-4.62l-2.666 4.62L0 10.31l5.19-8.99 2.666 4.62-2.523 4.37zm10.523-.25h-5.333l-5.19-8.99h5.334l5.19 8.99z"/>
       </svg>
     `
