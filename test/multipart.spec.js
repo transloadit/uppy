054cffac6e61f4e0cc992d7f160d02d79863991b
Harry Hedger <hedgerh@gmail.com>
2016-03-16 01:10:01 -0400

    Completed multipart test. Added debug:true to multipart example.  Added test files.

--- test/multipart.spec.js
+++ test/multipart.spec.js
@@ -1,18 +1,55 @@
 var test = require('tape')
 var webdriver = require('selenium-webdriver')
+var By = webdriver.By
 
 test('upload two files', function (t) {
   var driver = new webdriver.Builder()
-  .withCapabilities(webdriver.Capabilities.chrome())
-  .build()
+    .withCapabilities(webdriver.Capabilities.chrome())
+    .build()
 
-  driver.get('http://www.uppy.io/examples/multipart/')
-  driver.findElement(webdriver.By.id('file1')).sendKeys('./dummyFile')
-  driver.findElement(webdriver.By.id('file2')).sendKeys('./dummyFile2')
-  driver.findElement(webdriver.By.id('myupload')).click()
-  // driver.wait
-  // check for success
+  driver.get('http://localhost:4000/examples/multipart/')
+  driver.findElement(By.id('myfile1')).sendKeys(__dirname + '/image.jpg')
+  driver.findElement(By.id('myfile2')).sendKeys(__dirname + '/dummy.css')
+  driver.findElement(By.id('myupload')).click()
+
+  // Our success/fail message will be logged in the console element
+  var consoleElement = driver.findElement(By.id('console-log'))
+
+  // Wait for our upload message to be logged
+  driver.wait(isUploadComplete.bind(this, consoleElement))
+
+  // Get the result of our upload and test it
+  getElementValue(consoleElement)
+    .then(function (value) {
+      var result = value.split('\n')[0]
+      t.equal(result, 'DEBUG LOG: Upload result -> success!')
+    })
+
   driver.quit()
 
   t.end()
+
+  /**
+   * Check if uploading is finished by looking for a result message
+   * in the example's console output element.
+   * @return {Boolean} If uploading is complete
+   */
+  function isUploadComplete (consoleElement) {
+    return getElementValue(consoleElement)
+      .then(function (value) {
+        return value.indexOf('DEBUG LOG: Upload result ->') !== -1
+      })
+  }
+
+  /**
+   * Get value attribute of element
+   * @param  {webdriver.WebElement} element Selenium element object
+   * @return {webdriver.promise} Promise resolving to element value
+   */
+  function getElementValue (element) {
+    return element.getAttribute('value')
+      .then(function (value) {
+        return value
+      })
+  }
 })
