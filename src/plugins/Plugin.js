fb3e9dbce83dd5f808a0223d4981f30bef154e5f
Artur Paikin <artur@arturpaikin.com>
2016-03-23 00:48:37 -0400

    generateFileID function — a unique ID for each acquired file

--- src/plugins/Plugin.js
+++ src/plugins/Plugin.js
@@ -37,6 +37,20 @@ export default class Plugin {
     }
   }
 
+  /**
+   * Takes a fileName and turns it into fileID, by converting to lowercase,
+   * removing extra characters and adding unix timestamp
+   *
+   * @param {String} fileName
+   *
+   */
+  generateFileID (fileName) {
+    let fileID = fileName.toLowerCase()
+    fileID = fileID.replace(/[^A-Z0-9]/ig, '')
+    fileID = fileID + Date.now()
+    return fileID
+  }
+
   extractFiles (results) {
     console.log({
       class: 'Plugin',
