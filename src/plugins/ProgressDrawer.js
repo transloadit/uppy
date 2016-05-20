5e3520836fdedf6bb679745b54b32952a6323132
Artur Paikin <artur@arturpaikin.com>
2016-05-20 18:09:41 -0400

    ProgressDrawer & DragDrop improvements

--- src/plugins/ProgressDrawer.js
+++ src/plugins/ProgressDrawer.js
@@ -33,25 +33,37 @@ export default class ProgressDrawer extends Plugin {
     return yo`<li id="${file.id}" class="UppyProgressDrawer-item"
                   title="${file.name}">
       <div class="UppyProgressDrawer-itemInfo">
-        ${file.type.general === 'bla'
-          ? yo`<img class="UppyProgressDrawer-itemIcon" alt="${file.name}" src="${file.preview}">`
-          : yo`<span class="UppyProgressDrawer-itemType">${file.type.specific}</span>`
-        }
+        <svg width="32" height="44" viewBox="0 0 21 29">
+          <path d="M2.473.31C1.44.31.59 1.21.59 2.307V26.31c0 1.097.85 2 1.883 2H18.71c1.03 0 1.88-.903 1.88-2V7.746a.525.525 0 0 0-.014-.108v-.015a.51.51 0 0 0-.014-.03v-.017a.51.51 0 0 0-.015-.03.482.482 0 0 0-.014-.016v-.015a.482.482 0 0 0-.015-.015.51.51 0 0 0-.014-.03.482.482 0 0 0-.014-.017.51.51 0 0 0-.015-.03.483.483 0 0 0-.03-.03L13.636.45a.47.47 0 0 0-.118-.093.448.448 0 0 0-.044-.015.448.448 0 0 0-.044-.016.448.448 0 0 0-.045-.015.44.44 0 0 0-.073 0H2.474zm0 .99h10.372v4.943c0 1.097.85 2 1.88 2h4.932V26.31c0 .56-.42 1.007-.948 1.007H2.472c-.527 0-.95-.446-.95-1.007V2.308c0-.56.423-1.008.95-1.008zm11.305.667l4.843 4.927.352.357h-4.246c-.527 0-.948-.446-.948-1.007V1.967z"
+                fill="#F6A623"
+                fill-rule="evenodd" />
+          <text font-family="ArialMT, Arial"
+                font-size="5"
+                font-weight="bold"
+                fill="#F6A623"
+                text-anchor="middle"
+                x="11"
+                y="22">
+            ${file.type.specific ? file.type.specific.toUpperCase() : '?'}
+          </text>
+          </svg>
       </div>
       <div class="UppyProgressDrawer-itemInner">
-        <span class="UppyProgressDrawer-itemProgress"
-              style="width: ${file.progress}%"></span>
         <h4 class="UppyProgressDrawer-itemName">
           ${file.uploadURL
             ? yo`<a href="${file.uploadURL}" target="_blank">${file.name}</a>`
-            : yo`<span>${file.name} (${file.progress}%)</span>`
+            : yo`<span>${file.name}</span>`
           }
+          <br>
+          <span>${file.progress > 0 && file.progress < 100 ? file.progress + '%' : ''}</span>
         </h4>
           ${isUploaded ? checkIcon : ''}
           ${isUploaded
             ? ''
             : yo`<button class="UppyProgressDrawer-itemRemove" onclick=${remove}>×</button>`
           }
+          <div class="UppyProgressDrawer-itemProgress"
+               style="width: ${file.progress}%"></div>
       </div>
     </li>`
   }
@@ -81,11 +93,6 @@ export default class ProgressDrawer extends Plugin {
     }
 
     return yo`<div class="UppyProgressDrawer ${isSomethingSelectedOrUploaded ? 'is-visible' : ''}">
-      <div class="UppyProgressDrawer-status">
-        ${isSomethingSelected ? this.core.i18n('filesChosen', {'smart_count': selectedFileCount}) : ''}
-        ${isSomethingSelected && isSomethingUploaded ? ', ' : ''}
-        ${isSomethingUploaded ? this.core.i18n('filesUploaded', {'smart_count': uploadedFileCount}) : ''}
-      </div>
       <ul class="UppyProgressDrawer-list">
         ${Object.keys(files).map((fileID) => {
           return this.drawerItem(files[fileID])
@@ -94,7 +101,8 @@ export default class ProgressDrawer extends Plugin {
       ${autoProceed
         ? ''
         : yo`<button class="UppyProgressDrawer-upload ${isSomethingSelected ? 'is-active' : ''}"
-                     type="button" onclick=${next}>
+                     type="button"
+                     onclick=${next}>
           ${isSomethingSelected
             ? this.core.i18n('uploadFiles', {'smart_count': selectedFileCount})
             : this.core.i18n('selectToUpload')
@@ -102,6 +110,13 @@ export default class ProgressDrawer extends Plugin {
         </button>`
       }
     </div>`
+
+    // TODO: add this info to the upload button?
+    //    <div class="UppyProgressDrawer-status">
+    //      ${isSomethingSelected ? this.core.i18n('filesChosen', {'smart_count': selectedFileCount}) : ''}
+    //      ${isSomethingSelected && isSomethingUploaded ? ', ' : ''}
+    //      ${isSomethingUploaded ? this.core.i18n('filesUploaded', {'smart_count': uploadedFileCount}) : ''}
+    //    </div>
   }
 
   install () {
