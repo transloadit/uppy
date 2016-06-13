5f80d4c5968a3a765e5d26b70dbc04e77f6233ce
Artur Paikin <artur@arturpaikin.com>
2016-06-12 23:58:08 -0400

    regular + min + gzipped versions of the bundle

--- bin/build-css.js
+++ bin/build-css.js
@@ -12,15 +12,48 @@ function handleErr (err) {
   console.error(chalk.red('✗ Error:'), chalk.red(err.message))
 }
 
-sass.render({file: './src/scss/uppy.scss'}, function (err, sassResult) {
-  if (err) handleErr(err)
-  postcss([ cssnano, autoprefixer ])
-    .process(sassResult.css)
-    .then(function (postCSSResult) {
-      postCSSResult.warnings().forEach(function (warn) {
-        console.warn(warn.toString())
-      })
-      fs.writeFileSync('./dist/uppy.min.css', postCSSResult.css)
-      console.info(chalk.green('✓ Built Uppy CSS:'), chalk.magenta('uppy.min.css'))
+function minifyCSS () {
+  return new Promise(function (resolve) {
+    fs.readFile('./dist/uppy.css', function (err, css) {
+      if (err) handleErr(err)
+      postcss([ cssnano ])
+        .process(css)
+        .then(function (postCSSResult) {
+          postCSSResult.warnings().forEach(function (warn) {
+            console.warn(warn.toString())
+          })
+          fs.writeFile('./dist/uppy.min.css', postCSSResult.css, function (err) {
+            if (err) handleErr(err)
+            console.info(chalk.green('✓ Minified Bundle CSS:'), chalk.magenta('uppy.min.css'))
+            resolve()
+          })
+        })
     })
-})
+  })
+}
+
+function compileCSS () {
+  return new Promise(function (resolve) {
+    sass.render({file: './src/scss/uppy.scss'}, function (err, sassResult) {
+      if (err) handleErr(err)
+      postcss([ autoprefixer ])
+        .process(sassResult.css)
+        .then(function (postCSSResult) {
+          postCSSResult.warnings().forEach(function (warn) {
+            console.warn(warn.toString())
+          })
+          fs.writeFile('./dist/uppy.css', postCSSResult.css, function (err) {
+            if (err) handleErr(err)
+            console.info(chalk.green('✓ Built Uppy CSS:'), chalk.magenta('uppy.css'))
+            resolve()
+          })
+        })
+    })
+  })
+}
+
+compileCSS()
+  .then(minifyCSS)
+  .then(function () {
+    console.info(chalk.yellow('✓ CSS Bundle 🎉'))
+  })
