c63fe7d65ae4c1871555540f707368d1605768f2
Artur Paikin <artur@arturpaikin.com>
2016-06-13 14:49:35 -0400

    Locales should actually be browserified

--- bin/build-js.js
+++ bin/build-js.js
@@ -5,7 +5,7 @@ var chalk = require('chalk')
 var mkdirp = require('mkdirp')
 var glob = require('glob')
 var browserify = require('browserify')
-var exec = require('child_process').exec
+// var exec = require('child_process').exec
 var exorcist = require('exorcist')
 
 var distPath = './dist'
@@ -51,48 +51,52 @@ function buildUppyBundle (minify) {
   })
 }
 
-function copyLocales () {
-  var copyCommand = 'cp -R ' + path.join(srcPath, 'locales/') + ' ' + path.join(distPath, 'locales/')
-  return new Promise(function (resolve, reject) {
-    exec(copyCommand, function (error, stdout, stderr) {
-      if (error) {
-        handleErr(error)
-        reject(error)
-        return
-      }
-      console.info(chalk.green('✓ Copied locales to dist'))
-      resolve()
-    })
-  })
-}
-
-// function buildUppyLocales () {
-//   mkdirp.sync('./dist/locales')
-//   glob('./src/locales/*.js', function (err, files) {
-//     if (err) console.log(err)
-//     files.forEach(function (file) {
-//       var fileName = path.basename(file, '.js')
-//       browserify(file, { debug: true })
-//         .plugin('minifyify', {
-//           map: fileName + '.min.js.map',
-//           output: './dist/locales/' + fileName + '.min.js.map'
-//         })
-//         // .transform(rollupify)
-//         .transform(babelify)
-//         .on('error', handleErr)
-//         .bundle()
-//         .pipe(fs.createWriteStream('./dist/locales/' + fileName + '.min.js', 'utf8'))
-//         .on('error', handleErr)
-//         .on('finish', function () {
-//           console.info(chalk.green('✓ Built Locale:'), chalk.magenta(fileName + '.min.js'))
-//         })
+// function copyLocales () {
+//   var copyCommand = 'cp -R ' + path.join(srcPath, 'locales/') + ' ' + path.join(distPath, 'locales/')
+//   return new Promise(function (resolve, reject) {
+//     exec(copyCommand, function (error, stdout, stderr) {
+//       if (error) {
+//         handleErr(error)
+//         reject(error)
+//         return
+//       }
+//       console.info(chalk.green('✓ Copied locales to dist'))
+//       resolve()
 //     })
 //   })
 // }
 
+function buildLocale (file) {
+  return new Promise(function (resolve, reject) {
+    var fileName = path.basename(file, '.js')
+    browserify(file)
+      .transform(babelify)
+      .on('error', handleErr)
+      .bundle()
+      .pipe(fs.createWriteStream('./dist/locales/' + fileName + '.js', 'utf8'))
+      .on('error', handleErr)
+      .on('finish', function () {
+        console.info(chalk.green('✓ Built Locale:'), chalk.magenta(fileName + '.js'))
+        resolve()
+      })
+  })
+}
+
+function buildUppyLocales () {
+  mkdirp.sync('./dist/locales')
+  var localePromises = []
+  glob('./src/locales/*.js', function (err, files) {
+    if (err) console.log(err)
+    files.forEach(function (file) {
+      localePromises.push(buildLocale(file))
+    })
+  })
+  return Promise.all(localePromises)
+}
+
 mkdirp.sync(distPath)
 
-Promise.all([buildUppyBundle(), buildUppyBundle(true), copyLocales()])
+Promise.all([buildUppyBundle(), buildUppyBundle(true), buildUppyLocales()])
   .then(function () {
     console.info(chalk.yellow('✓ JS Bundle 🎉'))
   })
