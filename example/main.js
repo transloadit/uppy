0a99800ac4c82a8521bb4b5dd7a2d08ad65cf5c0
Artur Paikin <artur@arturpaikin.com>
2016-07-01 23:07:08 -0400

    Dashboard does not exist in this universe

--- example/main.js
+++ example/main.js
@@ -2,7 +2,10 @@ const Uppy = require('../src/index.js')
 
 const uppy = new Uppy.Core({debug: true})
   .use(Uppy.plugins.Modal, {trigger: '#uppyModalOpener'})
-  .use(Uppy.plugins.Dashboard, {target: Uppy.plugins.Modal})
+  .use(Uppy.plugins.DragDrop, {target: Uppy.plugins.Modal})
+  // .use(Uppy.plugins.Dashboard, {target: Uppy.plugins.Modal})
   .use(Uppy.plugins.Dummy, {target: Uppy.plugins.Modal})
 
 uppy.run()
+
+document.querySelector('#uppyModalOpener').click()
