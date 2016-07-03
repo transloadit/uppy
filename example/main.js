7e7e10dc1d57b6064950a883b613bc55aa306680
Artur Paikin <artur@arturpaikin.com>
2016-07-03 00:59:52 -0400

    Update example

--- example/main.js
+++ example/main.js
@@ -1,8 +1,17 @@
-const Uppy = require('../src/index.js')
+import Uppy from '../src/core/Core.js'
+import Modal from '../src/plugins/Modal'
+import Dashboard from '../src/plugins/dashboard'
+import Dummy from '../src/plugins/Dummy'
+import ProgressBar from '../src/plugins/ProgressBar'
+import Tus10 from '../src/plugins/Tus10'
 
-const uppy = new Uppy.Core({debug: true})
-  .use(Uppy.plugins.Modal, {trigger: '#uppyModalOpener'})
-  .use(Uppy.plugins.Dashboard, {target: Uppy.plugins.Modal})
-  .use(Uppy.plugins.Dummy, {target: Uppy.plugins.Modal})
+const uppy = new Uppy({debug: true})
+  .use(Modal, {trigger: '#uppyModalOpener'})
+  .use(Dashboard, {target: Modal})
+  .use(Dummy, {target: Modal})
+  .use(ProgressBar, {target: Modal})
+  .use(Tus10, {endpoint: 'http://master.tus.io:8080/files/'})
 
 uppy.run()
+
+document.querySelector('#uppyModalOpener').click()
