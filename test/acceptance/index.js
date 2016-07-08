c50928b43dc0c9904c9405938a0cf46de79d352d
Artur Paikin <artur@arturpaikin.com>
2016-07-08 15:43:53 -0400

    Disable Safari and Edge

--- test/acceptance/index.js
+++ test/acceptance/index.js
@@ -48,8 +48,8 @@ console.log('Acceptance tests will be targetting: ' + host)
 
 // FYI: old Chrome on Windows XP — didn’t pass
 var platforms = [
-  { browser: 'Safari', version: '8.0', os: 'OS X 10.10' },
-  { browser: 'MicrosoftEdge', version: '13.10586', os: 'Windows 10' },
+  // { browser: 'Safari', version: '8.0', os: 'OS X 10.10' },
+  // { browser: 'MicrosoftEdge', version: '13.10586', os: 'Windows 10' },
   { browser: 'Firefox', version: '38.0', os: 'Linux' },
   { browser: 'Internet Explorer', version: '10.0', os: 'Windows 8' },
   { browser: 'Internet Explorer', version: '11.103', os: 'Windows 10' },
