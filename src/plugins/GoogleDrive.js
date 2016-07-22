4192a6237e279d90ed32c17450a8ab0aa229c4ef
Harry Hedger <hedgerh@gmail.com>
2016-07-22 19:44:44 -0400

    Removed socket usage from Google Drive plugin

--- src/plugins/GoogleDrive.js
+++ src/plugins/GoogleDrive.js
@@ -40,51 +40,6 @@ export default class Google extends Plugin {
     this.opts = Object.assign({}, defaultOptions, opts)
 
     const host = this.opts.host.replace(/^https?:\/\//, '')
-
-    this.socket = this.core.initSocket({
-      target: 'ws://' + host + '/'
-    })
-
-    this.socket.on('google.auth.pass', () => {
-      console.log('google.auth.pass')
-      this.getFolder(this.core.getState().googleDrive.directory.id)
-    })
-
-    this.socket.on('uppy.debug', (payload) => {
-      console.log('GOOGLE DEBUG:')
-      console.log(payload)
-    })
-
-    this.socket.on('google.list.ok', (data) => {
-      console.log('google.list.ok')
-      let folders = []
-      let files = []
-      data.items.forEach((item) => {
-        if (item.mimeType === 'application/vnd.google-apps.folder') {
-          folders.push(item)
-        } else {
-          files.push(item)
-        }
-      })
-
-      this.updateState({
-        folders,
-        files,
-        authenticated: true
-      })
-    })
-
-    this.socket.on('google.list.fail', (data) => {
-      console.log('google.list.fail')
-      console.log(data)
-    })
-
-    this.socket.on('google.auth.fail', () => {
-      console.log('google.auth.fail')
-      this.updateState({
-        authenticated: false
-      })
-    })
   }
 
   install () {
