edf2a1333e95e71370d07a55e4c6730a262bc968
Artur Paikin <artur@arturpaikin.com>
2016-07-29 22:28:35 -0400

    MetaData plugin that allows to specify metadata fields

---
+++ src/plugins/MetaData.js
@@ -0,0 +1,42 @@
+import Plugin from './Plugin'
+
+/**
+ * Meta Data
+ * Adds metadata fields to Uppy
+ *
+ */
+export default class MetaData extends Plugin {
+  constructor (core, opts) {
+    super(core, opts)
+    this.type = 'progressindicator'
+    this.id = 'MetaData'
+    this.title = 'Meta Data'
+
+    // set default options
+    const defaultOptions = {}
+
+    // merge default options with the ones set by user
+    this.opts = Object.assign({}, defaultOptions, opts)
+  }
+
+  addInitialMeta () {
+    const metaFields = this.opts.fields
+
+    this.core.setState({
+      metaFields: metaFields
+    })
+
+    this.core.emitter.on('file-added', (fileID) => {
+      metaFields.forEach((item) => {
+        const obj = {}
+        obj[item.name] = item.value
+        this.core.updateMeta(obj, fileID)
+      })
+    })
+  }
+
+  install () {
+    console.log(this.opts.fields)
+    this.addInitialMeta()
+  }
+}
