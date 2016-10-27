18d284a9f71ceaf5a310fe9f1459fe28414a07ea
Artur Paikin <artur@arturpaikin.com>
2016-10-27 16:09:53 -0400

    throttled StatusBar

--- src/plugins/Dashboard/StatusBar.js
+++ src/plugins/Dashboard/StatusBar.js
@@ -1,9 +1,34 @@
 import html from '../../core/html'
+// import { cacheElement } from '../../core/Utils'
+
+let lastUpdate = Date.now()
+let cachedEl
+
+function cacheElement (el, time) {
+  console.log('throttleElement')
+
+  function wrapper () {
+    console.log(Date.now())
+    console.log(lastUpdate)
+    if (Date.now() - lastUpdate < time) {
+      console.log('cached!')
+      return cachedEl
+    }
+
+    console.log('updating progress')
+    cachedEl = el
+    lastUpdate = Date.now()
+
+    return el
+  }
+
+  return wrapper()
+}
 
 export default (props) => {
   props = props || {}
 
-  return html`
+  let statusBar = html`
     <div class="UppyDashboard-statusBar">
       ${!props.isAllComplete
         ? !props.isAllPaused
@@ -11,5 +36,8 @@ export default (props) => {
           : `Paused・${props.totalProgress}%`
         : null
       }
-    </div>`
+    </div>
+  `
+
+  return cacheElement(statusBar, 1000)
 }
