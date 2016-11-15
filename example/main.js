b3e20bc41866af52d49a8bd1007bbda98769ed2f
Artur Paikin <artur@arturpaikin.com>
2016-11-15 09:47:36 -0500

    individual imports and meta viewport in example

--- example/main.js
+++ example/main.js
@@ -1,24 +1,29 @@
-import { Core,
-         Dummy,
-         Dashboard,
-         GoogleDrive,
-         Webcam,
-         Tus10,
-         MetaData,
-         Informer } from '../src/index.js'
+import Core from '../src/core/index.js'
+import Dummy from '../src/plugins/Dummy.js'
+import Dashboard from '../src/plugins/Dashboard'
+import GoogleDrive from '../src/plugins/GoogleDrive'
+import Webcam from '../src/plugins/Webcam'
+import Tus10 from '../src/plugins/Tus10'
+import MetaData from '../src/plugins/MetaData'
+import Informer from '../src/plugins/Informer'
+
+const PROTOCOL = location.protocol === 'https:' ? 'https' : 'http'
+const TUS_ENDPOINT = PROTOCOL + '://master.tus.io/files/'
 
 // import ru_RU from '../src/locales/ru_RU.js'
 // import MagicLog from '../src/plugins/MagicLog'
 // import PersistentState from '../src/plugins/PersistentState'
 
+// const dummy = Dummy()
+// console.log(dummy.render())
+// document.body.appendChild(dummy.render())
+
 const uppy = new Core({debug: true, autoProceed: false})
-  // .use(FileInput, {text: 'Выбрать файл', pretty: true})
-  // .use(PersistentState)
   .use(Dashboard, {trigger: '#uppyModalOpener', inline: false})
   .use(GoogleDrive, {target: Dashboard, host: 'http://localhost:3020'})
   .use(Dummy, {target: Dashboard})
   .use(Webcam, {target: Dashboard})
-  .use(Tus10, {endpoint: 'https://tusd.tus.io/files/', resume: true})
+  .use(Tus10, {endpoint: TUS_ENDPOINT, resume: true})
   .use(Informer, {target: Dashboard})
   .use(MetaData, {
     fields: [
