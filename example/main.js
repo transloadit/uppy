c5d6511ec9aacc63281d5224922cadc2f3750eea
Artur Paikin <artur@arturpaikin.com>
2016-11-25 13:52:06 -0600

    example

--- example/main.js
+++ example/main.js
@@ -1,11 +1,12 @@
-import Core from '../src/core/index.js'
-import Dummy from '../src/plugins/Dummy.js'
+import Uppy from '../src/core/index.js'
+// import Dummy from '../src/plugins/Dummy.js'
 import Dashboard from '../src/plugins/Dashboard'
-import GoogleDrive from '../src/plugins/GoogleDrive'
-import Webcam from '../src/plugins/Webcam'
+// import GoogleDrive from '../src/plugins/GoogleDrive'
+// import Webcam from '../src/plugins/Webcam'
 import Tus10 from '../src/plugins/Tus10'
 import MetaData from '../src/plugins/MetaData'
 import Informer from '../src/plugins/Informer'
+// import Multipart from '../src/plugins/Multipart'
 
 const PROTOCOL = location.protocol === 'https:' ? 'https' : 'http'
 const TUS_ENDPOINT = PROTOCOL + '://master.tus.io/files/'
@@ -14,16 +15,17 @@ const TUS_ENDPOINT = PROTOCOL + '://master.tus.io/files/'
 // import MagicLog from '../src/plugins/MagicLog'
 // import PersistentState from '../src/plugins/PersistentState'
 
-// const dummy = Dummy()
-// console.log(dummy.render())
-// document.body.appendChild(dummy.render())
+// const dummy = Dummy({bla: 'boop'})
 
-const uppy = new Core({debug: true, autoProceed: false})
+const uppy = Uppy({debug: true, autoProceed: false})
   .use(Dashboard, {trigger: '#uppyModalOpener', inline: false})
-  .use(GoogleDrive, {target: Dashboard, host: 'http://localhost:3020'})
-  .use(Dummy, {target: Dashboard})
-  .use(Webcam, {target: Dashboard})
+  // .use(GoogleDrive, {target: Dashboard, host: 'http://localhost:3020'})
+  // .use(Dummy, {target: Dashboard})
+  // .use(dummy)
+  // .use(Webcam, {target: Dashboard})
+  // .use(Multipart, {endpoint: '//api2.transloadit.com'})
   .use(Tus10, {endpoint: TUS_ENDPOINT, resume: true})
+  // .use(Multipart)
   .use(Informer, {target: Dashboard})
   .use(MetaData, {
     fields: [
