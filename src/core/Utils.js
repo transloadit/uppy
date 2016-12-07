9eea9d10c0fa1f1585dd4020cfdda30d5fa76bad
oliverpool <oliverpool@hotmail.fr>
2016-12-07 11:50:23 +0100

    add leading zero to trailing units

--- src/core/Utils.js
+++ src/core/Utils.js
@@ -373,9 +373,12 @@ export function prettyETA (seconds) {
 
   // Only display hours and minutes if they are greater than 0 but always
   // display minutes if hours is being displayed
+  // Display a leading zero if the there is a preceding unit: 1m 05s, but 5s
   const hoursStr = time.hours ? time.hours + 'h ' : ''
-  const minutesStr = (time.hours || time.minutes) ? time.minutes + 'm ' : ''
-  const secondsStr = time.seconds + 's'
+  const minutesVal = time.hours ? ('0' + time.minutes).substr(-2) : time.minutes
+  const minutesStr = minutesVal ? minutesVal + 'm ' : ''
+  const secondsVal = minutesVal ? ('0' + time.seconds).substr(-2) : time.seconds
+  const secondsStr = secondsVal + 's'
 
   return `${hoursStr}${minutesStr}${secondsStr}`
 }
