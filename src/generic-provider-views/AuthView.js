a1c68840a308bc81e05add34d814028d59b47d68
Artur Paikin <artur@arturpaikin.com>
2017-01-24 10:33:38 -0500

    minor Provider Auth View UI improvements

--- src/generic-provider-views/AuthView.js
+++ src/generic-provider-views/AuthView.js
@@ -1,11 +1,13 @@
 const html = require('yo-yo')
 
 module.exports = (props) => {
-  const demoLink = props.demo ? html`<a onclick=${props.handleDemoAuth}>Proceed with Demo Account</a>` : null
+  const demoLink = props.demo ? html`<a class="UppyProvider-authBtnDemo" onclick=${props.handleDemoAuth}>Proceed with Demo Account</a>` : null
   return html`
-    <div class="UppyProvider-authenticate">
-      <h1>You need to authenticate with ${props.pluginName} before selecting files.</h1>
-      <a href=${props.link}>Authenticate</a>
+    <div class="UppyProvider-auth">
+      <h1 class="UppyProvider-authTitle">
+        Please authenticate with <span class="UppyProvider-authTitleName">${props.pluginName}</span><br> to select files
+      </h1>
+      <a class="UppyProvider-authBtn" href=${props.link} target="_blank">Authenticate</a>
       ${demoLink}
     </div>
   `
