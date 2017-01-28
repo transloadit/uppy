0e5236ad696f09021f313a93ebc711af6d63917e
Artur Paikin <artur@arturpaikin.com>
2017-01-28 03:58:14 -0500

    getFileTypeIcon

---
+++ src/plugins/Dashboard/getFileTypeIcon.js
@@ -0,0 +1,36 @@
+const { iconText, iconFile, iconAudio, iconVideo, iconPDF } = require('./icons')
+
+module.exports = function getIconByMime (fileTypeGeneral, fileTypeSpecific) {
+  if (fileTypeGeneral === 'text') {
+    return {
+      color: '#e74c3c',
+      icon: iconText()
+    }
+  }
+
+  if (fileTypeGeneral === 'audio') {
+    return {
+      color: '#1abc9c',
+      icon: iconAudio()
+    }
+  }
+
+  if (fileTypeGeneral === 'video') {
+    return {
+      color: '#2980b9',
+      icon: iconVideo()
+    }
+  }
+
+  if (fileTypeGeneral === 'application' && fileTypeSpecific === 'pdf') {
+    return {
+      color: '#e74c3c',
+      icon: iconPDF()
+    }
+  }
+
+  return {
+    color: '#000',
+    icon: iconFile()
+  }
+}
