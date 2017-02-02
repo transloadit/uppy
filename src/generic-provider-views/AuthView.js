3cbc16ad1cb283ebe0f9305b4def67a513713b6a
Ifedapo Olarewaju <ifedapoolarewaju@gmail.com>
2017-02-02 14:35:49 +0100

    reactor: use button not anchor for semantics

--- src/generic-provider-views/AuthView.js
+++ src/generic-provider-views/AuthView.js
@@ -1,13 +1,13 @@
 const html = require('yo-yo')
 
 module.exports = (props) => {
-  const demoLink = props.demo ? html`<a class="UppyProvider-authBtnDemo" onclick=${props.handleDemoAuth}>Proceed with Demo Account</a>` : null
+  const demoLink = props.demo ? html`<button class="UppyProvider-authBtnDemo" onclick=${props.handleDemoAuth}>Proceed with Demo Account</button>` : null
   return html`
     <div class="UppyProvider-auth">
       <h1 class="UppyProvider-authTitle">
         Please authenticate with <span class="UppyProvider-authTitleName">${props.pluginName}</span><br> to select files
       </h1>
-      <a class="UppyProvider-authBtn" onclick=${props.handleAuth}>Authenticate</a>
+      <button class="UppyProvider-authBtn" onclick=${props.handleAuth}>Authenticate</button>
       ${demoLink}
     </div>
   `
