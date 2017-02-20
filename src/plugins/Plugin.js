6fc9ccbc2c47b88bb91087615b510a711cb04269
Ifedapo Olarewaju <ifedapoolarewaju@gmail.com>
2017-02-20 09:06:22 +0100

    debug: mutiline log

--- src/plugins/Plugin.js
+++ src/plugins/Plugin.js
@@ -32,7 +32,10 @@ module.exports = class Plugin {
 
     const newEl = this.render(state)
     yo.update(this.el, newEl)
-    console.log('plugin update', this.el, state, newEl)
+    console.log('plugin update')
+    console.log(this.el)
+    console.log(state)
+    console.log(newEl)
     // optimizes performance?
     // requestAnimationFrame(() => {
     //   const newEl = this.render(state)
