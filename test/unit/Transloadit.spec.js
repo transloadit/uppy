9a8432744727674a0f13d0bf27788309d5ef67bd
Renée Kooi <renee@kooi.me>
2017-04-28 11:43:52 +0200

    Fix Transloadit plugin tests. (#180)

--- test/unit/Transloadit.spec.js
+++ test/unit/Transloadit.spec.js
@@ -2,16 +2,39 @@ import test from 'tape'
 import Core from '../../src/core/Core'
 import Transloadit from '../../src/plugins/Transloadit'
 
-test('Throws errors if options are missing', (t) => {
+test('Transloadit: Throws errors if options are missing', (t) => {
   const uppy = new Core()
 
   t.throws(() => {
-    uppy.use(Transloadit, { key: null, templateId: 'abc' })
-  }, 'The `key` option is required.')
+    uppy.use(Transloadit, {})
+  }, /The `params` option is required/)
 
   t.throws(() => {
-    uppy.use(Transloadit, { key: 'abc', templateId: null })
-  }, 'The `templateId` option is required.')
+    uppy.use(Transloadit, { params: {} })
+  }, /The `params\.auth\.key` option is required/)
+
+  t.end()
+})
+
+test('Transloadit: Accepts a JSON string as `params` for signature authentication', (t) => {
+  const uppy = new Core()
+
+  t.throws(() => {
+    uppy.use(Transloadit, {
+      params: 'not json'
+    })
+  }, /The `params` option is a malformed JSON string/)
+
+  t.throws(() => {
+    uppy.use(Transloadit, {
+      params: '{"template_id":"some template id string"}'
+    })
+  }, /The `params\.auth\.key` option is required/)
+  t.doesNotThrow(() => {
+    uppy.use(Transloadit, {
+      params: '{"auth":{"key":"some auth key string"},"template_id":"some template id string"}'
+    })
+  }, /The `params\.auth\.key` option is required/)
 
   t.end()
 })
