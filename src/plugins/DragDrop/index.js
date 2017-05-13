f74151659bcbef482e55789ea8760d6818d3cdc2
Artur Paikin <artur@arturpaikin.com>
2017-05-13 00:17:23 -0400

    space

--- src/plugins/DragDrop/index.js
+++ src/plugins/DragDrop/index.js
@@ -148,8 +148,7 @@ module.exports = class DragDrop extends Plugin {
                  value=""
                  onchange=${this.handleInputChange.bind(this)} />
           <label class="UppyDragDrop-label" onclick=${onSelect}>
-            <strong>${this.i18n('chooseFile')}</strong>
-            <span class="UppyDragDrop-dragText">${this.i18n('orDragDrop')}</span>
+            <strong>${this.i18n('chooseFile')}</strong> <span class="UppyDragDrop-dragText">${this.i18n('orDragDrop')}</span>
           </label>
           ${selectedFilesCount > 0
             ? html`<div class="UppyDragDrop-selectedCount">
