e2a9fc2782b0f82b6c263ad500f3f8c945f4dae8
Renée Kooi <renee@kooi.me>
2017-06-19 18:51:06 +0200

    s3: First steps for S3 upload plugin.
    TODO: Remove additional `meta` from files … without breaking other
    plugins that may need it.
    
    Probably should add a `multipart.meta` or `multipart.fields` option that
    is used instead of the `meta` property for additional FormData.

---
+++ src/plugins/AwsS3/index.js
@@ -0,0 +1,65 @@
+const Plugin = require('../Plugin')
+
+module.exports = class AwsS3 extends Plugin {
+  constructor (core, opts) {
+    super(core, opts)
+    this.type = 'uploader'
+    this.id = 'AwsS3'
+    this.title = 'AWS S3'
+
+    const defaultOptions = {
+    }
+
+    this.opts = Object.assign({}, defaultOptions, opts)
+
+    this.prepareUpload = this.prepareUpload.bind(this)
+  }
+
+  getFile (id) {
+    return this.core.getState().files[id]
+  }
+
+  getCredentials (fileID) {
+    const file = this.getFile(fileID)
+
+    const filename = encodeURIComponent(file.name)
+    const type = encodeURIComponent(`${file.type.general}/${file.type.specific}`)
+    return fetch(`${this.opts.host}/s3/credentials?filename=${filename}&type=${type}`, {
+      method: 'get',
+      headers: { accept: 'application/json' }
+    }).then((response) => response.json())
+  }
+
+  prepareUpload (fileIDs) {
+    return Promise.all(
+      fileIDs.map((id) => this.getCredentials(id))
+    ).then((credentials) => {
+      const updatedFiles = {}
+      fileIDs.forEach((id, index) => {
+        const file = this.getFile(id)
+        const { endpoint, params } = credentials[index]
+        const updatedFile = Object.assign({}, file, {
+          meta: Object.assign({}, file.meta, params),
+          multipart: {
+            endpoint,
+            fieldName: 'file'
+          }
+        })
+
+        updatedFiles[id] = updatedFile
+      })
+
+      this.core.setState({
+        files: Object.assign({}, this.core.getState().files, updatedFiles)
+      })
+    })
+  }
+
+  install () {
+    this.core.addPreProcessor(this.prepareUpload)
+  }
+
+  uninstall () {
+    this.core.removePreProcessor(this.prepareUpload)
+  }
+}
