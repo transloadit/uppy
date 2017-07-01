ba405dba13482d516886730c24087d54ccc90314
Artur Paikin <artur@arturpaikin.com>
2017-07-01 16:19:33 -0400

    Buttons should have type="button", otherwise they submit forms :scream_cat:

--- src/plugins/Dashboard/Dashboard.js
+++ src/plugins/Dashboard/Dashboard.js
@@ -58,6 +58,7 @@ module.exports = function Dashboard (props) {
           onload=${() => props.updateDashboardElWidth()}>
 
     <button class="UppyDashboard-close"
+            type="button"
             aria-label="${props.i18n('closeModal')}"
             title="${props.i18n('closeModal')}"
             onclick=${props.hideModal}>${closeIcon()}</button>
@@ -134,6 +135,7 @@ module.exports = function Dashboard (props) {
               ${props.i18n('importFrom')} ${props.activePanel ? props.activePanel.name : null}
             </h2>
             <button class="UppyDashboardContent-back"
+                    type="button"
                     onclick=${props.hideAllPanels}>${props.i18n('done')}</button>
           </div>
           ${props.activePanel ? props.activePanel.render(props.state) : ''}
