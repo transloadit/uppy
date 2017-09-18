ce5d19bb35f93171a338c5dab952dc18a2c3a132
Renée Kooi <renee@kooi.me>
2017-09-18 08:42:14 +0200

    react: revert UppyWrapper version of DragDrop

--- src/uppy-react/DragDrop.js
+++ src/uppy-react/DragDrop.js
@@ -1,7 +1,7 @@
 const React = require('react')
 const PropTypes = require('prop-types')
 const UppyCore = require('../core')
-const UppyWrapper = require('./Wrapper')
+const DragDropPlugin = require('../plugins/DragDrop')
 
 const h = React.createElement
 
@@ -10,14 +10,41 @@ const h = React.createElement
  * uploaded.
  */
 
-const DragDrop = (props) =>
-  h(UppyWrapper, props)
+class DragDrop extends React.Component {
+  componentDidMount () {
+    const uppy = this.props.uppy
+    const options = Object.assign(
+      {},
+      this.props,
+      { target: this.container }
+    )
+    delete options.uppy
+
+    uppy.use(DragDropPlugin, options)
+
+    this.plugin = uppy.getPlugin('DragDrop')
+  }
+
+  componentWillUnmount () {
+    const uppy = this.props.uppy
+
+    uppy.removePlugin(this.plugin)
+  }
+
+  render () {
+    return h('div', {
+      ref: (container) => {
+        this.container = container
+      }
+    })
+  }
+}
 
 DragDrop.propTypes = {
-  uppy: PropTypes.instanceOf(UppyCore).isRequired
+  uppy: PropTypes.instanceOf(UppyCore).isRequired,
+  locale: PropTypes.object
 }
 DragDrop.defaultProps = {
-  plugin: 'DragDrop'
 }
 
 module.exports = DragDrop
