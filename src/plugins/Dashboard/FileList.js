22174816f780ba586ee8a1f0648e29a19e0efc04
Artur Paikin <artur@arturpaikin.com>
2017-11-12 11:33:47 -0500

    remove extra input

--- src/plugins/Dashboard/FileList.js
+++ src/plugins/Dashboard/FileList.js
@@ -20,8 +20,6 @@ module.exports = (props) => {
             ? html`<p class="UppyDashboard-note">${props.note}</p>`
             : ''
           }
-          <input class="UppyDashboard-input" aria-hidden="true" type="file" name="files[]" multiple="true"
-                 onchange=${props.handleInputChange} />
          </div>`
        : null
       }
