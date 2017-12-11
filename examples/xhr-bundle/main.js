2fc44cb6e38a792c53e3b6fa989c3ae16957b132
Renée Kooi <renee@kooi.me>
2017-12-11 12:49:35 +0100

    examples: Add XHRUpload `bundle` option example.

---
+++ examples/xhr-bundle/main.js
@@ -0,0 +1,21 @@
+const Uppy = require('uppy/lib/core')
+const Dashboard = require('uppy/lib/plugins/Dashboard')
+const XHRUpload = require('uppy/lib/plugins/XHRUpload')
+
+const uppy = Uppy({
+  autoProceed: false,
+  debug: true
+})
+
+uppy.use(Dashboard, {
+  target: '#app',
+  inline: true
+})
+
+uppy.use(XHRUpload, {
+  bundle: true,
+  endpoint: 'http://localhost:9967/upload',
+  fieldName: 'files'
+})
+
+uppy.run()
