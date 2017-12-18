34632191c0fe9ce3161b1b87e0af3dd33e2c2b51
Renée Kooi <renee@kooi.me>
2017-12-18 10:37:13 +0100

    Revert example

--- examples/bundled-example/main.js
+++ examples/bundled-example/main.js
@@ -3,25 +3,18 @@ const Dashboard = require('../../src/plugins/Dashboard')
 const GoogleDrive = require('../../src/plugins/GoogleDrive')
 const Dropbox = require('../../src/plugins/Dropbox')
 const Instagram = require('../../src/plugins/Instagram')
-// const Webcam = require('../../src/plugins/Webcam')
+const Webcam = require('../../src/plugins/Webcam')
 const Tus = require('../../src/plugins/Tus')
-const Transloadit = require('../../src/plugins/Transloadit')
-// const Multipart = require('../../src/plugins/Multipart')
+// const XHRUpload = require('../../src/plugins/XHRUpload')
 // const FileInput = require('../../src/plugins/FileInput')
 // const MetaData = require('../../src/plugins/MetaData')
 // const Informer = require('../../src/plugins/Informer')
 // const StatusBar = require('../../src/plugins/StatusBar')
 // const DragDrop = require('../../src/plugins/DragDrop')
-const GoldenRetriever = require('../../src/plugins/GoldenRetriever')
+// const GoldenRetriever = require('../../src/plugins/GoldenRetriever')
 
 const PROTOCOL = location.protocol === 'https:' ? 'https' : 'http'
 const TUS_ENDPOINT = PROTOCOL + '://master.tus.io/files/'
-
-console.warn(`
-
-  STARTING EXAMPLE!
-
-`)
 
 const uppy = Uppy({
   debug: true,
@@ -30,9 +23,30 @@ const uppy = Uppy({
     username: 'John',
     license: 'Creative Commons'
   }
+  // restrictions: {
+  //   maxFileSize: 300000,
+  //   maxNumberOfFiles: 10,
+  //   minNumberOfFiles: 2,
+  //   allowedFileTypes: ['image/*', 'video/*']
+  // }
+  // onBeforeFileAdded: (currentFile, files) => {
+  //   if (currentFile.name === 'pitercss-IMG_0616.jpg') {
+  //     return Promise.resolve()
+  //   }
+  //   return Promise.reject('this is not the file I was looking for')
+  // },
+  // onBeforeUpload: (files) => {
+  //   if (Object.keys(files).length < 2) {
+  //     return Promise.reject('too few files')
+  //   }
+  //   return Promise.resolve()
+  // }
 })
   .use(Dashboard, {
     trigger: '#uppyModalOpener',
+    target: '.MyForm',
+    // maxWidth: 350,
+    // maxHeight: 400,
     inline: false,
     disableStatusBar: false,
     disableInformer: false,
@@ -40,33 +54,22 @@ const uppy = Uppy({
     replaceTargetContent: false,
     hideUploadButton: false,
     closeModalOnClickOutside: false,
-    metaFields: [
-      { id: 'license', name: 'License', value: 'Creative Commons', placeholder: 'specify license' },
-      { id: 'caption', name: 'Caption', value: 'none', placeholder: 'describe what the image is about' }
-    ],
     locale: {
-      strings: {browse: 'browse'}
-    }
+      strings: { browse: 'browse' }
+    },
+    metaFields: [
+      { id: 'license', name: 'License', placeholder: 'specify license' },
+      { id: 'caption', name: 'Caption', placeholder: 'describe what the image is about' }
+    ]
+    // note: 'Images and video only, 300kb or less'
   })
   .use(GoogleDrive, {target: Dashboard, host: 'http://localhost:3020'})
   .use(Dropbox, {target: Dashboard, host: 'http://localhost:3020'})
   .use(Instagram, {target: Dashboard, host: 'http://localhost:3020'})
-  // .use(Tus, { resume: false })
-  .use(require('../../src/plugins/XHRUpload'))
-  .use(require('../../src/plugins/AwsS3'), {
-    host: 'http://localhost:3020'
-  })
+  .use(Webcam, {target: Dashboard})
+  .use(Tus, {endpoint: TUS_ENDPOINT, resume: true})
+  // .use(GoldenRetriever, {serviceWorker: true})
   .run()
-  .use(Transloadit, {
-    importFromUploadURLs: true,
-    // alwaysRunAssembly: true,
-    waitForEncoding: true,
-    params: {
-      auth: { key: '05a61ed019fe11e783fdbd1f56c73eb0' },
-      template_id: 'ff1fb8201b7211e7bbafa9a78f1dc173'
-    }
-  })
-  .use(GoldenRetriever, {})
 
 uppy.on('complete', ({ successful, failed }) => {
   if (failed.length === 0) {
@@ -76,47 +79,6 @@ uppy.on('complete', ({ successful, failed }) => {
   }
   console.log('successful files:', successful)
   console.log('failed files:', failed)
-  localStorage.savedResults = '[]'
-})
-
-const wrapper = document.createElement('div')
-const yo = require('yo-yo')
-function render () {
-  yo.update(wrapper, yo`
-    <div style="margin-top: 20px;">
-      <h1>Results (${results.length})</h1>
-      <div style="display: flex; justify-content: space-around; flex-wrap: wrap;">
-        ${results.map(ResultPreview)}
-      </div>
-    </div>
-  `)
-}
-
-function ResultPreview (result) {
-  if (result.type === 'image') {
-    return yo`<img class="UppyDemo-resultImg" src="${result.ssl_url}" />`
-  }
-  if (result.type === 'video') {
-    return yo`<video width="320" height="240" src="${result.ssl_url}" />`
-  }
-  if (result.type === 'audio') {
-    return yo`<audio width="320" height="52" src="${result.ssl_url}" />`
-  }
-
-  return ''
-}
-
-let results = []
-try {
-  results = JSON.parse(localStorage.savedResults)
-  render()
-} catch (err) {}
-
-uppy.on('transloadit:result', (stepName, result) => {
-  results.push({ ssl_url: result.ssl_url, type: result.type })
-  render()
-  localStorage.savedResults = JSON.stringify(results)
->>>>>>> Stashed changes
 })
 
 if ('serviceWorker' in navigator) {
@@ -132,5 +94,3 @@ if ('serviceWorker' in navigator) {
 
 var modalTrigger = document.querySelector('#uppyModalOpener')
 if (modalTrigger) modalTrigger.click()
-
-document.body.appendChild(wrapper)
