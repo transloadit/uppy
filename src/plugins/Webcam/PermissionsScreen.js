2a18782722c2205979cf608929aeab789b13e0d1
muhammadInam <inam.haider92@gmail.com>
2018-01-09 00:42:18 -0800

    Font styling for Webcam option. Styling made similar to rest of dashboard options

--- src/plugins/Webcam/PermissionsScreen.js
+++ src/plugins/Webcam/PermissionsScreen.js
@@ -3,7 +3,7 @@ const { h } = require('preact')
 module.exports = (props) => {
   return (
     <div class="uppy-Webcam-permissons">
-      <h1>Please allow access to your camera</h1>
+      <h1 class="uppy-Webcam-Title">Please allow access to your camera</h1>
       <p>You have been prompted to allow camera access from this site.<br />
       In order to take pictures with your camera you must approve this request.</p>
     </div>
