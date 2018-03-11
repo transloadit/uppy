1abe4b13083f8bd81456c931efc429afd0e8d93c
Artur Paikin <artur@arturpaikin.com>
2018-03-10 21:22:32 -0500

    Options to disable showLinkToFileUploadResult and meta editing if metaFields is not provided

--- src/plugins/Dashboard/FileItem.js
+++ src/plugins/Dashboard/FileItem.js
@@ -48,10 +48,15 @@ module.exports = function fileItem (props) {
   return <li class={dashboardItemClass} id={`uppy_${file.id}`} title={file.meta.name}>
     <div class="uppy-DashboardItem-preview">
       <div class="uppy-DashboardItem-previewInnerWrap" style={{ backgroundColor: getFileTypeIcon(file.type).color }}>
+        {props.showLinkToFileUploadResult && file.uploadURL
+          ? <a class="uppy-DashboardItem-previewLink" href={file.uploadURL} target="_blank" />
+          : null
+        }
         {file.preview
           ? <img alt={file.name} src={file.preview} />
           : <div class="uppy-DashboardItem-previewIconWrap">
             <span class="uppy-DashboardItem-previewIcon" style={{ color: getFileTypeIcon(file.type).color }}>{getFileTypeIcon(file.type).icon}</span>
+            <span class="uppy-DashboardItem-previewType">{file.extension}</span>
             <svg class="uppy-DashboardItem-previewIconBg" width="72" height="93" viewBox="0 0 72 93"><g><path d="M24.08 5h38.922A2.997 2.997 0 0 1 66 8.003v74.994A2.997 2.997 0 0 1 63.004 86H8.996A2.998 2.998 0 0 1 6 83.01V22.234L24.08 5z" fill="#FFF" /><path d="M24 5L6 22.248h15.007A2.995 2.995 0 0 0 24 19.244V5z" fill="#E4E4E4" /></g></svg>
           </div>
         }
@@ -104,14 +109,15 @@ module.exports = function fileItem (props) {
           </div>
         }
       </div>
-      {!uploadInProgressOrComplete &&
-        <button class="uppy-DashboardItem-edit"
+      {(!uploadInProgressOrComplete && props.metaFields && props.metaFields.length)
+        ? <button class="uppy-DashboardItem-edit"
           type="button"
           aria-label={props.i18n('editFile')}
           title={props.i18n('editFile')}
-          onclick={(e) => props.showFileCard(file.id)}>
+          onclick={(e) => props.toggleFileCard(file.id)}>
           {iconEdit()}
         </button>
+        : null
       }
       {file.uploadURL &&
         <button class="uppy-DashboardItem-copyLink"
