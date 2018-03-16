928801fc87b2cbd4de97882746f695b363c218c9
Lucas Sifoni <Lucassifoni@users.noreply.github.com>
2018-03-16 10:24:13 +0100

    Create fr_FR locale
    I use Uppy for my clients, and needed to translate the UI. I settled for "Envoyer" instead of "Téléverser" which is the formal term for "Upload", but isn't as weel understood.

---
+++ src/locales/fr_FR.js
@@ -0,0 +1,54 @@
+/* eslint camelcase: 0 */
+
+const fr_FR = {}
+
+fr_FR.strings = {
+  chooseFile: 'Sélectionnez un fichier',
+  youHaveChosen: 'Vous avez sélectionné: %{fileName}',
+  orDragDrop: 'ou glissez-le ici',
+  filesChosen: {
+    0: '%{smart_count} fichier sélectionné',
+    1: '%{smart_count} fichiers sélectionnés'
+  },
+  filesUploaded: {
+    0: '%{smart_count} fichier envoyé',
+    1: '%{smart_count} fichiers envoyés'
+  },
+  files: {
+    0: '%{smart_count} fichier',
+    1: '%{smart_count} fichiers'
+  },
+  uploadFiles: {
+    0: 'Envoyer %{smart_count} fichier',
+    1: 'Envoyer %{smart_count} fichiers'
+  },
+  selectToUpload: 'Sélectionnez des fichiers à envoyer',
+  closeModal: 'Fermer',
+  upload: 'Envoyer',
+  importFrom: 'Importer des fichiers depuis',
+  dashboardWindowTitle: 'Panneau de contrôle d'Uppy (Appuyez sur échap. pour le fermer)',
+  dashboardTitle: 'Panneau de contrôle d'Uppy',
+  copyLinkToClipboardSuccess: 'Lien copié dans le presse-papier.',
+  copyLinkToClipboardFallback: 'Copiez l'URL ci-dessous',
+  done: 'Terminé',
+  localDisk: 'Fichiers locaux',
+  dropPasteImport: 'Glissez des fichiers ici, collez-en, ou importez les depuis les plate-formes ci-dessus.',
+  dropPaste: 'Glissez des fichiers ici, ou collez-en',
+  browse: 'Naviguer',
+  fileProgress: 'Progression: vitesse d'envoi et temps d'attente estimé',
+  numberOfSelectedFiles: 'Nombre de fichiers sélectionnés',
+  uploadAllNewFiles: 'Envoyer tous les nouveaux fichiers'
+}
+
+fr_FR.pluralize = function (n) {
+  if (n === 1) {
+    return 0
+  }
+  return 1
+}
+
+if (typeof window !== 'undefined' && typeof window.Uppy !== 'undefined') {
+  window.Uppy.locales.fr_FR = fr_FR
+}
+
+module.exports = fr_FR
