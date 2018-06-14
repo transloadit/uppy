8b1bae796d2ef82dea676da7cfc71b0588d175e2
Renée Kooi <renee@kooi.me>
2018-06-14 12:54:46 +0200

    Add Babel build script for Lerna.
    Builds packages/*/src/**/*.js into packages/*/lib/**/*.js, ignoring
    tests.

---
+++ bin/build-lib.js
@@ -0,0 +1,34 @@
+const babel = require('babel-core')
+const { promisify } = require('util')
+const glob = promisify(require('glob'))
+const mkdirp = promisify(require('mkdirp'))
+const fs = require('fs')
+const path = require('path')
+
+const transformFile = promisify(babel.transformFile)
+const writeFile = promisify(fs.writeFile)
+
+const SOURCE = 'packages/{*,@uppy/*}/src/**/*.js'
+// Files not to build (such as tests)
+const IGNORE = /\.test\.js$|__mocks__/
+
+async function buildLib () {
+  const files = await glob(SOURCE)
+  for (const file of files) {
+    if (IGNORE.test(file)) continue
+
+    const libFile = file.replace('/src/', '/lib/')
+    const { code, map } = await transformFile(file, {})
+    await mkdirp(path.dirname(libFile))
+    await Promise.all([
+      writeFile(libFile, code),
+      writeFile(libFile + '.map', JSON.stringify(map))
+    ])
+    console.log(libFile)
+  }
+}
+
+buildLib().catch((err) => {
+  console.error(err.stack)
+  process.exit(1)
+})
