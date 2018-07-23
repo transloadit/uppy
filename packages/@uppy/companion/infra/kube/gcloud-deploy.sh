0680660757f37057012494a8af682ee8d44067e4
Abdelhadi Khiati <kiloreux@gmail.com>
2018-07-23 14:19:08 +0100

    Changes

--- packages/@uppy/companion/infra/kube/gcloud-deploy.sh
+++ packages/@uppy/companion/infra/kube/gcloud-deploy.sh
@@ -9,10 +9,10 @@ __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 __kube="${__dir}"
 
 # Store the new image in docker hub
-docker build --quiet -t transloadit/uppy-server:latest -t transloadit/uppy-server:$TRAVIS_COMMIT .;
+docker build --quiet -t transloadit/uppy-server:latest -t transloadit/uppy-companion:$TRAVIS_COMMIT .;
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD";
-docker push transloadit/uppy-server:$TRAVIS_COMMIT;
-docker push transloadit/uppy-server:latest;
+docker push transloadit/uppy-companion:$TRAVIS_COMMIT;
+docker push transloadit/uppy-companion:latest;
 
 echo $CA_CRT | base64 --decode -i > ${HOME}/ca.crt
 
@@ -24,16 +24,16 @@ kubectl config set-credentials travis-uppy --token=$SA_TOKEN
 kubectl config set-context travis --cluster=$CLUSTER_NAME --user=travis-uppy --namespace=uppy
 kubectl config use-context travis
 # Should be already removed. Using it temporarily.
-rm -f "${__kube}/uppy-server/uppy-env.yaml"
-echo $UPPY_ENV | base64 --decode > "${__kube}/uppy-server/uppy-env.yaml"
+rm -f "${__kube}/companion/uppy-env.yaml"
+echo $UPPY_ENV | base64 --decode > "${__kube}/companion/uppy-env.yaml"
 
 kubectl config current-context
 
-kubectl apply -f "${__kube}/uppy-server/uppy-env.yaml"
+kubectl apply -f "${__kube}/companion/uppy-env.yaml"
 sleep 10s # This cost me some precious debugging time.
-kubectl apply -f "${__kube}/uppy-server/uppy-server-kube.yaml"
-kubectl apply -f "${__kube}/uppy-server/uppy-server-redis.yaml"
-kubectl set image statefulset uppy-server --namespace=uppy uppy-server=docker.io/transloadit/uppy-server:$TRAVIS_COMMIT
+kubectl apply -f "${__kube}/companion/uppy-server-kube.yaml"
+kubectl apply -f "${__kube}/companion/uppy-server-redis.yaml"
+kubectl set image statefulset uppy-server --namespace=uppy uppy-server=docker.io/transloadit/uppy-companion:$TRAVIS_COMMIT
 sleep 10s
 
 kubectl get pods --namespace=uppy
@@ -42,7 +42,7 @@ kubectl get deployment --namespace=uppy
 
 function cleanup {
     printf "Cleaning up...\n"
-    rm -vf "${__kube}/uppy-server/uppy-env.yaml"
+    rm -vf "${__kube}/companion/uppy-env.yaml"
     printf "Cleaning done."
 }
 
