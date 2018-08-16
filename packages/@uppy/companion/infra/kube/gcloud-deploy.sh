4c9c4818db6eefd30bf6690fb3466c486f13605b
Abdelhadi Khiati <kiloreux@gmail.com>
2018-08-16 22:41:14 +0100

    remove env

--- packages/@uppy/companion/infra/kube/gcloud-deploy.sh
+++ packages/@uppy/companion/infra/kube/gcloud-deploy.sh
@@ -23,19 +23,17 @@ docker push kiloreux/uppy-companion:$TRAVIS_COMMIT;
 docker push kiloreux/uppy-companion:latest;
 
 
-echo "base64 testing..."
-echo $TRAVIS|base64 --decode -i
 echo "Create directory..."
 mkdir ${HOME}/.kube
 echo "Writing KUBECONFIG to file..."
 echo $KUBECONFIG | base64 --decode -i > ${HOME}/.kube/config
 
 # Should be already removed. Using it temporarily.
-rm -f "${__kube}/companion/companion-env.yaml"
-echo "Writing COMPANION_ENV to file..."
-echo $COMPANION_ENV | base64 --decode > "${__kube}/companion/companion-env.yaml"
+# rm -f "${__kube}/companion/companion-env.yaml"
+# echo "Writing COMPANION_ENV to file..."
+# echo $COMPANION_ENV | base64 --decode > "${__kube}/companion/companion-env.yaml"
 
-kubectl apply -f "${__kube}/companion/companion-env.yaml"
+# kubectl apply -f "${__kube}/companion/companion-env.yaml"
 sleep 10s # This cost me some precious debugging time.
 kubectl apply -f "${__kube}/companion/companion-kube.yaml"
 kubectl apply -f "${__kube}/companion/companion-redis.yaml"
