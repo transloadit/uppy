bffe13d0c49ad55b72fc04711201fd6b89eee0dd
Renée Kooi <renee@kooi.me>
2018-11-01 15:54:41 +0100

    Nicen demo page

--- examples/transloadit/main.js
+++ examples/transloadit/main.js
@@ -1,22 +1,8 @@
 const transloadit = require('@uppy/transloadit-preset')
 
-transloadit.modal('body', {
-  restrictions: {
-    allowedFileTypes: ['.png']
-  },
-  waitForEncoding: true,
-  params: {
-    auth: { key: '05a61ed019fe11e783fdbd1f56c73eb0' },
-    template_id: 'be001500a56011e889f9cddd88df842c'
-  },
-  providers: [
-    'webcam'
-  ]
-  // if providers need custom config
-  // webcam: {
-  //   option: 'whatever'
-  // }
-}).then(console.log, console.error)
+/**
+ * transloadit.form
+ */
 
 window.formUppy = transloadit.form('#test-form', {
   debug: true,
@@ -30,24 +16,42 @@ window.formUppy = transloadit.form('#test-form', {
   }
 })
 
-const input = document.createElement('input')
-input.setAttribute('type', 'file')
-input.setAttribute('multiple', 'multiple')
+/**
+ * transloadit.modal
+ */
 
-const p = document.createElement('p')
-p.append(
-  document.createTextNode('An <input type=file> backed by `transloadit.upload`:'),
-  input
-)
+function openModal () {
+  transloadit.modal('body', {
+    restrictions: {
+      allowedFileTypes: ['.png']
+    },
+    waitForEncoding: true,
+    params: {
+      auth: { key: '05a61ed019fe11e783fdbd1f56c73eb0' },
+      template_id: 'be001500a56011e889f9cddd88df842c'
+    },
+    providers: [
+      'webcam'
+    ]
+    // if providers need custom config
+    // webcam: {
+    //   option: 'whatever'
+    // }
+  }).then(console.log, console.error)
+}
 
-input.addEventListener('change', () => {
-  transloadit.upload(input.files, {
+window.openModal = openModal
+
+/**
+ * transloadit.upload
+ */
+
+window.doUpload = (event) => {
+  transloadit.upload(event.target.files, {
     waitForEncoding: true,
     params: {
       auth: { key: '05a61ed019fe11e783fdbd1f56c73eb0' },
       template_id: 'be001500a56011e889f9cddd88df842c'
     }
   }).then(console.log, console.error)
-})
-
-document.querySelector('main').append(p)
+}
