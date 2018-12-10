71fba74014432c17a286c66f0e53e9dc94ebc6c9
Renée Kooi <renee@kooi.me>
2018-12-10 12:31:56 +0100

    webcam,xhr-upload: Test and typing fixes

--- packages/@uppy/webcam/types/index.d.ts
+++ packages/@uppy/webcam/types/index.d.ts
@@ -1,25 +1,25 @@
 import Uppy = require('@uppy/core');
 
 declare module Webcam {
-    export type WebcamMode = 'video-audio' | 'video-only' | 'audio-only' | 'picture';
+  export type WebcamMode = 'video-audio' | 'video-only' | 'audio-only' | 'picture';
 
-    export interface WebcamOptions extends Uppy.PluginOptions {
-        onBeforeSnapshot?: () => Promise<void>;
-        countdown?: number | boolean;
-        mirror?: boolean;
-        facingMode?: string;
-        modes: WebcamMode[];
-    }
+  export interface WebcamOptions extends Uppy.PluginOptions {
+    onBeforeSnapshot?: () => Promise<void>;
+    countdown?: number | boolean;
+    mirror?: boolean;
+    facingMode?: string;
+    modes: WebcamMode[];
+  }
 }
 
 declare class Webcam extends Uppy.Plugin {
-    constructor(uppy: Uppy.Uppy, opts: Partial<Webcam.WebcamOptions>);
+  constructor(uppy: Uppy.Uppy, opts: Partial<Webcam.WebcamOptions>);
 }
 
 export = Webcam;
 
 declare module '@uppy/core' {
-    export interface Uppy {
-        use(pluginClass: typeof Webcam, opts: Partial<Webcam.WebcamOptions>): Uppy.Uppy;
-    }
+  export interface Uppy {
+    use(pluginClass: typeof Webcam, opts: Partial<Webcam.WebcamOptions>): Uppy.Uppy;
+  }
 }
--- packages/@uppy/xhr-upload/types/index.d.ts
+++ packages/@uppy/xhr-upload/types/index.d.ts
@@ -2,27 +2,27 @@ import Uppy = require('@uppy/core');
 
 declare module XHRUpload {
   export interface XHRUploadOptions extends Uppy.PluginOptions {
-    limit: string;
-    bundle: boolean;
-    formData: FormData;
-    headers: any;
-    metaFields: string[];
-    fieldName: string;
-    timeout: number;
-    responseUrlFieldName: string;
+    limit?: number;
+    bundle?: boolean;
+    formData?: FormData;
+    headers?: any;
+    metaFields?: string[];
+    fieldName?: string;
+    timeout?: number;
+    responseUrlFieldName?: string;
     endpoint: string;
-    method: 'GET' | 'POST' | 'HEAD';
+    method?: 'GET' | 'POST' | 'PUT' | 'HEAD';
   }
 }
 
 declare class XHRUpload extends Uppy.Plugin {
-  constructor(uppy: Uppy.Uppy, opts: Partial<XHRUpload.XHRUploadOptions>);
+  constructor(uppy: Uppy.Uppy, opts: XHRUpload.XHRUploadOptions);
 }
 
 export = XHRUpload;
 
 declare module '@uppy/core' {
   export interface Uppy {
-    use(pluginClass: typeof XHRUpload, opts: Partial<XHRUpload.XHRUploadOptions>): Uppy.Uppy;
+    use(pluginClass: typeof XHRUpload, opts: XHRUpload.XHRUploadOptions): Uppy.Uppy;
   }
 }
