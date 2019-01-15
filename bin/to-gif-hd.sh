36be5320b3fd86164a63d583b7db18bc0b7e4818
Artur Paikin <artur@arturpaikin.com>
2019-01-15 11:16:19 +0300

    video to gif script, decent quality
    worked well for “2hr youtube” demo
    `to-gif /path/to/input.mp4 /path/to/output.gif`

---
+++ bin/to-gif-hd.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# Convert a video file to a gif.
+# `to-gif /path/to/input.mp4 /path/to/output.gif`
+palette="/tmp/to-gif-palette.png"
+filters="fps=15"
+ffmpeg -v warning -i $1 -vf "$filters,palettegen" -y $palette
+ffmpeg -v warning -i $1 -i $palette -lavfi "$filters [x]; [x][1:v] paletteuse" -y $2
+
+# resize after
+# gifsicle --resize-fit-width 1000 -i animation.gif > animation-1000px.gif
