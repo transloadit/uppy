09d0f3b16a34f80488672779d9e50fbcba08883c
Artur Paikin <artur@arturpaikin.com>
2019-02-06 14:09:54 +0300

    fix breadcrumbs, bug with x button being stuck

--- packages/@uppy/provider-views/src/Breadcrumbs.js
+++ packages/@uppy/provider-views/src/Breadcrumbs.js
@@ -2,18 +2,26 @@ const { h } = require('preact')
 
 const Breadcrumb = (props) => {
   return (
-    <button type="button" onclick={props.getFolder}>{props.title}</button>
+    <span>
+      <button
+        type="button"
+        class="uppy-u-reset"
+        onclick={props.getFolder}>{props.title}</button>
+      {!props.isLast ? ' / ' : ''}
+    </span>
   )
 }
 
 module.exports = (props) => {
   return (
     <div class="uppy-Provider-breadcrumbs">
+      <div class="uppy-Provider-breadcrumbsIcon">{props.breadcrumbsIcon}</div>
       {
         props.directories.map((directory, i) => {
           return Breadcrumb({
             getFolder: () => props.getFolder(directory.id),
-            title: i === 0 ? props.title : directory.title
+            title: i === 0 ? props.title : directory.title,
+            isLast: i + 1 === props.directories.length
           })
         })
       }
