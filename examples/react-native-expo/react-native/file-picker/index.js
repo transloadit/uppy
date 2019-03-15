776d6ae77a044ae5b5db989c7acf9b36ddfdcf35
Artur Paikin <artur@arturpaikin.com>
2019-03-15 13:57:07 +0300

    upload shouldn’t start here

--- examples/react-native-expo/react-native/file-picker/index.js
+++ examples/react-native-expo/react-native/file-picker/index.js
@@ -65,7 +65,6 @@ export default class UppyReactNativeFilePicker extends React.Component {
         type: file.type,
         data: file
       })
-      this.uppy.upload()
       this.props.onRequestClose()
     })
   }
