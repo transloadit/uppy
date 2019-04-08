c70409c006ee3a7762b46bf4a4616579dc0831c4
Alexander Zaytsev <nqst@users.noreply.github.com>
2019-04-08 11:23:52 +0300

    Correct font sizes; Improved Add icon

--- packages/@uppy/dashboard/src/components/PickerPanelTopBar.js
+++ packages/@uppy/dashboard/src/components/PickerPanelTopBar.js
@@ -94,8 +94,8 @@ function PanelTopBar (props) {
           aria-label={props.i18n('addMoreFiles')}
           title={props.i18n('addMoreFiles')}
           onclick={() => props.toggleAddFilesPanel(true)}>
-          <svg class="UppyIcon" width="15" height="15" viewBox="0 0 13 13" version="1.1" xmlns="http://www.w3.org/2000/svg">
-            <path d="M7,6 L13,6 L13,7 L7,7 L7,13 L6,13 L6,7 L0,7 L0,6 L6,6 L6,0 L7,0 L7,6 Z" />
+          <svg class="UppyIcon" width="14" height="14" viewBox="0 0 14 14" xmlns="http://www.w3.org/2000/svg">
+            <path d="M7.5 6H13a.5.5 0 0 1 .5.5V7a.5.5 0 0 1-.5.5H7.5V13a.5.5 0 0 1-.5.5h-.5A.5.5 0 0 1 6 13V7.5H.5A.5.5 0 0 1 0 7v-.5A.5.5 0 0 1 .5 6H6V.5a.5.5 0 0 1 .5-.5H7a.5.5 0 0 1 .5.5V6z" />
           </svg>
         </button>
       }
