85e63f94bf72f2977807d6db6ddab254f426f3ef
Artur Paikin <artur@arturpaikin.com>
2019-04-12 15:21:34 +0300

    write a pretty en_US locale to a file, using stringify-object and template.js

--- bin/build-locale-pack.js
+++ bin/build-locale-pack.js
@@ -1,65 +1,86 @@
-/* eslint-disable */
 const glob = require('glob')
-const Dashboard = require('../packages/@uppy/dashboard')
-const StatusBar = require('../packages/@uppy/status-bar')
 const Uppy = require('../packages/@uppy/core')
 const chalk = require('chalk')
 const path = require('path')
+const stringifyObject = require('stringify-object')
+const fs = require('fs')
 
 const uppy = Uppy()
 
-const plugins = {}
-const packagesGlobPath = path.join(__dirname, '..', 'packages', '@uppy', '*', 'package.json')
-glob(packagesGlobPath, (er, files) => {
+// Go over all uppy plugins, check if they are constructors
+// and instanciate them, check for defaultLocale property,
+// then add to plugins object
+
+function buildPluginsList () {
+  const packagesGlobPath = path.join(__dirname, '..', 'packages', '@uppy', '*', 'package.json')
+  const files = glob.sync(packagesGlobPath)
+  const plugins = {}
+
   files.forEach((file) => {
     const dirName = path.dirname(file)
     const pluginName = path.basename(dirName)
+    const Plugin = require(dirName)
+    let plugin
 
     try {
-      const Plugin = require(dirName)
-      const plugin = new Plugin(uppy)
-      // console.log(Plugin)
-      if (plugin.defaultLocale) {
-        plugins[pluginName] = plugin
-      }
+      plugin = new Plugin(uppy)
     } catch (err) {
 
     }
 
+    if (plugin && plugin.defaultLocale) {
+      plugins[pluginName] = plugin
+    }
+
     // console.log(pluginName)
   })
-  console.log(plugins)
-  process.exit()
-})
 
-return
+  return plugins
+}
 
-// es-lint ignore //
-
-const DashboardLocale = new Dashboard(uppy)
-const StatusBarLocale = new StatusBar(uppy)
-
-const localePack = {}
-
-function addLocaleToPack (plugin) {
+function addLocaleToPack (plugin, pluginName) {
   const localeStrings = plugin.defaultLocale.strings
 
-  Object.keys(localeStrings).forEach((key) => {
+  for (let key in localeStrings) {
     const valueInPlugin = JSON.stringify(localeStrings[key])
     const valueInPack = JSON.stringify(localePack[key])
 
     if (key in localePack && valueInPlugin !== valueInPack) {
-      console.error(`Plugin ${chalk.magenta(plugin.id || 'Core')} has a duplicate key: ${chalk.magenta(key)}`)
+      console.error(`Plugin ${chalk.magenta(pluginName)} has a duplicate key: ${chalk.magenta(key)}`)
       console.error(`Value in plugin: ${chalk.cyan(valueInPlugin)}`)
       console.error(`Value in pack  : ${chalk.yellow(valueInPack)}`)
       console.log()
     }
     localePack[key] = localeStrings[key]
-  })
+  }
 }
 
-addLocaleToPack(DashboardLocale)
-addLocaleToPack(StatusBarLocale)
-addLocaleToPack(uppy)
+function sortObjectAlphabetically (obj, sortFunc) {
+  return Object.keys(obj).sort(sortFunc).reduce(function (result, key) {
+    result[key] = obj[key]
+    return result
+  }, {})
+}
 
-// console.log(localePack)
+const plugins = buildPluginsList()
+let localePack = {}
+
+for (let pluginName in plugins) {
+  addLocaleToPack(plugins[pluginName], pluginName)
+}
+
+localePack = sortObjectAlphabetically(localePack)
+
+const prettyLocale = stringifyObject(localePack, {
+  indent: '  ',
+  singleQuotes: true,
+  inlineCharacterLimit: 12
+})
+
+const localeTemplatePath = path.join(__dirname, '..', 'packages', '@uppy', 'locales', 'template.js')
+const template = fs.readFileSync(localeTemplatePath, 'utf-8')
+
+const finalLocale = template.replace('en_US.strings = {}', 'en_US.strings = ' + prettyLocale)
+
+const localePackagePath = path.join(__dirname, '..', 'packages', '@uppy', 'locales', 'en_US.js')
+fs.writeFileSync(localePackagePath, finalLocale, 'utf-8')
