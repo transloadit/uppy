d321be016f3c279a411c33aad6ef880203f01b56
Artur Paikin <artur@arturpaikin.com>
2019-04-19 09:47:18 +0300

    use newer OSes for tests

--- test/endtoend/wdio.remote.conf.js
+++ test/endtoend/wdio.remote.conf.js
@@ -12,12 +12,11 @@ function createCapability (capability) {
 
 exports.config = Object.assign(base.config, {
   capabilities: [
-    { browserName: 'firefox', version: '38.0', platform: 'Linux' },
+    { browserName: 'firefox', version: '40.0', platform: 'Linux' },
     { browserName: 'firefox', version: '61.0', platform: 'Windows 10' },
-    { browserName: 'internet explorer', version: '10.0', platform: 'Windows 7' },
-    { browserName: 'internet explorer', version: '11.0', platform: 'Windows 7' },
+    { browserName: 'internet explorer', version: '10.0', platform: 'Windows 8' },
+    { browserName: 'internet explorer', version: '11.0', platform: 'Windows 10' },
     { browserName: 'chrome', version: '70.0', platform: 'Windows 10' },
-    // { browserName: 'chrome', version: '69.0', platform: 'Windows 10' },
     { browserName: 'MicrosoftEdge', version: '14', platform: 'Windows 10' },
     { browserName: 'MicrosoftEdge', version: '17', platform: 'Windows 10' },
     // { browserName: 'safari', version: '11.0', platform: 'macOS 10.12' },
