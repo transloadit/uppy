23b78bf588c1950e7451a47d7b7d68eb34139cb5
tim-kos <tim@debuggable.com>
2019-04-23 14:03:08 +0200

    Adding initial version.

---
+++ packages/@uppy/locales/src/de_DE.js
@@ -0,0 +1,145 @@
+/* eslint camelcase: 0 */
+
+var de_DE = {};
+
+de_DE.strings = {
+  addMoreFiles: 'Dateien hinzufügen',
+  addingMoreFiles: 'Dateien hinzufügen',
+  allowAccessDescription: 'Um Bilder oder Videos mit Ihrer Kamera aufzunehmen, erlauben Sie dieser Website bitte Zugriff auf Ihre Kamera.',
+  allowAccessTitle: 'Bitte erlauben Sie Zugriff auf Ihre Kamera',
+  back: 'Zurück',
+  browse: 'durchblättern',
+  cancel: 'Abbrechen',
+  cancelUpload: 'Upload abbrechen',
+  chooseFiles: 'Dateien auswählen',
+  closeModal: 'Modal schließen',
+  companionAuthError: 'Authorisierung benötigt',
+  companionError: 'Verbindung zu Companion fehlgeschlagen',
+  complete: 'Fertig',
+  connectedToInternet: 'Verbunden mit dem Internet',
+  copyLink: 'Link kopieren',
+  copyLinkToClipboardFallback: 'Untenstehenden URL kopieren',
+  copyLinkToClipboardSuccess: 'Link in die Zwischenablage kopiert',
+  creatingAssembly: 'Upload vorbereiten...',
+  creatingAssemblyFailed: 'Transloadit: Konnte Assembly nicht erstellen',
+  dashboardTitle: 'Datei Uploader',
+  dashboardWindowTitle: 'Datei Uploader Fenster (Esc drücken zum Schließen)',
+  dataUploadedOfTotal: '%{complete} von %{total}',
+  done: 'Fertig',
+  dropHereOr: 'Ziehen Sie Dateien über Drag/Drop hierher oder %{browse}',
+  dropHint: 'Ziehen Sie Dateien über Drag/Drop hierher',
+  dropPaste: 'Ziehen Sie Dateien über Drag/Drop hierher, fügen Sie sie ein, oder %{browse}',
+  dropPasteImport: 'Ziehen Sie Dateien über Drag/Drop hierher, fügen Sie sie ein, %{browse} oder importieren Sie sie von',
+  edit: 'Bearbeiten',
+  editFile: 'Datei bearbeiten',
+  editing: '%{file} bearbeiten',
+  emptyFolderAdded: 'Keine Dateien wurden aus dem leeren Ordner hinzugefügt',
+  encoding: 'Enkodieren...',
+  enterCorrectUrl: 'Falsche URL: Bitte stellen Sie sicher, dass einen direkten Link zu einer Datei eingeben',
+  enterUrlToImport: 'URL zum Importieren einer Datei eingeben',
+  exceedsSize: 'Diese Datei ist größer als die maximal erlaubte Dateigröße',
+  failedToFetch: 'Companion konnte diese URL nicht verarbeiten - stellen Sie bitte sicher, dass sie korrekt ist',
+  failedToUpload: 'Fehler beim Upload von Datei %{file}',
+  fileSource: 'Dateiquelle: %{name}',
+  filesUploadedOfTotal: {
+    '0': '%{complete} von %{smart_count} Datei hochgeladen',
+    '1': '%{complete} von %{smart_count} Dateien hochgeladen',
+    '2': '%{complete} von %{smart_count} Dateien hochgeladen'
+  },
+  filter: 'Filter',
+  finishEditingFile: 'Dateibearbeitung beenden',
+  folderAdded: {
+    '0': '%{smart_count} Datei von %{folder} hinzugefügt',
+    '1': '%{smart_count} Dateien von %{folder} hinzugefügt',
+    '2': '%{smart_count} Dateien von %{folder} hinzugefügt'
+  },
+  import: 'Import',
+  importFrom: 'Importieren von %{name}',
+  link: 'Link',
+  loading: 'Laden...',
+  logOut: 'Ausloggen',
+  myDevice: 'Mein Gerät',
+  noFilesFound: 'Sie haben keine Dateien oder Ordner hier',
+  noInternetConnection: 'Keine Internetverbindung',
+  pause: 'Pausieren',
+  pauseUpload: 'Upload pausieren',
+  paused: 'Pausiert',
+  preparingUpload: 'Upload vorbereiten...',
+  processingXFiles: {
+    '0': '%{smart_count} Datei verarbeiten',
+    '1': '%{smart_count} Dateien verarbeiten',
+    '2': '%{smart_count} Dateien verarbeiten'
+  },
+  removeFile: 'Datei entfernen',
+  resetFilter: 'Filter zurücksetzen',
+  resume: 'Fortsetzen',
+  resumeUpload: 'Upload fortsetzen',
+  retry: 'Erneut versuchen',
+  retryUpload: 'Upload erneut versuchen',
+  saveChanges: 'Änderungen speichern',
+  selectXFiles: {
+    '0': '%{smart_count} Datei auswählen',
+    '1': '%{smart_count} Dateien auswählen',
+    '2': '%{smart_count} Dateien auswählen'
+  },
+  smile: 'Bitte lächeln!',
+  startRecording: 'Videoaufnahme starten',
+  stopRecording: 'Videoaufnahme beenden',
+  takePicture: 'Ein Foto machen',
+  timedOut: 'Upload für %{seconds} Sekunden stehen geblieben, breche ab.',
+  upload: 'Upload',
+  uploadComplete: 'Upload beendet',
+  uploadFailed: 'Upload fehlgeschlagen',
+  uploadPaused: 'Upload pausiert',
+  uploadXFiles: {
+    '0': '%{smart_count} Datei hochladen',
+    '1': '%{smart_count} Dateien hochladen',
+    '2': '%{smart_count} Dateien hochladen'
+  },
+  uploadXNewFiles: {
+    '0': '+%{smart_count} Datei hochladen',
+    '1': '+%{smart_count} Dateien hochladen',
+    '2': '+%{smart_count} Dateien hochladen'
+  },
+  uploading: 'Uploading',
+  uploadingXFiles: {
+    '0': '%{smart_count} Datei wird hochgeladen',
+    '1': '%{smart_count} Dateien werden hochgeladen',
+    '2': '%{smart_count} Dateien werden hochgeladen'
+  },
+  xFilesSelected: {
+    '0': '%{smart_count} Datei ausgewählt',
+    '1': '%{smart_count} Dateien ausgewählt',
+    '2': '%{smart_count} Dateien ausgewählt'
+  },
+  xMoreFilesAdded: {
+    '0': '%{smart_count} weitere Datei hinzugefügt',
+    '1': '%{smart_count} weitere Dateien hinzugefügt',
+    '2': '%{smart_count} weitere Dateien hinzugefügt'
+  },
+  xTimeLeft: '%{time} verbleibend',
+  youCanOnlyUploadFileTypes: 'Sie können nur folgende Dateitypen hochladen: %{types}',
+  youCanOnlyUploadX: {
+    '0': 'Sie können nur %{smart_count} Datei hochladen',
+    '1': 'Sie können nur %{smart_count} Dateien hochladen',
+    '2': 'Sie können nur %{smart_count} Dateien hochladen'
+  },
+  youHaveToAtLeastSelectX: {
+    '0': 'Sie müssen mindestens %{smart_count} Datei auswählen',
+    '1': 'Sie müssen mindestens %{smart_count} Dateien auswählen',
+    '2': 'Sie müssen mindestens %{smart_count} Dateien auswählen'
+  }
+};
+
+de_DE.pluralize = function (n) {
+  if (n === 1) {
+    return 0;
+  }
+  return 1;
+};
+
+if (typeof window !== 'undefined' && typeof window.Uppy !== 'undefined') {
+  window.Uppy.locales.de_DE = de_DE;
+}
+
+module.exports = de_DE;
