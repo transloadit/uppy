b424160feae3206df5832c738ba718fe3efa6b5b
kiloreux <kiloreux@gmail.com>
2019-04-24 00:12:46 +0100

    Added french language pack

---
+++ packages/@uppy/locales/src/fr_FR.js
@@ -0,0 +1,145 @@
+/* eslint camelcase: 0 */
+
+const fr_FR = {}
+
+fr_FR.strings = {
+  addMoreFiles: 'Ajouter d\'autres fichiers',
+  addingMoreFiles: 'En train d\'ajouter des fichiers',
+  allowAccessDescription: 'Pour prendre des photos ou enregistrer une vidéo avec votre caméra, veuillez autoriser l\'accès à votre caméra pour ce site.',
+  allowAccessTitle: 'Veuillez autoriser l\'accès à votre caméra',
+  back: 'Retour',
+  browse: 'naviguer',
+  cancel: 'Annuler',
+  cancelUpload: 'Annuler téléchargement',
+  chooseFiles: 'Choisir des fichiers',
+  closeModal: 'Fermer fenêtre',
+  companionAuthError: 'Autorisation requise',
+  companionError: 'Connexion avec Companion a échoué',
+  complete: 'Complete',
+  connectedToInternet: 'Connecté à Internet',
+  copyLink: 'Copier lien',
+  copyLinkToClipboardFallback: 'Copier le lien en dessous',
+  copyLinkToClipboardSuccess: 'Lien copié',
+  creatingAssembly: 'Préparation du téléchargement...',
+  creatingAssemblyFailed: 'Transloadit: Impossible de créer une Assembly',
+  dashboardTitle: 'Téléchargeur de fichier',
+  dashboardWindowTitle: 'Fenêtre de Téléchargeur de fichier (Appuyez sur echap pour fermer)',
+  dataUploadedOfTotal: '%{complete} sur %{total}',
+  done: 'Terminé',
+  dropHereOr: 'Déposez les fichiers ici ou %{browse}',
+  dropHint: 'Déposez vos fichiers ici',
+  dropPaste: 'Déposez les fichiers ici, coller ou %{browse}',
+  dropPasteImport: 'Déposez les fichiers ici, coller, %{browse} ou importer du',
+  edit: 'Modifier',
+  editFile: 'Modifier fichier',
+  editing: 'Modification en cours du %{file}',
+  emptyFolderAdded: 'Aucun fichier n\'a été ajouté depuis un dossier vide',
+  encoding: 'Traitement...',
+  enterCorrectUrl: 'Lien incorrecte: Assurez-vous que vous entrez un lien direct vers le fichier',
+  enterUrlToImport: 'Entrez un lien ou importer un fichier',
+  exceedsSize: 'Ce fichier dépasse la taille maximale autorisée de',
+  failedToFetch: 'Companion a échoué de récupérer ce lien, assurez-vous que c\'est correct',
+  failedToUpload: 'Le téléchargement du %{file} a échoué',
+  fileSource: 'Fichier source: %{name}',
+  filesUploadedOfTotal: {
+    '0': '%{complete} sur %{smart_count} fichier téléchargé',
+    '1': '%{complete} sur %{smart_count} fichiers téléchargés',
+    '2': '%{complete} sur %{smart_count} fichiers téléchargés'
+  },
+  filter: 'Filtrer',
+  finishEditingFile: 'Terminer l\'édition du fichier',
+  folderAdded: {
+    '0': '%{smart_count} ajouté fichier de %{folder}',
+    '1': '%{smart_count} ajouté fichiers de %{folder}',
+    '2': '%{smart_count} ajouté fichiers de %{folder}'
+  },
+  import: 'Importer',
+  importFrom: 'Importer du %{name}',
+  link: 'Lien',
+  loading: 'Chargement...',
+  logOut: 'Déconnexion',
+  myDevice: 'Mon appareil',
+  noFilesFound: 'Vous n\'avez aucun fichier ou dossier ici',
+  noInternetConnection: 'Pas de connexion Internet',
+  pause: 'Pause',
+  pauseUpload: 'Mettre en pause le téléchargement',
+  paused: 'En pause',
+  preparingUpload: 'Préparation du téléchargement...',
+  processingXFiles: {
+    '0': 'Traitement de %{smart_count} fichier',
+    '1': 'Traitement de %{smart_count} fichiers',
+    '2': 'Traitement de %{smart_count} fichiers'
+  },
+  removeFile: 'Effacer le fichier',
+  resetFilter: 'Réinitialiser filtre',
+  resume: 'Continuer',
+  resumeUpload: 'Continuer le téléchargement',
+  retry: 'Réessayer',
+  retryUpload: 'Réessayez le téléchargement',
+  saveChanges: 'Sauvegarder les modifications',
+  selectXFiles: {
+    '0': 'Sélectionner %{smart_count} fichier',
+    '1': 'Sélectionner %{smart_count} fichiers',
+    '2': 'Sélectionner %{smart_count} fichiers'
+  },
+  smile: 'Souris!',
+  startRecording: 'Commencer l\'enregistrement vidéo',
+  stopRecording: 'Arrêter l\'enregistrement vidéo',
+  takePicture: 'Prendre une photo',
+  timedOut: 'Téléchargement bloqué pour %{seconds} secondes, annulation.',
+  upload: 'Télécharger',
+  uploadComplete: 'Téléchargement terminé',
+  uploadFailed: 'Téléchargement a échoué',
+  uploadPaused: 'Téléchargement mis en pause',
+  uploadXFiles: {
+    '0': 'Télécharger %{smart_count} fichier',
+    '1': 'Télécharger %{smart_count} fichiers',
+    '2': 'Télécharger %{smart_count} fichiers'
+  },
+  uploadXNewFiles: {
+    '0': 'Télécharger +%{smart_count} fichier',
+    '1': 'Télécharger +%{smart_count} fichiers',
+    '2': 'Télécharger +%{smart_count} fichiers'
+  },
+  uploading: 'Téléchargement en cours',
+  uploadingXFiles: {
+    '0': 'Uploading %{smart_count} file',
+    '1': 'Uploading %{smart_count} files',
+    '2': 'Uploading %{smart_count} files'
+  },
+  xFilesSelected: {
+    '0': '%{smart_count} fichier sélectionné',
+    '1': '%{smart_count} fichiers sélectionnés',
+    '2': '%{smart_count} fichiers sélectionnés'
+  },
+  xMoreFilesAdded: {
+    '0': '%{smart_count} autre fichier ajouté',
+    '1': '%{smart_count} autres fichiers ajoutés',
+    '2': '%{smart_count} autres fichiers ajoutés'
+  },
+  xTimeLeft: '%{time} restantes',
+  youCanOnlyUploadFileTypes: 'Vous pouvez seulement télécharger: %{types}',
+  youCanOnlyUploadX: {
+    '0': 'Vous pouvez seulement télécharger %{smart_count} fichier',
+    '1': 'Vous pouvez seulement télécharger %{smart_count} fichiers',
+    '2': 'Vous pouvez seulement télécharger %{smart_count} fichiers'
+  },
+  youHaveToAtLeastSelectX: {
+    '0': 'Vous devez sélectionner au moins %{smart_count} fichier',
+    '1': 'Vous devez sélectionner au moins %{smart_count} fichiers',
+    '2': 'Vous devez sélectionner au moins %{smart_count} fichiers'
+  }
+}
+
+fr_FR.pluralize = function (n) {
+  if (n === 1) {
+    return 0
+  }
+  return 1
+}
+
+if (typeof window !== 'undefined' && typeof window.Uppy !== 'undefined') {
+  window.Uppy.locales.fr_FR = fr_FR
+}
+
+module.exports = fr_FR
