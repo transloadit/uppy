52012d036b1178892147e6045442dfcb29ed2779
bonopengate@gmail.com <bon@BonMBP.local>
2019-04-29 07:58:16 +0800

    Add zh_CN translation

---
+++ packages/@uppy/locales/src/zh_CN.js
@@ -0,0 +1,148 @@
+/* eslint camelcase: 0 */
+
+const zh_CN = {}
+
+zh_CN.strings = {
+  addMoreFiles: '添加更多文件',
+  addingMoreFiles: '正在添加更多文件',
+  allowAccessDescription: '为了通过您的相机进行拍照或录像，请给网站相机的访问权限',
+  allowAccessTitle: '请允许对相机的访问权限',
+  authenticateWith: '连接到%{pluginName}',
+  authenticateWithTitle: '请使用%{pluginName}进行身份验证以选择文件',
+  back: '返回',
+  browse: '浏览',
+  cancel: '取消',
+  cancelUpload: '取消上传',
+  chooseFiles: '选择文件',
+  closeModal: '关闭模态',
+  companionAuthError: '需要认证',
+  companionError: '与Companion的连接失败',
+  complete: '完成',
+  connectedToInternet: '连接至网络',
+  copyLink: '复制链接',
+  copyLinkToClipboardFallback: '复制以下网址',
+  copyLinkToClipboardSuccess: '链接已复制到剪贴板',
+  creatingAssembly: '准备上传中...',
+  creatingAssemblyFailed: 'Transloadit：无法创建程序集',
+  dashboardTitle: '文件上传工具',
+  dashboardWindowTitle: '文件上传工具窗口（点击离开以关闭）',
+  dataUploadedOfTotal: '%{total}%{complete}',
+  done: '完成',
+  dropHereOr: '拖动文件到这里或%{browse}',
+  dropHint: '拖动文件到这里',
+  dropPaste: '拖动文件到这里，粘贴或者%{browse}',
+  dropPasteImport: '拖动文件到这里，粘贴，%{browse}或者导入',
+  edit: '编辑',
+  editFile: '编辑文件',
+  editing: '正在编辑%{file}',
+  emptyFolderAdded: '无法从空文件夹添加文件',
+  encoding: '编码中...',
+  enterCorrectUrl: '错误链接： 请确认您输入的是文件的链接',
+  enterUrlToImport: '输入链接或者导入文件',
+  exceedsSize: '此文件超出允许的最大大小',
+  failedToFetch: 'Companion无法抓取此链接，请确保它是正确的',
+  failedToUpload: '上传%{file}失败',
+  fileSource: '文件源：%{name}',
+  filesUploadedOfTotal: {
+    '0': '%{smart_count}个文件上传%{complete}',
+    '1': '%{smart_count}个文件上传%{complete}',
+    '2': '%{smart_count}个文件上传%{complete}'
+  },
+  filter: '筛选器',
+  finishEditingFile: '完成文件编辑',
+  folderAdded: {
+    '0': '从%{folder}添加了%{smart_count}个文件',
+    '1': '从%{folder}添加了%{smart_count}个文件',
+    '2': '从%{folder}添加了%{smart_count}个文件'
+  },
+  import: '导入',
+  importFrom: '从%{name}导入',
+  link: '链接',
+  loading: '载入中...',
+  logOut: '登出',
+  myDevice: '我的设备',
+  noFilesFound: '这里空空如也',
+  noInternetConnection: '无法连接到网络',
+  pause: '暂停',
+  pauseUpload: '暂停上传',
+  paused: '已暂停',
+  poweredBy: '强力驱动于',
+  preparingUpload: '准备上传中...',
+  processingXFiles: {
+    '0': '%{smart_count}个文件处理中',
+    '1': '%{smart_count}个文件处理中',
+    '2': '%{smart_count}个文件处理中'
+  },
+  removeFile: '移除文件',
+  resetFilter: '重置筛选器',
+  resume: '恢复',
+  resumeUpload: '恢复上传',
+  retry: '重试',
+  retryUpload: '重试上传',
+  saveChanges: '保存变更',
+  selectXFiles: {
+    '0': '已选择%{smart_count}个文件',
+    '1': '已选择%{smart_count}个文件',
+    '2': '已选择%{smart_count}个文件'
+  },
+  smile: '笑！',
+  startRecording: '开始视频录制',
+  stopRecording: '停止视频录制',
+  takePicture: '拍照',
+  timedOut: '上传已经停滞不前%{seconds}秒，中止上传',
+  upload: '上传',
+  uploadComplete: '上传完成',
+  uploadFailed: '上传失败',
+  uploadPaused: '上传暂停',
+  uploadXFiles: {
+    '0': '上传%{smart_count}个文件',
+    '1': '上传%{smart_count}个文件',
+    '2': '上传%{smart_count}个文件'
+  },
+  uploadXNewFiles: {
+    '0': '新上传了%{smart_count}个文件',
+    '1': '新上传了%{smart_count}个文件',
+    '2': '新上传了%{smart_count}个文件'
+  },
+  uploading: '正在上传',
+  uploadingXFiles: {
+    '0': '正在上传%{smart_count}个文件',
+    '1': '正在上传%{smart_count}个文件',
+    '2': '正在上传%{smart_count}个文件'
+  },
+  xFilesSelected: {
+    '0': '%{smart_count}个文件被选中',
+    '1': '%{smart_count}个文件被选中',
+    '2': '%{smart_count}个文件被选中'
+  },
+  xMoreFilesAdded: {
+    '0': '又有%{smart_count}个文件被添加',
+    '1': '又有%{smart_count}个文件被添加',
+    '2': '又有%{smart_count}个文件被添加'
+  },
+  xTimeLeft: '还剩下%{time}',
+  youCanOnlyUploadFileTypes: '您只能上传这些文件类型：%{types}',
+  youCanOnlyUploadX: {
+    '0': '您只能上传%{smart_count}个文件',
+    '1': '您只能上传%{smart_count}个文件',
+    '2': '您只能上传%{smart_count}个文件'
+  },
+  youHaveToAtLeastSelectX: {
+    '0': '您至少要选择%{smart_count}个文件',
+    '1': '您至少要选择%{smart_count}个文件',
+    '2': '您至少要选择%{smart_count}个文件'
+  }
+}
+
+zh_CN.pluralize = function (n) {
+  if (n === 1) {
+    return 0
+  }
+  return 1
+}
+
+if (typeof window !== 'undefined' && typeof window.Uppy !== 'undefined') {
+  window.Uppy.locales.zh_CN = zh_CN
+}
+
+module.exports = zh_CN
