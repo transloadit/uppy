fd5a1e7bb0b7814e84affbbd23f0d5f536cd6a5f
Renée Kooi <renee@kooi.me>
2019-04-29 14:18:02 +0200

    robodog: actually support specifying Dashboard options

---
+++ packages/@uppy/robodog/src/addDashboardPlugin.js
@@ -0,0 +1,53 @@
+const Dashboard = require('@uppy/dashboard')
+
+const dashboardOptionNames = [
+  'metaFields',
+  'width',
+  'height',
+  'thumbnailWidth',
+  'showLinkToFileUploadResult',
+  'showProgressDetails',
+  'hideRetryButton',
+  'hidePauseResumeCancelButtons',
+  'hideProgressAfterFinish',
+  'note',
+  'disableStatusBar',
+  'disableInformer',
+  'disableThumbnailGenerator',
+  'showSelectedFiles'
+]
+
+const modalDashboardOptionNames = [
+  'trigger',
+  'closeModalOnClickOutside',
+  'closeAfterFinish',
+  'disablePageScrollWhenModalOpen',
+  'animateOpenClose',
+  'onRequestCloseModal',
+  'browserBackButtonClose'
+]
+
+function addDashboardPlugin (uppy, opts, overrideOpts) {
+  const dashboardOpts = {}
+  dashboardOptionNames.forEach((key) => {
+    if (opts.hasOwnProperty(key)) {
+      dashboardOpts[key] = opts[key]
+    }
+  })
+
+  const inline = overrideOpts.inline == null ? dashboardOpts.inline : overrideOpts.inline
+  if (inline === false) {
+    modalDashboardOptionNames.forEach((key) => {
+      if (opts.hasOwnProperty(key)) {
+        dashboardOpts[key] = opts[key]
+      }
+    })
+  }
+
+  uppy.use(Dashboard, {
+    ...dashboardOpts,
+    ...overrideOpts
+  })
+}
+
+module.exports = addDashboardPlugin
