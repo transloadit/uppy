6a1598a7df59e5aa4a7c83fd70c602ad986350e0
Kevin van Zonneveld <kevin@vanzonneveld.net>
2019-05-01 15:26:02 +0200

    Update packages/@uppy/locales/src/fa_IR.js

--- packages/@uppy/locales/src/fa_IR.js
+++ packages/@uppy/locales/src/fa_IR.js
@@ -1,147 +1,147 @@
 /* eslint camelcase: 0 */
 
-const fa_IR = {};
+const fa_IR = {}
 
 fa_IR.strings = {
-    addMoreFiles: 'افزودن فایل‌های بیشتر',
-    addingMoreFiles: 'درحال افزودن فایل‌ها',
-    allowAccessDescription: 'برای استفاده از دوربین جهت عکس و فیلمبرداری، لطفا به این سایت اجازه‌ی دسترسی به دوربین را بدهید',
-    allowAccessTitle: 'لطفا به دوربین اجازه‌ی دسترسی بدهید',
-    authenticateWith: 'در حال اتصال به %{pluginName}',
-    authenticateWithTitle: 'احراز هویت %{pluginName} برای انتخاب فایل ضروریست!',
-    back: 'بازگشت',
-    browse: 'از رایانه',
-    cancel: 'انصراف',
-    cancelUpload: 'لغو بارگذاری',
-    chooseFiles: 'انتخاب فایل',
-    closeModal: 'بستن پنجره',
-    companionAuthError: 'نیازمند تایید هویت',
-    companionError: 'Connection with Companion failed',
-    complete: 'کامل شد',
-    connectedToInternet: 'م به شبکه اینترنت متصل شد',
-    copyLink: 'کپی پیوند',
-    copyLinkToClipboardFallback: 'پیوند زیر را کپی کنید',
-    copyLinkToClipboardSuccess: 'پیوند به حافظه‌ی موقت منتقل شد',
-    creatingAssembly: 'درحال آماده سازی برای بارگذاری',
-    creatingAssemblyFailed: 'Transloadit: Could not create Assembly',
-    dashboardTitle: 'بارگذاری فایل',
-    dashboardWindowTitle: 'پنجره بارگذاری فایل. برای لغو کلید esc را بفشارید',
-    dataUploadedOfTotal: '%{complete} از %{total}',
-    done: 'انجام شد',
-    dropHereOr: 'فایل را بکشید و اینجا رها کنید یا  %{browse}',
-    dropHint: 'فایل‌ها را بکشید و اینجا رها کنید',
-    dropPaste: 'فایلها اینجا رها کنید، بچسبانید یا  %{browse}',
-    dropPasteImport: 'فایلها را اینجا رها کنید، بچسبانید یا %{browse} انتخاب کنید',
-    edit: 'ویرایش',
-    editFile: 'ویرایش فایل',
-    editing: 'در حال ویرایش %{file}',
-    emptyFolderAdded: 'از پوشه‌ی خالی هیچ فایلی افزوده نشد',
-    encoding: 'رمزگذاری...',
-    enterCorrectUrl: 'آدرس نامعتبر. لطفا مطمئن شوید که آدرس مستقیم به یک فایل را انتخاب کرده‌اید.',
-    enterUrlToImport: 'آدرس فایل را برای بارگذاری بنویسید',
-    exceedsSize: 'اندازه‌ی این فایل از حد مجاز بیشتر است!',
-    failedToFetch: 'Companion failed to fetch this URL, please make sure it’s correct',
-    failedToUpload: 'شکست در بارگذاری %{file}',
-    fileSource: 'منبع فایل: %{name}',
-    filesUploadedOfTotal: {
-        '0': '%{complete} از %{smart_count} فایل بارگذاری شد.',
-        '1': '%{complete} از %{smart_count} فایل بارگذاری شد.',
-        '2': '%{complete} از %{smart_count} فایل بارگذاری شد.'
-    },
-    filter: 'پالایش',
-    finishEditingFile: 'اتمام ویرایش فایل',
-    folderAdded: {
-        '0': '%{smart_count} فایل از %{folder} افزوده شد.',
-        '1': '%{smart_count} فایل از %{folder} افزوده شد.',
-        '2': '%{smart_count} فایل از %{folder} افزوده شد.'
-    },
-    import: 'واردکردن',
-    importFrom: 'واردکردن از %{name}',
-    link: 'پیوند',
-    loading: 'درحال بارگذاری',
-    logOut: 'خروج',
-    myDevice: 'دستگاه من',
-    noFilesFound: 'هیچ فایل یا پوشه‌ای اینجا ندارید',
-    noInternetConnection: 'عدم اتصال به اینترنت',
-    pause: 'توقف',
-    pauseUpload: 'توقف بارگذاری',
-    paused: 'متوقف شده',
-    poweredBy: 'قدرت گرفته از',
-    preparingUpload: 'درحال آماده سازی برای بارگذاری',
-    processingXFiles: {
-        '0': 'درحال پردازش %{smart_count} فایل',
-        '1': 'درحال پردازش %{smart_count} فایل',
-        '2': 'درحال پردازش %{smart_count} فایل'
-    },
-    removeFile: 'حذف فایل',
-    resetFilter: 'بازنشانی فیلتر',
-    resume: 'ادامه',
-    resumeUpload: 'ادامه بارگذاری',
-    retry: 'تلاش دوباره',
-    retryUpload: 'تلاش دوباره بارگذاری',
-    saveChanges: 'ذخیره‌ی تغییرات',
-    selectXFiles: {
-        '0': 'انتخاب %{smart_count} فایل',
-        '1': 'انتخاب %{smart_count} فایل',
-        '2': 'انتخاب %{smart_count} فایل'
-    },
-    smile: 'Smile!',
-    startRecording: 'آغاز تصویربرداری',
-    stopRecording: 'توقف تصویربرداری',
-    takePicture: 'عکس بگیرید',
-    timedOut: 'بارگذاری به مدت %{seconds} ثانیه متوقف شد, درحال متوقف کردن.',
-    upload: 'بارگذاری',
-    uploadComplete: 'بارگذاری انجام شد',
-    uploadFailed: 'بارگذاری شکست خورد',
-    uploadPaused: 'بارگذاری متوقف شد',
-    uploadXFiles: {
-        '0': 'بارگذاری %{smart_count} فایل',
-        '1': 'بارگذاری %{smart_count} فایل',
-        '2': 'بارگذاری %{smart_count} فایل'
-    },
-    uploadXNewFiles: {
-        '0': 'بارگذاری +%{smart_count} فایل',
-        '1': 'بارگذاری +%{smart_count} فایل',
-        '2': 'بارگذاری +%{smart_count} فایل'
-    },
-    uploading: 'بارگذاری',
-    uploadingXFiles: {
-        '0': 'بارگذاری %{smart_count} فایل',
-        '1': 'بارگذاری %{smart_count} فایل',
-        '2': 'بارگذاری %{smart_count} fiفایلles'
-    },
-    xFilesSelected: {
-        '0': '%{smart_count} فایل انتخاب شده',
-        '1': '%{smart_count} فایل انتخاب شده',
-        '2': '%{smart_count} فایل انتخاب شده'
-    },
-    xMoreFilesAdded: {
-        '0': '%{smart_count} فایل دیگر افزوده شد',
-        '1': '%{smart_count} فایل دیگر افزوده شد',
-        '2': '%{smart_count} فایل دیگر افزوده شد'
-    },
-    xTimeLeft: '%{time} left',
-    youCanOnlyUploadFileTypes: 'فایل‌های قابل قبول: %{types}',
-    youCanOnlyUploadX: {
-        '0': 'فقط می‌توانید %{smart_count} فایل انتخاب کنید',
-        '1': 'فقط می‌توانید %{smart_count} فایل انتخاب کنید',
-        '2': 'فقط می‌توانید %{smart_count} فایل انتخاب کنید'
-    },
-    youHaveToAtLeastSelectX: {
-        '0': 'می‌بایست حداقل %{smart_count} فایل انتخاب کنید',
-        '1': 'می‌بایست حداقل %{smart_count} فایل انتخاب کنید',
-        '2': 'می‌بایست حداقل %{smart_count} فایل انتخاب کنید'
-    }
-};
+  addMoreFiles: 'افزودن فایل‌های بیشتر',
+  addingMoreFiles: 'درحال افزودن فایل‌ها',
+  allowAccessDescription: 'برای استفاده از دوربین جهت عکس و فیلمبرداری، لطفا به این سایت اجازه‌ی دسترسی به دوربین را بدهید',
+  allowAccessTitle: 'لطفا به دوربین اجازه‌ی دسترسی بدهید',
+  authenticateWith: 'در حال اتصال به %{pluginName}',
+  authenticateWithTitle: 'احراز هویت %{pluginName} برای انتخاب فایل ضروریست!',
+  back: 'بازگشت',
+  browse: 'از رایانه',
+  cancel: 'انصراف',
+  cancelUpload: 'لغو بارگذاری',
+  chooseFiles: 'انتخاب فایل',
+  closeModal: 'بستن پنجره',
+  companionAuthError: 'نیازمند تایید هویت',
+  companionError: 'Connection with Companion failed',
+  complete: 'کامل شد',
+  connectedToInternet: 'م به شبکه اینترنت متصل شد',
+  copyLink: 'کپی پیوند',
+  copyLinkToClipboardFallback: 'پیوند زیر را کپی کنید',
+  copyLinkToClipboardSuccess: 'پیوند به حافظه‌ی موقت منتقل شد',
+  creatingAssembly: 'درحال آماده سازی برای بارگذاری',
+  creatingAssemblyFailed: 'Transloadit: Could not create Assembly',
+  dashboardTitle: 'بارگذاری فایل',
+  dashboardWindowTitle: 'پنجره بارگذاری فایل. برای لغو کلید esc را بفشارید',
+  dataUploadedOfTotal: '%{complete} از %{total}',
+  done: 'انجام شد',
+  dropHereOr: 'فایل را بکشید و اینجا رها کنید یا  %{browse}',
+  dropHint: 'فایل‌ها را بکشید و اینجا رها کنید',
+  dropPaste: 'فایلها اینجا رها کنید، بچسبانید یا  %{browse}',
+  dropPasteImport: 'فایلها را اینجا رها کنید، بچسبانید یا %{browse} انتخاب کنید',
+  edit: 'ویرایش',
+  editFile: 'ویرایش فایل',
+  editing: 'در حال ویرایش %{file}',
+  emptyFolderAdded: 'از پوشه‌ی خالی هیچ فایلی افزوده نشد',
+  encoding: 'رمزگذاری...',
+  enterCorrectUrl: 'آدرس نامعتبر. لطفا مطمئن شوید که آدرس مستقیم به یک فایل را انتخاب کرده‌اید.',
+  enterUrlToImport: 'آدرس فایل را برای بارگذاری بنویسید',
+  exceedsSize: 'اندازه‌ی این فایل از حد مجاز بیشتر است!',
+  failedToFetch: 'Companion failed to fetch this URL, please make sure it’s correct',
+  failedToUpload: 'شکست در بارگذاری %{file}',
+  fileSource: 'منبع فایل: %{name}',
+  filesUploadedOfTotal: {
+    '0': '%{complete} از %{smart_count} فایل بارگذاری شد.',
+    '1': '%{complete} از %{smart_count} فایل بارگذاری شد.',
+    '2': '%{complete} از %{smart_count} فایل بارگذاری شد.'
+  },
+  filter: 'پالایش',
+  finishEditingFile: 'اتمام ویرایش فایل',
+  folderAdded: {
+    '0': '%{smart_count} فایل از %{folder} افزوده شد.',
+    '1': '%{smart_count} فایل از %{folder} افزوده شد.',
+    '2': '%{smart_count} فایل از %{folder} افزوده شد.'
+  },
+  import: 'واردکردن',
+  importFrom: 'واردکردن از %{name}',
+  link: 'پیوند',
+  loading: 'درحال بارگذاری',
+  logOut: 'خروج',
+  myDevice: 'دستگاه من',
+  noFilesFound: 'هیچ فایل یا پوشه‌ای اینجا ندارید',
+  noInternetConnection: 'عدم اتصال به اینترنت',
+  pause: 'توقف',
+  pauseUpload: 'توقف بارگذاری',
+  paused: 'متوقف شده',
+  poweredBy: 'قدرت گرفته از',
+  preparingUpload: 'درحال آماده سازی برای بارگذاری',
+  processingXFiles: {
+    '0': 'درحال پردازش %{smart_count} فایل',
+    '1': 'درحال پردازش %{smart_count} فایل',
+    '2': 'درحال پردازش %{smart_count} فایل'
+  },
+  removeFile: 'حذف فایل',
+  resetFilter: 'بازنشانی فیلتر',
+  resume: 'ادامه',
+  resumeUpload: 'ادامه بارگذاری',
+  retry: 'تلاش دوباره',
+  retryUpload: 'تلاش دوباره بارگذاری',
+  saveChanges: 'ذخیره‌ی تغییرات',
+  selectXFiles: {
+    '0': 'انتخاب %{smart_count} فایل',
+    '1': 'انتخاب %{smart_count} فایل',
+    '2': 'انتخاب %{smart_count} فایل'
+  },
+  smile: 'Smile!',
+  startRecording: 'آغاز تصویربرداری',
+  stopRecording: 'توقف تصویربرداری',
+  takePicture: 'عکس بگیرید',
+  timedOut: 'بارگذاری به مدت %{seconds} ثانیه متوقف شد, درحال متوقف کردن.',
+  upload: 'بارگذاری',
+  uploadComplete: 'بارگذاری انجام شد',
+  uploadFailed: 'بارگذاری شکست خورد',
+  uploadPaused: 'بارگذاری متوقف شد',
+  uploadXFiles: {
+    '0': 'بارگذاری %{smart_count} فایل',
+    '1': 'بارگذاری %{smart_count} فایل',
+    '2': 'بارگذاری %{smart_count} فایل'
+  },
+  uploadXNewFiles: {
+    '0': 'بارگذاری +%{smart_count} فایل',
+    '1': 'بارگذاری +%{smart_count} فایل',
+    '2': 'بارگذاری +%{smart_count} فایل'
+  },
+  uploading: 'بارگذاری',
+  uploadingXFiles: {
+    '0': 'بارگذاری %{smart_count} فایل',
+    '1': 'بارگذاری %{smart_count} فایل',
+    '2': 'بارگذاری %{smart_count} fiفایلles'
+  },
+  xFilesSelected: {
+    '0': '%{smart_count} فایل انتخاب شده',
+    '1': '%{smart_count} فایل انتخاب شده',
+    '2': '%{smart_count} فایل انتخاب شده'
+  },
+  xMoreFilesAdded: {
+    '0': '%{smart_count} فایل دیگر افزوده شد',
+    '1': '%{smart_count} فایل دیگر افزوده شد',
+    '2': '%{smart_count} فایل دیگر افزوده شد'
+  },
+  xTimeLeft: '%{time} left',
+  youCanOnlyUploadFileTypes: 'فایل‌های قابل قبول: %{types}',
+  youCanOnlyUploadX: {
+    '0': 'فقط می‌توانید %{smart_count} فایل انتخاب کنید',
+    '1': 'فقط می‌توانید %{smart_count} فایل انتخاب کنید',
+    '2': 'فقط می‌توانید %{smart_count} فایل انتخاب کنید'
+  },
+  youHaveToAtLeastSelectX: {
+    '0': 'می‌بایست حداقل %{smart_count} فایل انتخاب کنید',
+    '1': 'می‌بایست حداقل %{smart_count} فایل انتخاب کنید',
+    '2': 'می‌بایست حداقل %{smart_count} فایل انتخاب کنید'
+  }
+}
 
 fa_IR.pluralize = function (n) {
-    if (n === 1) {
-        return 0;
-    }
-    return 1;
-};
+  if (n === 1) {
+    return 0
+  }
+  return 1
+}
 
 if (typeof window !== 'undefined' && typeof window.Uppy !== 'undefined') {
-    window.Uppy.locales.fa_IR = fa_IR;
+  window.Uppy.locales.fa_IR = fa_IR
 }
-module.exports = fa_IR;
+module.exports = fa_IR
