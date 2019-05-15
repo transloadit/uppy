71ff1ae0088cbd03361fc12d348011e0b2aa4c29
Artur Paikin <artur@arturpaikin.com>
2019-05-15 16:54:35 +0300

    trying with appiumVersion and iPhone 8

--- test/endtoend/wdio.remote.conf.js
+++ test/endtoend/wdio.remote.conf.js
@@ -20,7 +20,7 @@ exports.config = Object.assign(base.config, {
     { browserName: 'MicrosoftEdge', version: '14', platform: 'Windows 10' },
     { browserName: 'MicrosoftEdge', version: '17', platform: 'Windows 10' },
     // { browserName: 'safari', version: '11.0', platform: 'macOS 10.12' },
-    { browserName: 'Safari', platformName: 'iOS', platformVersion: '12.2', deviceOrientation: 'portrait', deviceName: 'iPhone 6 Simulator' },
+    { browserName: 'Safari', platformName: 'iOS', platformVersion: '12.2', deviceOrientation: 'portrait', deviceName: 'iPhone 8 Simulator', appiumVersion: '1.13.0' },
     { browserName: 'chrome', platformName: 'Android', platformVersion: '6.0', deviceOrientation: 'portrait', deviceName: 'Android Emulator' }
   ].map(createCapability),
 
