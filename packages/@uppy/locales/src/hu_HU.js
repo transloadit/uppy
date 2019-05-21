f135a9d905d70280504df589a39ab7307834eead
Viktor Nagy <viktor.nagy@gmail.com>
2019-05-21 21:34:33 +0200

    Added Hungarian translations

---
+++ packages/@uppy/locales/src/hu_HU.js
@@ -0,0 +1,145 @@
+/* eslint camelcase: 0 */
+
+const hu_HU = {}
+
+hu_HU.strings = {
+  addMoreFiles: 'További fájlok hozzáadása',
+  addingMoreFiles: 'További fájlok hozzáadása',
+  allowAccessDescription: 'A képek vagy videó felvételéhez, kérjük engedélyezze a kamera használatát ezen az oldalon.',
+  allowAccessTitle: 'Engedélyezze a kamera használatát',
+  authenticateWith: 'Kapcsolódás a %{pluginName}-val',
+  authenticateWithTitle: 'Kérjük lépjen be a %{pluginName}-ba a fájlok kiválasztásához',
+  back: 'Vissza',
+  browse: 'válasszon',
+  cancel: 'Mégse',
+  cancelUpload: 'Feltöltés megszakítása',
+  chooseFiles: 'Fájlok kiválasztása',
+  closeModal: 'Ablak bezárása',
+  companionAuthError: 'Bejelentkezés szükséges',
+  companionError: 'A Companion-hoz történő kapcsolódás nem sikerült',
+  complete: 'Kész',
+  connectedToInternet: 'Kapcsolódótt az Internethez',
+  copyLink: 'Link másolása',
+  copyLinkToClipboardFallback: 'Másolja ki az alábbi URL-t',
+  copyLinkToClipboardSuccess: 'Link a vágólapra másolva',
+  creatingAssembly: 'Feltöltés előkészítése...',
+  creatingAssemblyFailed: 'Transloadit: Nem sikerült létrehozni az Assembly-t',
+  dashboardTitle: 'Fájlfeltöltő',
+  dashboardWindowTitle: 'Fájlfeltöltő ablak (Escape a bezáráshoz)',
+  dataUploadedOfTotal: '%{complete} / %{total}',
+  done: 'Kész',
+  dropHereOr: 'Ejtse ide vagy %{browse} fájlt',
+  dropHint: 'Ejtse ide a fájlokat',
+  dropPaste: 'Ejtse vagy másolja ide a fájlokat, vagy %{browse}',
+  dropPasteImport: 'Ejtse vagy másolja ide a fájlokat, vagy %{browse} vagy importálja',
+  edit: 'Szerkesztés',
+  editFile: 'Fájl szerkesztése',
+  editing: '%{file} szerkesztése',
+  emptyFolderAdded: 'Az üres mappából nem kerültek fájlok hozzáadásra',
+  encoding: 'Kódolás...',
+  enterCorrectUrl: 'Érvénytelen URL: Bizonyosodjon meg róla, hogy egy fájlra mutató közvetlen linket ír be',
+  enterUrlToImport: 'Adjon meg egy URL-t a fájl importálásához',
+  exceedsSize: 'Ez a fájl meghaladja a maximális megengedett méretet',
+  failedToFetch: 'A Companion-nak nem sikerült az URL letöltése, győzödjön meg az URL helyességéről',
+  failedToUpload: '%{file}-t nem sikerült feltölteni',
+  fileSource: 'Fájl forrása: %{name}',
+  filesUploadedOfTotal: {
+    '0': 'A %{smart_count}-ból %{complete} fájl feltöltve',
+    '1': 'A %{smart_count}-ból %{complete} fájl feltöltve',
+    '2': 'A %{smart_count}-ból %{complete} fájl feltöltve'
+  },
+  filter: 'Szűrő',
+  finishEditingFile: 'Fájl szerkesztésének befejezése',
+  folderAdded: {
+    '0': 'A %{folder}-ból %{smart_count} fájl hozzáadva',
+    '1': 'A %{folder}-ból %{smart_count} fájl hozzáadva',
+    '2': 'A %{folder}-ból %{smart_count} fájl hozzáadva'
+  },
+  import: 'Importálás',
+  importFrom: 'Importálás innen: %{name}',
+  link: 'Link',
+  loading: 'Töltés...',
+  logOut: 'Kijelentkezés',
+  myDevice: 'Eszközöm',
+  noFilesFound: 'Nem találhatóak fájlok vagy könyvtárak',
+  noInternetConnection: 'Nincsen Internetkapcsolat',
+  pause: 'Szünet',
+  pauseUpload: 'Feltöltés szüneteltetése',
+  paused: 'Szüneteltetve',
+  poweredBy: 'Meghajtja az',
+  preparingUpload: 'Feltöltés előkészítése...',
+  processingXFiles: {
+    '0': '%{smart_count} fájl feldolgozása',
+    '1': '%{smart_count} fájl feldolgozása',
+    '2': '%{smart_count} fájl feldolgozása'
+  },
+  removeFile: 'Fájl törlése',
+  resetFilter: 'Szűrő visszaállítása',
+  resume: 'Folytatás',
+  resumeUpload: 'Feltöltés folytatása',
+  retry: 'Újra',
+  retryUpload: 'Próbálja újra a feltöltést',
+  saveChanges: 'Változtatások mentése',
+  selectXFiles: {
+    '0': '%{smart_count} fájl kiválasztva',
+    '1': '%{smart_count} fájl kiválasztva',
+    '2': '%{smart_count} fájl kiválasztva'
+  },
+  smile: 'Csíííz!',
+  startRecording: 'Videófeltével indul',
+  stopRecording: 'Videófelvétel megáll',
+  takePicture: 'Fénykép',
+  timedOut: 'A feltöltés elakadt %{seconds} másodpercig, a feltöltés leáll.',
+  upload: 'Feltöltés',
+  uploadComplete: 'A feltöltés kész',
+  uploadFailed: 'Sikertelen feltöltés',
+  uploadPaused: 'Szüneteltetett feltöltés',
+  uploadXFiles: {
+    '0': '%{smart_count} fájl feltöltése',
+    '1': '%{smart_count} fájl feltöltése',
+    '2': '%{smart_count} fájl feltöltése'
+  },
+  uploadXNewFiles: {
+    '0': '+%{smart_count} fájl feltöltése',
+    '1': '+%{smart_count} fájl feltöltése',
+    '2': '+%{smart_count} fájl feltöltése'
+  },
+  uploading: 'Feltölés',
+  uploadingXFiles: {
+    '0': '+%{smart_count} fájl feltöltése',
+    '1': '+%{smart_count} fájl feltöltése',
+    '2': '+%{smart_count} fájl feltöltése'
+  },
+  xFilesSelected: {
+    '0': '%{smart_count} fájl kiválasztva',
+    '1': '%{smart_count} fájl kiválasztva',
+    '2': '%{smart_count} fájl kiválasztva'
+  },
+  xMoreFilesAdded: {
+    '0': 'további %{smart_count} fájl hozzáadva',
+    '1': 'további %{smart_count} fájl hozzáadva',
+    '2': 'további %{smart_count} fájl hozzáadva'
+  },
+  xTimeLeft: '%{time} van hátra',
+  youCanOnlyUploadFileTypes: 'Feltölthető formátumok: %{types}',
+  youCanOnlyUploadX: {
+    '0': 'Csak %{smart_count} fájl tölthető fel',
+    '1': 'Csak %{smart_count} fájl tölthető fel',
+    '2': 'Csak %{smart_count} fájl tölthető fel'
+  },
+  youHaveToAtLeastSelectX: {
+    '0': 'Legalább %{smart_count} fájlt ki kell választania',
+    '1': 'Legalább %{smart_count} fájlt ki kell választania',
+    '2': 'Legalább %{smart_count} fájlt ki kell választania'
+  }
+}
+
+hu_HU.pluralize = function (n) {
+  return 0
+}
+
+if (typeof window !== 'undefined' && typeof window.Uppy !== 'undefined') {
+  window.Uppy.locales.hu_HU = hu_HU
+}
+
+module.exports = hu_HU
