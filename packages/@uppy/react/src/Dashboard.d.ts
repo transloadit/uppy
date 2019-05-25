275bb69f61d8a9a22722cde920d20e3b2ee3818e
Sagar Jain <sagar@zitto.com>
2019-05-25 12:43:09 +0530

    dashboard react component prop typings updated

--- packages/@uppy/react/src/Dashboard.d.ts
+++ packages/@uppy/react/src/Dashboard.d.ts
@@ -13,19 +13,27 @@ export interface DashboardProps {
   trigger?: string;
   width?: number;
   height?: number;
-  showProgressDetails?: boolean;
   showLinkToFileUploadResult?: boolean;
-  showSelectedFiles?: boolean;
+  showProgressDetails?: boolean;
   hideUploadButton?: boolean;
+  hideRetryButton?: boolean;
+  hidePauseResumeButton?: boolean;
+  hideCancelButton?: boolean;
   hideProgressAfterFinish?: boolean;
+  showSelectedFiles?: boolean;
   note?: string;
   metaFields?: Array<MetaField>;
+  closeModalOnClickOutside?: boolean;
+  closeAfterFinish?: boolean;
+  disablePageScrollWhenModalOpen?: boolean;
+  animateOpenClose?: boolean;
   proudlyDisplayPoweredByUppy?: boolean;
   disableStatusBar?: boolean;
   disableInformer?: boolean;
   disableThumbnailGenerator?: boolean;
   thumbnailWidth?: number;
   locale?: Locale;
+  replaceTargetContent?: boolean;
 }
 
 /**
