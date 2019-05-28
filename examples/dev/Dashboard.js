8e5dd526f6e4515b45d3b999bc2de8eba638c678
Evgenia Karunus <lakesare@gmail.com>
2019-05-28 07:06:48 +0500

    ~/examples/dev - made watchify only watch files once

--- examples/dev/Dashboard.js
+++ examples/dev/Dashboard.js
@@ -1,68 +1,70 @@
-const Uppy = require('./../../packages/@uppy/core/src')
-const Dashboard = require('./../../packages/@uppy/dashboard/src')
-const Instagram = require('./../../packages/@uppy/instagram/src')
-const Dropbox = require('./../../packages/@uppy/dropbox/src')
-const GoogleDrive = require('./../../packages/@uppy/google-drive/src')
-const Url = require('./../../packages/@uppy/url/src')
-const Webcam = require('./../../packages/@uppy/webcam/src')
-const Tus = require('./../../packages/@uppy/tus/src')
-// const XHRUpload = require('./../../packages/@uppy/xhr-upload/src')
-const Form = require('./../../packages/@uppy/form/src')
+const Uppy = require('@uppy/core/src')
+const Dashboard = require('@uppy/dashboard/src')
+const Instagram = require('@uppy/instagram/src')
+const Dropbox = require('@uppy/dropbox/src')
+const GoogleDrive = require('@uppy/google-drive/src')
+const Url = require('@uppy/url/src')
+const Webcam = require('@uppy/webcam/src')
+const Tus = require('@uppy/tus/src')
+// const XHRUpload = require('@uppy/xhr-upload/src')
+const Form = require('@uppy/form/src')
 
 const TUS_ENDPOINT = 'https://master.tus.io/files/'
 // const XHR_ENDPOINT = 'https://api2.transloadit.com'
 
-const uppyDashboard = Uppy({
-  debug: true,
-  meta: {
-    username: 'John',
-    license: 'Creative Commons'
-  }
-})
-  .use(Dashboard, {
-    trigger: '#pick-files',
-    // inline: true,
-    target: '.foo',
-    metaFields: [
-      { id: 'license', name: 'License', placeholder: 'specify license' },
-      { id: 'caption', name: 'Caption', placeholder: 'add caption' }
-    ],
-    showProgressDetails: true,
-    proudlyDisplayPoweredByUppy: true,
-    note: '2 files, images and video only'
+module.exports = () => {
+  const uppyDashboard = Uppy({
+    debug: true,
+    meta: {
+      username: 'John',
+      license: 'Creative Commons'
+    }
   })
-  .use(GoogleDrive, { target: Dashboard, companionUrl: 'http://localhost:3020' })
-  .use(Instagram, { target: Dashboard, companionUrl: 'http://localhost:3020' })
-  .use(Dropbox, { target: Dashboard, companionUrl: 'http://localhost:3020' })
-  .use(Url, { target: Dashboard, companionUrl: 'http://localhost:3020' })
-  .use(Webcam, { target: Dashboard })
-  .use(Tus, { endpoint: TUS_ENDPOINT })
-  // .use(XHRUpload, { endpoint: XHR_ENDPOINT })
-  .use(Form, { target: '#upload-form' })
-  // .use(GoldenRetriever, {serviceWorker: true})
+    .use(Dashboard, {
+      trigger: '#pick-files',
+      // inline: true,
+      target: '.foo',
+      metaFields: [
+        { id: 'license', name: 'License', placeholder: 'specify license' },
+        { id: 'caption', name: 'Caption', placeholder: 'add caption' }
+      ],
+      showProgressDetails: true,
+      proudlyDisplayPoweredByUppy: true,
+      note: '2 files, images and video only'
+    })
+    .use(GoogleDrive, { target: Dashboard, companionUrl: 'http://localhost:3020' })
+    .use(Instagram, { target: Dashboard, companionUrl: 'http://localhost:3020' })
+    .use(Dropbox, { target: Dashboard, companionUrl: 'http://localhost:3020' })
+    .use(Url, { target: Dashboard, companionUrl: 'http://localhost:3020' })
+    .use(Webcam, { target: Dashboard })
+    .use(Tus, { endpoint: TUS_ENDPOINT })
+    // .use(XHRUpload, { endpoint: XHR_ENDPOINT })
+    .use(Form, { target: '#upload-form' })
+    // .use(GoldenRetriever, {serviceWorker: true})
 
-uppyDashboard.on('complete', (result) => {
-  if (result.failed.length === 0) {
-    console.log('Upload successful 😀')
-  } else {
-    console.warn('Upload failed 😞')
+  uppyDashboard.on('complete', (result) => {
+    if (result.failed.length === 0) {
+      console.log('Upload successful 😀')
+    } else {
+      console.warn('Upload failed 😞')
+    }
+    console.log('successful files:', result.successful)
+    console.log('failed files:', result.failed)
+  })
+
+  const modalTrigger = document.querySelector('#pick-files')
+  if (modalTrigger) modalTrigger.click()
+
+  /* eslint-disable compat/compat */
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+      .register('/sw.js')
+      .then((registration) => {
+        console.log('ServiceWorker registration successful with scope: ', registration.scope)
+      })
+      .catch((error) => {
+        console.log('Registration failed with ' + error)
+      })
   }
-  console.log('successful files:', result.successful)
-  console.log('failed files:', result.failed)
-})
-
-const modalTrigger = document.querySelector('#pick-files')
-if (modalTrigger) modalTrigger.click()
-
-/* eslint-disable compat/compat */
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker
-    .register('/sw.js')
-    .then((registration) => {
-      console.log('ServiceWorker registration successful with scope: ', registration.scope)
-    })
-    .catch((error) => {
-      console.log('Registration failed with ' + error)
-    })
+  /* eslint-enable */
 }
-/* eslint-enable */
--- examples/dev/DragDrop.js
+++ examples/dev/DragDrop.js
@@ -1,25 +1,27 @@
-const Uppy = require('./../../packages/@uppy/core/src')
-const Tus = require('./../../packages/@uppy/tus/src')
+const Uppy = require('@uppy/core/src')
+const Tus = require('@uppy/tus/src')
 
-const DragDrop = require('./../../packages/@uppy/drag-drop/src')
-const ProgressBar = require('./../../packages/@uppy/progress-bar/src')
+const DragDrop = require('@uppy/drag-drop/src')
+const ProgressBar = require('@uppy/progress-bar/src')
 
-const uppyDragDrop = Uppy({
-  debug: true,
-  autoProceed: true
-})
-  .use(DragDrop, {
-    target: '#uppyDragDrop'
+module.exports = () => {
+  const uppyDragDrop = Uppy({
+    debug: true,
+    autoProceed: true
   })
-  .use(ProgressBar, { target: '#uppyDragDrop-progress', hideAfterFinish: false })
-  .use(Tus, { endpoint: 'https://master.tus.io/files/' })
+    .use(DragDrop, {
+      target: '#uppyDragDrop'
+    })
+    .use(ProgressBar, { target: '#uppyDragDrop-progress', hideAfterFinish: false })
+    .use(Tus, { endpoint: 'https://master.tus.io/files/' })
 
-uppyDragDrop.on('complete', (result) => {
-  if (result.failed.length === 0) {
-    console.log('Upload successful 😀')
-  } else {
-    console.warn('Upload failed 😞')
-  }
-  console.log('successful files:', result.successful)
-  console.log('failed files:', result.failed)
-})
+  uppyDragDrop.on('complete', (result) => {
+    if (result.failed.length === 0) {
+      console.log('Upload successful 😀')
+    } else {
+      console.warn('Upload failed 😞')
+    }
+    console.log('successful files:', result.successful)
+    console.log('failed files:', result.failed)
+  })
+}
