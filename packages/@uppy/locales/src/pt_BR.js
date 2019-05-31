f819d652714e598cf111b7a4280e7b2d5e5f38c6
Willy Camargo <camargowilly@gmail.com>
2019-05-31 12:23:16 -0300

    Added pt_BR translation

---
+++ packages/@uppy/locales/src/pt_BR.js
@@ -0,0 +1,148 @@
+/* eslint camelcase: 0 */
+
+const pt_BR = {}
+
+pt_BR.strings = {
+  addMoreFiles: 'Adicionar mais arquivos',
+  addingMoreFiles: 'Adicionando mais arquivos',
+  allowAccessDescription: 'Para poder tirar fotos e gravar vídeos com sua câmera, por favor permita o acesso a câmera para esse site.',
+  allowAccessTitle: 'Por favor permita o acesso a sua câmera',
+  authenticateWith: 'Conectar com %{pluginName}',
+  authenticateWithTitle: 'Por favor conecte com %{pluginName} para selecionar arquivos',
+  back: 'Voltar',
+  browse: 'procurar',
+  cancel: 'Cancelar',
+  cancelUpload: 'Cancelar envio de arquivos',
+  chooseFiles: 'Selecionar arquivos',
+  closeModal: 'Fechar Modal',
+  companionAuthError: 'Autorização necessária',
+  companionError: 'Conexão com serviço falhou',
+  complete: 'Complete',
+  connectedToInternet: 'Conectado á internet',
+  copyLink: 'Copiar link',
+  copyLinkToClipboardFallback: 'Copiar URL abaixo',
+  copyLinkToClipboardSuccess: 'Link copiado para a área de transferência',
+  creatingAssembly: 'Preparando envio de arquivos...',
+  creatingAssemblyFailed: 'Transloadit: Não foi possível criar o Assembly',
+  dashboardTitle: 'Envio de arquivos',
+  dashboardWindowTitle: 'Janela para envio de arquivos (Pressione esc para fechar)',
+  dataUploadedOfTotal: '%{complete} de %{total}',
+  done: 'Concluído',
+  dropHereOr: 'Arraste arquivos aqui ou %{browse}',
+  dropHint: 'Solte seus arquivos aqui',
+  dropPaste: 'Solte arquivos aqui, cole ou %{browse}',
+  dropPasteImport: 'Solte arquivos aqui, cole, %{browse} ou importe de',
+  edit: 'Editar',
+  editFile: 'Editar arquivo',
+  editing: 'Editando %{file}',
+  emptyFolderAdded: 'Nenhum arquivo foi adicionado da pasta vazia',
+  encoding: 'Codificando...',
+  enterCorrectUrl: 'URL incorreta: Por favor tenha certeza que inseriu um link direto para um arquivo',
+  enterUrlToImport: 'Coloque a URL para importar um arquivo',
+  exceedsSize: 'Esse arquivo excedeu o tamanho máximo permitido',
+  failedToFetch: 'Serviço falgou para buscar essa URL, por favor tenha certeza que a URL está correta',
+  failedToUpload: 'Falha para enviar %{file}',
+  fileSource: 'Origem do arquivo: %{name}',
+  filesUploadedOfTotal: {
+    '0': '%{complete} de %{smart_count} arquivo enviado',
+    '1': '%{complete} de %{smart_count} arquivos enviados',
+    '2': '%{complete} de %{smart_count} arquivos enviados'
+  },
+  filter: 'Filtrar',
+  finishEditingFile: 'Finalizar edição de arquivo',
+  folderAdded: {
+    '0': 'Adicionado %{smart_count} arquivo de %{folder}',
+    '1': 'Adicionado %{smart_count} arquivos de %{folder}',
+    '2': 'Adicionado %{smart_count} arquivos de %{folder}'
+  },
+  import: 'Importar',
+  importFrom: 'Importar de %{name}',
+  link: 'Link',
+  loading: 'Carregando...',
+  logOut: 'Deslogar',
+  myDevice: 'Meu dispositivo',
+  noFilesFound: 'Você não possui arquivos ou pastas aqui',
+  noInternetConnection: 'Sem conexão com a internet',
+  pause: 'Pausar',
+  pauseUpload: 'Pausar envio de arquivos',
+  paused: 'Pausado',
+  poweredBy: 'Desenvolvido por',
+  preparingUpload: 'Preparando envio de arquivos...',
+  processingXFiles: {
+    '0': 'Processando %{smart_count} arquivo',
+    '1': 'Processando %{smart_count} arquivos',
+    '2': 'Processando %{smart_count} arquivos'
+  },
+  removeFile: 'Remover arquivo',
+  resetFilter: 'Resetar filtro',
+  resume: 'Retomar',
+  resumeUpload: 'Retomar envio de arquivos',
+  retry: 'Tentar novamente',
+  retryUpload: 'Tentar enviar novamente',
+  saveChanges: 'Salvar alterações',
+  selectXFiles: {
+    '0': 'Selecionar %{smart_count} arquivo',
+    '1': 'Selecionar %{smart_count} arquivo',
+    '2': 'Selecionar %{smart_count} arquivos'
+  },
+  smile: 'Sorria!',
+  startRecording: 'Começar gravação de vídeo',
+  stopRecording: 'Parar gravação de vídeo',
+  takePicture: 'Tirar uma foto',
+  timedOut: 'Envio de arquivos parado por %{seconds} segundos, abortando.',
+  upload: 'Enviar arquivos',
+  uploadComplete: 'Envio de arquivos finalizado',
+  uploadFailed: 'Envio de arquivos falhou',
+  uploadPaused: 'Envio de arquivos pausado',
+  uploadXFiles: {
+    '0': 'Enviar %{smart_count} arquivo',
+    '1': 'Enviar %{smart_count} arquivo',
+    '2': 'Enviar %{smart_count} arquivos'
+  },
+  uploadXNewFiles: {
+    '0': 'Enviar +%{smart_count} arquivo',
+    '1': 'Enviar +%{smart_count} arquivos',
+    '2': 'Enviar +%{smart_count} arquivos'
+  },
+  uploading: 'Enviando',
+  uploadingXFiles: {
+    '0': 'Enviando %{smart_count} arquivo',
+    '1': 'Enviando %{smart_count} arquivos',
+    '2': 'Enviando %{smart_count} arquivos'
+  },
+  xFilesSelected: {
+    '0': '%{smart_count} arquivo selecionado',
+    '1': '%{smart_count} arquivos selecionados',
+    '2': '%{smart_count} arquivos selecionados'
+  },
+  xMoreFilesAdded: {
+    '0': '%{smart_count} arquivo adicionados',
+    '1': '%{smart_count} arquivos adicionados',
+    '2': '%{smart_count} arquivos adicionados'
+  },
+  xTimeLeft: '%{time} restantes',
+  youCanOnlyUploadFileTypes: 'Você só pode enviar aquivos: %{types}',
+  youCanOnlyUploadX: {
+    '0': 'Você só pode enviar %{smart_count} arquivo',
+    '1': 'Você só pode enviar %{smart_count} arquivos',
+    '2': 'Você só pode enviar %{smart_count} arquivos'
+  },
+  youHaveToAtLeastSelectX: {
+    '0': 'Você precisa selecionar pelo menos %{smart_count} arquivo',
+    '1': 'Você precisa selecionar pelo menos %{smart_count} arquivos',
+    '2': 'Você precisa selecionar pelo menos %{smart_count} arquivos'
+  }
+}
+
+pt_BR.pluralize = function (n) {
+  if (n === 1) {
+    return 0
+  }
+  return 1
+}
+
+if (typeof window !== 'undefined' && typeof window.Uppy !== 'undefined') {
+  window.Uppy.locales.pt_BR = pt_BR
+}
+
+module.exports = pt_BR
