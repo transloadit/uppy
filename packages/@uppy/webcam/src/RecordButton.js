468a37d4fc78eb886f219d64617b7866f67e5cc9
Evgenia Karunus <lakesare@gmail.com>
2019-06-13 15:49:01 +0500

    @uppy/dashboard - made all svgs not focusable in IE11

--- packages/@uppy/webcam/src/RecordButton.js
+++ packages/@uppy/webcam/src/RecordButton.js
@@ -10,7 +10,7 @@ module.exports = function RecordButton ({ recording, onStartRecording, onStopRec
         aria-label={i18n('stopRecording')}
         onclick={onStopRecording}
         data-uppy-super-focusable>
-        <svg aria-hidden="true" class="UppyIcon" width="100" height="100" viewBox="0 0 100 100">
+        <svg aria-hidden="true" focusable="false" class="UppyIcon" width="100" height="100" viewBox="0 0 100 100">
           <rect x="15" y="15" width="70" height="70" />
         </svg>
       </button>
@@ -24,7 +24,7 @@ module.exports = function RecordButton ({ recording, onStartRecording, onStopRec
       aria-label={i18n('startRecording')}
       onclick={onStartRecording}
       data-uppy-super-focusable>
-      <svg aria-hidden="true" class="UppyIcon" width="100" height="100" viewBox="0 0 100 100">
+      <svg aria-hidden="true" focusable="false" class="UppyIcon" width="100" height="100" viewBox="0 0 100 100">
         <circle cx="50" cy="50" r="40" />
       </svg>
     </button>
