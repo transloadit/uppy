aa2ddd17f455622a2ddc13e2f7b69289b28778e1
Renée Kooi <renee@kooi.me>
2019-06-17 15:24:22 +0200

    file-input: add update notice above chooseFiles default

--- packages/@uppy/file-input/src/index.js
+++ packages/@uppy/file-input/src/index.js
@@ -14,6 +14,9 @@ module.exports = class FileInput extends Plugin {
 
     this.defaultLocale = {
       strings: {
+        // The same key is used for the same purpose by @uppy/robodog's `form()` API, but our
+        // locale pack scripts can't access it in Robodog. If it is updated here, it should
+        // also be updated there!
         chooseFiles: 'Choose files'
       }
     }
