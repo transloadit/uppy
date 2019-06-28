3b4547fa2cacbc359e23b7ba6a0748610769798b
Alexander Zaytsev <nqst@users.noreply.github.com>
2019-06-28 15:20:04 +0300

    More design improvements (#1574)
    * Better focus styles
    
    * Improve focus on the remove button
    
    * "Add more" caption to the "+" button (todo: i18n)
    
    * Add i18n (todo: check)
    
    * Fix icon size
    
    * Try removing file source (todo: remove icons)
    
    * Simplify caption
    
    * Revert "Try removing file source (todo: remove icons)"
    
    This reverts commit 20c5ecb554dfab117c432ce660f0b2d423fd1d8c.
    
    * (WIP) Use icons for Edit / Copy link actions
    
    * Class names optimizing
    
    * Subtle shadows
    
    * Optimize code, align action icons on smaller screens
    
    * Reduce Tab-btn horizontal padding on mobile
    
    * Fix padding
    
    * i18n
    
    * replace arabic with persian
    
    * initial refactor
    
    * primarily FileItem's css - reintegrated Alex's changes
    
    * @uppy/dashboard - unnested subcomponents from ./components
    
    * @uppy/dashboard scss - added some spacing as per conventions
    
    * @uppy/dashboard - changed behaviour of truncateString() to always return a predictable number of symbols
    
    * @uppy/dashboard - subdivided FileItem.js and its css into smaller files
    
    Removed unused css classes, made filenames avoid overflow
    
    * @uppy/dashboard - removed unused .currentWidth prop, FileItem.js - made fileName take up at most 2 lines
    
    * test:endtoend:prepare-ci - fix test by reordering locale lines
    
    * @uppy/dashboard - decorative FileItem.js changes
    
    * @uppy/dashboard - made css of FileItem.js more flat
    
    * @uppy/Dashboard - in FileItem.js made max filename length 30 symbols
    
    * fix image getting width: 100% in DashboardFileCard
    
    //cc @lakesare
    
    * FileItem.scss - removed tagnames

---
+++ packages/@uppy/dashboard/src/components/FileItem/FileProgress/index.js
@@ -0,0 +1,83 @@
+const { h } = require('preact')
+const { iconRetry } = require('../../icons')
+const PauseResumeCancelIcon = require('./PauseResumeCancelIcon')
+
+function onPauseResumeCancelRetry (props) {
+  if (props.isUploaded) return
+
+  if (props.error && !props.hideRetryButton) {
+    props.retryUpload(props.file.id)
+    return
+  }
+
+  if (props.hidePauseResumeCancelButtons) {
+    return
+  }
+
+  if (props.resumableUploads) {
+    props.pauseUpload(props.file.id)
+  } else if (props.individualCancellation) {
+    props.cancelUpload(props.file.id)
+  }
+}
+
+function progressIndicatorTitle (props) {
+  if (props.isUploaded) {
+    return props.i18n('uploadComplete')
+  }
+
+  if (props.error) {
+    return props.i18n('retryUpload')
+  }
+
+  if (props.resumableUploads) {
+    if (props.file.isPaused) {
+      return props.i18n('resumeUpload')
+    }
+    return props.i18n('pauseUpload')
+  } else if (props.individualCancellation) {
+    return props.i18n('cancelUpload')
+  }
+
+  return ''
+}
+
+module.exports = function FileProgress (props) {
+  if (props.hideRetryButton && props.error) {
+    return <div class="uppy-DashboardItem-progress" />
+  } else if (
+    props.isUploaded ||
+    (props.hidePauseResumeCancelButtons && !props.error)
+  ) {
+    return (
+      <div class="uppy-DashboardItem-progress">
+        <div class="uppy-DashboardItem-progressIndicator">
+          <PauseResumeCancelIcon
+            progress={props.file.progress.percentage}
+            hidePauseResumeCancelButtons={props.hidePauseResumeCancelButtons}
+          />
+        </div>
+      </div>
+    )
+  } else {
+    return (
+      <div class="uppy-DashboardItem-progress">
+        <button
+          class="uppy-u-reset uppy-DashboardItem-progressIndicator"
+          type="button"
+          aria-label={progressIndicatorTitle(props)}
+          title={progressIndicatorTitle(props)}
+          onclick={() => onPauseResumeCancelRetry(props)}
+        >
+          {props.error
+            ? props.hideRetryButton ? null : iconRetry()
+            : <PauseResumeCancelIcon
+              progress={props.file.progress.percentage}
+              hidePauseResumeCancelButtons={props.hidePauseResumeCancelButtons}
+            />
+          }
+        </button>
+      </div>
+    )
+  }
+}
