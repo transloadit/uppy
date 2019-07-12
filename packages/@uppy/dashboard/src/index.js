bbc720f9b2351c72447e6518fec17e98dc1cb30f
Artur Paikin <artur@arturpaikin.com>
2019-07-12 17:54:29 +0200

    Allow re-initializing i18n locales after they’ve been updated with .setOptions

--- packages/@uppy/dashboard/src/index.js
+++ packages/@uppy/dashboard/src/index.js
@@ -131,10 +131,7 @@ module.exports = class Dashboard extends Plugin {
     // merge default options with the ones set by user
     this.opts = { ...defaultOptions, ...opts }
 
-    // i18n
-    this.translator = new Translator([ this.defaultLocale, this.uppy.locale, this.opts.locale ])
-    this.i18n = this.translator.translate.bind(this.translator)
-    this.i18nArray = this.translator.translateArray.bind(this.translator)
+    this.i18nInit()
 
     this.openModal = this.openModal.bind(this)
     this.closeModal = this.closeModal.bind(this)
@@ -173,6 +170,13 @@ module.exports = class Dashboard extends Plugin {
     // Timeouts
     this.makeDashboardInsidesVisibleAnywayTimeout = null
     this.removeDragOverClassTimeout = null
+  }
+
+  i18nInit () {
+    this.translator = new Translator([ this.defaultLocale, this.uppy.locale, this.opts.locale ])
+    this.i18n = this.translator.translate.bind(this.translator)
+    this.i18nArray = this.translator.translateArray.bind(this.translator)
+    this.setPluginState() // so that UI re-renders and we see the updated locale
   }
 
   removeTarget (plugin) {
