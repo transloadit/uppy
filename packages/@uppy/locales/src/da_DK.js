278e65d9623319b4292abc5d258e1c02704b0bb2
Pzoco <dan@thoeisen.dk>
2019-09-16 11:24:44 +0200

    Added Danish locale (#1837)
    * Added Danish locale
    
    * fix lint

---
+++ packages/@uppy/locales/src/da_DK.js
@@ -0,0 +1,152 @@
+const da_DK = {}
+
+da_DK.strings = {
+  addMore: 'Tilføj flere',
+  addMoreFiles: 'Tilføj flere filer',
+  addingMoreFiles: 'Tilføj flere filer',
+  allowAccessDescription: 'For at kunne tage billeder eller video med dit kamera, skal du tillade adgang til dit kamera for denne side.',
+  allowAccessTitle: 'Venligst giv adgang til dit kamera',
+  authenticateWith: 'Forbind til %{pluginName}',
+  authenticateWithTitle: 'Venligst autentificer med %{pluginName} for at vælge filer',
+  back: 'Tilbage',
+  browse: 'gennemse',
+  cancel: 'Annuller',
+  cancelUpload: 'Annuller upload',
+  chooseFiles: 'Vælg filer',
+  closeModal: 'Luk Modal',
+  companionAuthError: 'Autorisation påkrævet',
+  companionError: 'Forbindelse til Companion fejlede',
+  complete: 'Afsluttet',
+  connectedToInternet: 'Forbundet til internettet',
+  copyLink: 'Kopier link',
+  copyLinkToClipboardFallback: 'Kopier URLen forneden',
+  copyLinkToClipboardSuccess: 'Link kopieret til udklipsholderen',
+  creatingAssembly: 'Forbereder upload...',
+  creatingAssemblyFailed: 'Transloadit: Kunne ikke oprette Assembly',
+  dashboardTitle: 'Fil Uploader',
+  dashboardWindowTitle: 'Fil Uploader Vindue (Tryk escape for at lukke)',
+  dataUploadedOfTotal: '%{complete} af %{total}',
+  done: 'Færdig',
+  dropHereOr: 'Træk filer her eller %{browse}',
+  dropHint: 'Træk dine filer her',
+  dropPaste: 'Træk filer her, sæt ind eller %{browse}',
+  dropPasteImport: 'Træk filer her, sæt ind, %{browse} eller importer fra',
+  edit: 'Rediger',
+  editFile: 'Rediger fil',
+  editing: 'Redigerer %{file}',
+  emptyFolderAdded: 'Ingen filer blev tilføjet fra en tom mappe',
+  encoding: 'Encoding...',
+  enterCorrectUrl: 'Forkert URL: Venligst sørg for at du indtaster et direkte link til en fil',
+  enterUrlToImport: 'Indtast URL for at importerer en fil',
+  exceedsSize: 'Denne fil overskrider den maksimale tilladte størrelse af',
+  failedToFetch: 'Companion kunne ikke hente denne URL, venligst undersøg om denne er korrekt',
+  failedToUpload: 'Fejlede upload af %{file}',
+  fileSource: 'Fil kilde: %{name}',
+  filesUploadedOfTotal: {
+    '0': '%{complete} af %{smart_count} filer uploaded',
+    '1': '%{complete} af %{smart_count} fil uploaded',
+    '2': '%{complete} af %{smart_count} filer uploaded'
+  },
+  filter: 'Filter',
+  finishEditingFile: 'Færddiggør redigering af fil',
+  folderAdded: {
+    '0': 'Tilføjet %{smart_count} filer fra %{folder}',
+    '1': 'Tilføjet %{smart_count} fil fra %{folder}',
+    '2': 'Tilføjet %{smart_count} filer fra %{folder}'
+  },
+  import: 'Importer',
+  importFrom: 'Importer fra %{name}',
+  link: 'Link',
+  loading: 'Loading...',
+  logOut: 'Log ud',
+  myDevice: 'Min enhed',
+  noFilesFound: 'Du har ingen filer eller mapper her',
+  noInternetConnection: 'Ingen Internet forbindelse',
+  openFolderNamed: 'Åben mappe %{name}',
+  pause: 'Pause',
+  pauseUpload: 'Pause upload',
+  paused: 'Sat på pause',
+  poweredBy: 'Drevet af',
+  preparingUpload: 'Forbereder upload...',
+  processingXFiles: {
+    '0': 'Behandler %{smart_count} filer',
+    '1': 'Behandler %{smart_count} fil',
+    '2': 'Behandler %{smart_count} filer'
+  },
+  removeFile: 'Fjern fil',
+  resetFilter: 'Nulstil filter',
+  resume: 'Genoptag',
+  resumeUpload: 'Genoptag upload',
+  retry: 'Forsøg igen',
+  retryUpload: 'Forsøg upload igen',
+  saveChanges: 'Gem ændringer',
+  selectAllFilesFromFolderNamed: 'Vælg alle filer fra mappen %{name}',
+  selectFileNamed: 'Vælg fil %{name}',
+  selectX: {
+    '0': 'Vælg %{smart_count}',
+    '1': 'Vælg %{smart_count}',
+    '2': 'Vælg %{smart_count}'
+  },
+  smile: 'Smil!',
+  startRecording: 'Start video optagelse',
+  stopRecording: 'Stop video optagelse',
+  takePicture: 'Tag et billede',
+  timedOut: 'Upload gået i stå for %{seconds} sekunder, afbryder.',
+  unselectAllFilesFromFolderNamed: 'Afmarker alle filer fra mappen %{name}',
+  unselectFileNamed: 'Afmarker filen %{name}',
+  upload: 'Upload',
+  uploadComplete: 'Upload færdig',
+  uploadFailed: 'Upload fejlede',
+  uploadPaused: 'Upload sat på pause',
+  uploadXFiles: {
+    '0': 'Upload %{smart_count} fil',
+    '1': 'Upload %{smart_count} filer',
+    '2': 'Upload %{smart_count} filer'
+  },
+  uploadXNewFiles: {
+    '0': 'Upload +%{smart_count} fil',
+    '1': 'Upload +%{smart_count} filer',
+    '2': 'Upload +%{smart_count} filer'
+  },
+  uploading: 'Uploader',
+  uploadingXFiles: {
+    '0': 'Uploader %{smart_count} fil',
+    '1': 'Uploader %{smart_count} filer',
+    '2': 'Uploader %{smart_count} filer'
+  },
+  xFilesSelected: {
+    '0': '%{smart_count} fil valgt',
+    '1': '%{smart_count} filer valgt',
+    '2': '%{smart_count} filer valgt'
+  },
+  xMoreFilesAdded: {
+    '0': '%{smart_count} flere filer tilføjet',
+    '1': '%{smart_count} flere filer tilføjet',
+    '2': '%{smart_count} flere filer tilføjet'
+  },
+  xTimeLeft: '%{time} tilbage',
+  youCanOnlyUploadFileTypes: 'Du kan kun uploade: %{types}',
+  youCanOnlyUploadX: {
+    '0': 'Du kan kun uploade %{smart_count} fil',
+    '1': 'Du kan kun uploade %{smart_count} filer',
+    '2': 'Du kan kun uploade %{smart_count} filer'
+  },
+  youHaveToAtLeastSelectX: {
+    '0': 'Du skal vælge mindst %{smart_count} fil',
+    '1': 'Du skal vælge mindst %{smart_count} filer',
+    '2': 'Du skal vælge mindst %{smart_count} filer'
+  }
+}
+
+da_DK.pluralize = function (n) {
+  if (n === 1) {
+    return 0
+  }
+  return 1
+}
+
+if (typeof window !== 'undefined' && typeof window.Uppy !== 'undefined') {
+  window.Uppy.locales.da_DK = da_DK
+}
+
+module.exports = da_DK
