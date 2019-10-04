3ca83a1e529a8af76364d001afb13c4070aa70ae
Renée Kooi <renee@kooi.me>
2019-10-04 11:33:59 +0200

    url: manually fix lint errors

--- packages/@uppy/url/src/UrlUI.js
+++ packages/@uppy/url/src/UrlUI.js
@@ -22,24 +22,26 @@ class UrlUI extends Component {
   }
 
   render () {
-    return <div class="uppy-Url">
-      <input
-        class="uppy-u-reset uppy-c-textInput uppy-Url-input"
-        type="text"
-        aria-label={this.props.i18n('enterUrlToImport')}
-        placeholder={this.props.i18n('enterUrlToImport')}
-        onkeyup={this.handleKeyPress}
-        ref={(input) => { this.input = input }}
-        data-uppy-super-focusable
-      />
-      <button
-        class="uppy-u-reset uppy-c-btn uppy-c-btn-primary uppy-Url-importButton"
-        type="button"
-        onclick={this.handleClick}
-      >
-        {this.props.i18n('import')}
-      </button>
-    </div>
+    return (
+      <div class="uppy-Url">
+        <input
+          class="uppy-u-reset uppy-c-textInput uppy-Url-input"
+          type="text"
+          aria-label={this.props.i18n('enterUrlToImport')}
+          placeholder={this.props.i18n('enterUrlToImport')}
+          onkeyup={this.handleKeyPress}
+          ref={(input) => { this.input = input }}
+          data-uppy-super-focusable
+        />
+        <button
+          class="uppy-u-reset uppy-c-btn uppy-c-btn-primary uppy-Url-importButton"
+          type="button"
+          onclick={this.handleClick}
+        >
+          {this.props.i18n('import')}
+        </button>
+      </div>
+    )
   }
 }
 
