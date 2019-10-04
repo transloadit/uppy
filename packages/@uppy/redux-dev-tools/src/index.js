82057c4657667b52e59c673564c40a226379dab7
Renée Kooi <renee@kooi.me>
2019-10-04 11:54:16 +0200

    redux-dev-tools: manually fix lint errors

--- packages/@uppy/redux-dev-tools/src/index.js
+++ packages/@uppy/redux-dev-tools/src/index.js
@@ -40,11 +40,12 @@ module.exports = class ReduxDevTools extends Plugin {
           case 'RESET':
             this.uppy.reset()
             return
-          case 'IMPORT_STATE':
+          case 'IMPORT_STATE': {
             const computedStates = message.payload.nextLiftedState.computedStates
             this.uppy.store.state = Object.assign({}, this.uppy.getState(), computedStates[computedStates.length - 1].state)
             this.uppy.updateAll(this.uppy.getState())
             return
+          }
           case 'JUMP_TO_STATE':
           case 'JUMP_TO_ACTION':
             this.uppy.store.state = Object.assign({}, this.uppy.getState(), JSON.parse(message.state))
