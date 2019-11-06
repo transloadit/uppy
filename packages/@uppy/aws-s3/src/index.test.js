2318003d3728f8a2594f2436b79b693adb43d08a
Alexis <alexis@cvofhope.com>
2019-11-07 10:32:50 +1100

    test getUplaodParameters

--- packages/@uppy/aws-s3/src/index.test.js
+++ packages/@uppy/aws-s3/src/index.test.js
@@ -9,4 +9,14 @@ describe('AwsS3', () => {
     const pluginNames = core.plugins.uploader.map((plugin) => plugin.constructor.name)
     expect(pluginNames).toContain('AwsS3')
   })
+
+  describe('getUploadParameters', () => {
+    it('Throws an error if configured without companionUrl', () => {
+      const core = new Core()
+      core.use(AwsS3)
+      const awsS3 = core.getPlugin('AwsS3')
+
+      expect(awsS3.opts.getUploadParameters).toThrow()
+    })
+  })
 })
