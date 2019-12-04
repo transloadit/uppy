09c632b7ee1493137e43429335705dfb5934b7ea
Renée Kooi <renee@kooi.me>
2019-12-04 13:59:50 +0100

    Remove virtualization leftovers

--- packages/@uppy/dashboard/src/components/Dashboard.js
+++ packages/@uppy/dashboard/src/components/Dashboard.js
@@ -48,15 +48,6 @@ module.exports = function Dashboard (props) {
     'uppy-Dashboard--isInnerWrapVisible': props.areInsidesReadyToBeVisible
   })
 
-  let itemsPerRow = 1 // mobile
-  if (props.containerWidth > WIDTH_XL) {
-    itemsPerRow = 5
-  } else if (props.containerWidth > WIDTH_LG) {
-    itemsPerRow = 4
-  } else if (props.containerWidth > WIDTH_MD) {
-    itemsPerRow = 3
-  }
-
   const showFileList = props.showSelectedFiles && !noFiles
 
   return (
@@ -102,10 +93,7 @@ module.exports = function Dashboard (props) {
           {showFileList && <PanelTopBar {...props} />}
 
           {showFileList ? (
-            <FileList
-              {...props}
-              itemsPerRow={itemsPerRow}
-            />
+            <FileList {...props} />
           ) : (
             <AddFiles {...props} />
           )}
