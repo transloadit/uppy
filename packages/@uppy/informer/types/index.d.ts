c55ac59db1e7353f5507d4913487e94e65f3ce2c
Renée Kooi <renee@kooi.me>
2019-12-16 14:19:41 +0100

    informer: remove obsolete option from typings

--- packages/@uppy/informer/types/index.d.ts
+++ packages/@uppy/informer/types/index.d.ts
@@ -1,15 +1,9 @@
 import Uppy = require('@uppy/core')
 
 declare module Informer {
-  interface Color {
-    bg: string | number
-    text: string | number
-  }
-
   interface InformerOptions extends Uppy.PluginOptions {
     replaceTargetContent?: boolean
     target?: Uppy.PluginTarget
-    typeColors?: { [type: string]: Color }
   }
 }
 
