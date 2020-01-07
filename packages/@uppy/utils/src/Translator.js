7073658297061f5516b0bed4982bfba273884771
Artur Paikin <artur@arturpaikin.com>
2020-01-07 16:24:01 +0100

    Translator: support multiple components in options
    When the source contains multiple placeholders for interpolation, we should ignore chunks that are not strings, because those can be JSX objects and will be otherwise incorrectly turned into strings.
    
    Without this condition we’d get this: [object Object] hello [object Object] my <button>

--- packages/@uppy/utils/src/Translator.js
+++ packages/@uppy/utils/src/Translator.js
@@ -83,6 +83,14 @@ module.exports = class Translator {
     function insertReplacement (source, rx, replacement) {
       const newParts = []
       source.forEach((chunk) => {
+        // When the source contains multiple placeholders for interpolation,
+        // we should ignore chunks that are not strings, because those
+        // can be JSX objects and will be otherwise incorrectly turned into strings.
+        // Without this condition we’d get this: [object Object] hello [object Object] my <button>
+        if (typeof chunk !== 'string') {
+          return newParts.push(chunk)
+        }
+
         split.call(chunk, rx).forEach((raw, i, list) => {
           if (raw !== '') {
             newParts.push(raw)
