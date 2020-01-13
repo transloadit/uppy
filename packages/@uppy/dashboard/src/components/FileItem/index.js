1463ee79cea25aa623b69cc8027683f7b8b5f3c7
Renée Kooi <renee@kooi.me>
2020-01-13 12:29:08 +0100

    Improve performance of adding and removing files (#1949)
    * core: add an addFiles() method that only updates state once
    
    Previously, adding 1300-ish files took around 260ms, and looked like
    this in Firefox's performance tab:
    
    ![Flamegraph](https://i.imgur.com/08veuoU.png)
    
    All of the downward peaks are `setState()` calls and GC.
    
    Now it takes around 60ms, and looks like this:
    
    ![Flamegraph](https://i.imgur.com/xFdwVBV.png)
    
    Here, most of the time is spent generating file IDs and guessing file
    types. Those would be areas to look at next.
    
    * dashboard: prevent frequent state update if nothing changed
    
    After the last commit, `addFiles()` still spends a lot of time in
    `emit()` and `hideAllPanels()`. The reason is that `addFiles()` still
    emits all the hundreds of file-added events, and the Dashboard responds
    to each with `hideAllPanels()`, which does a state update. But this all
    happens synchronously, and the state almost certainly did not change
    since the last `file-added` event that fired a millisecond ago.
    
    This adds a check to avoid the state update if it is not necessary.
    
    ![Flamegraph](https://i.imgur.com/KhuD035.png)
    
    Adding 1300 files takes about 40ms now.
    
    With this change, the `addFiles()` call is no longer the slowest
    part—now preact rendering all the items is!
    
    * utils: optimize generateFileID and getFileNameAndExtension
    
    Replaces some clever things with more mundane and faster things!
    
    Now, generateFileID is a bunch of string concatenations.
    Now, getFileNameAndExtension uses `lastIndexOf()` instead of a regex.
    
    ![Flamegraph](https://i.imgur.com/ZQ1IhxI.png)
    
    Adding 1300 files takes about 25ms.
    
    * dashboard: use preact-virtual-list
    
    * thumbnail-generator: add `lazy` option
    
    * dashboard: request thumbnails once file item renders the first time
    
    * dashboard: fork preact-virtual-list
    
    * core: add removeFiles() to remove files in bulk
    
    * Implement removeFile() in terms of removeFiles()
    
    * thumbnail-generator: only queue files that can be previewed
    
    * rename size constants to accommodate WIDTH/HEIGHT
    
    * Use new uppy.addFiles in DragDrop and FileInput
    
    * utils: fix getFileNameAndExtension() type
    
    * Rip out the lazy thumbnail generation
    
    * Rip out virtualization.
    
    * Remove virtualization leftovers
    
    * tell future people that this is intentionally verbose
    
    * Update package-lock.json
    
    * henlo i am spell
    
    * Make `addFiles()` respect maxNumberOfFiles
    
    * core: show an informer error if some files fail in bulk add
    
    * locales: fix quotes to make build:locale-pack happy
    
    Co-authored-by: Artur Paikin <artur@arturpaikin.com>

--- packages/@uppy/dashboard/src/components/FileItem/index.js
+++ packages/@uppy/dashboard/src/components/FileItem/index.js
@@ -1,76 +1,82 @@
-const { h } = require('preact')
+const { h, Component } = require('preact')
 const classNames = require('classnames')
-const pure = require('../../utils/pure')
+const shallowEqual = require('is-shallow-equal')
 const FilePreviewAndLink = require('./FilePreviewAndLink')
 const FileProgress = require('./FileProgress')
 const FileInfo = require('./FileInfo')
 const Buttons = require('./Buttons')
 
-module.exports = pure(function FileItem (props) {
-  const file = props.file
+module.exports = class FileItem extends Component {
+  shouldComponentUpdate (nextProps) {
+    return !shallowEqual(this.props, nextProps)
+  }
 
-  const isProcessing = file.progress.preprocess || file.progress.postprocess
-  const isUploaded = file.progress.uploadComplete && !isProcessing && !file.error
-  const uploadInProgressOrComplete = file.progress.uploadStarted || isProcessing
-  const uploadInProgress = (file.progress.uploadStarted && !file.progress.uploadComplete) || isProcessing
-  const isPaused = file.isPaused || false
-  const error = file.error || false
+  render () {
+    const file = this.props.file
 
-  const showRemoveButton = props.individualCancellation
-    ? !isUploaded
-    : !uploadInProgress && !isUploaded
+    const isProcessing = file.progress.preprocess || file.progress.postprocess
+    const isUploaded = file.progress.uploadComplete && !isProcessing && !file.error
+    const uploadInProgressOrComplete = file.progress.uploadStarted || isProcessing
+    const uploadInProgress = (file.progress.uploadStarted && !file.progress.uploadComplete) || isProcessing
+    const isPaused = file.isPaused || false
+    const error = file.error || false
 
-  const dashboardItemClass = classNames(
-    'uppy-u-reset',
-    'uppy-DashboardItem',
-    { 'is-inprogress': uploadInProgress },
-    { 'is-processing': isProcessing },
-    { 'is-complete': isUploaded },
-    { 'is-paused': isPaused },
-    { 'is-error': !!error },
-    { 'is-resumable': props.resumableUploads },
-    { 'is-noIndividualCancellation': !props.individualCancellation }
-  )
+    const showRemoveButton = this.props.individualCancellation
+      ? !isUploaded
+      : !uploadInProgress && !isUploaded
 
-  return (
-    <li class={dashboardItemClass} id={`uppy_${file.id}`}>
-      <div class="uppy-DashboardItem-preview">
-        <FilePreviewAndLink
-          file={file}
-          showLinkToFileUploadResult={props.showLinkToFileUploadResult}
-        />
-        <FileProgress
-          {...props}
-          file={file}
-          error={error}
-          isUploaded={isUploaded}
-        />
-      </div>
+    const dashboardItemClass = classNames({
+      'uppy-u-reset': true,
+      'uppy-DashboardItem': true,
+      'is-inprogress': uploadInProgress,
+      'is-processing': isProcessing,
+      'is-complete': isUploaded,
+      'is-paused': isPaused,
+      'is-error': !!error,
+      'is-resumable': this.props.resumableUploads,
+      'is-noIndividualCancellation': !this.props.individualCancellation
+    })
 
-      <div class="uppy-DashboardItem-fileInfoAndButtons">
-        <FileInfo
-          file={file}
-          id={props.id}
-          acquirers={props.acquirers}
-          containerWidth={props.containerWidth}
-          i18n={props.i18n}
-        />
-        <Buttons
-          file={file}
-          metaFields={props.metaFields}
+    return (
+      <li class={dashboardItemClass} id={`uppy_${file.id}`}>
+        <div class="uppy-DashboardItem-preview">
+          <FilePreviewAndLink
+            file={file}
+            showLinkToFileUploadResult={this.props.showLinkToFileUploadResult}
+          />
+          <FileProgress
+            {...this.props}
+            file={file}
+            error={error}
+            isUploaded={isUploaded}
+          />
+        </div>
 
-          showLinkToFileUploadResult={props.showLinkToFileUploadResult}
-          showRemoveButton={showRemoveButton}
+        <div class="uppy-DashboardItem-fileInfoAndButtons">
+          <FileInfo
+            file={file}
+            id={this.props.id}
+            acquirers={this.props.acquirers}
+            containerWidth={this.props.containerWidth}
+            i18n={this.props.i18n}
+          />
+          <Buttons
+            file={file}
+            metaFields={this.props.metaFields}
 
-          uploadInProgressOrComplete={uploadInProgressOrComplete}
-          removeFile={props.removeFile}
-          toggleFileCard={props.toggleFileCard}
+            showLinkToFileUploadResult={this.props.showLinkToFileUploadResult}
+            showRemoveButton={showRemoveButton}
 
-          i18n={props.i18n}
-          log={props.log}
-          info={props.info}
-        />
-      </div>
-    </li>
-  )
-})
+            uploadInProgressOrComplete={uploadInProgressOrComplete}
+            removeFile={this.props.removeFile}
+            toggleFileCard={this.props.toggleFileCard}
+
+            i18n={this.props.i18n}
+            log={this.props.log}
+            info={this.props.info}
+          />
+        </div>
+      </li>
+    )
+  }
+}
