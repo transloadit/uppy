f291688fb813c55ff905abb334eff61c1c5a9dd0
Artur Paikin <artur@arturpaikin.com>
2020-02-11 20:16:32 +0100

    Add OneDrive to uppy bundle

--- packages/uppy/index.mjs
+++ packages/uppy/index.mjs
@@ -22,6 +22,7 @@ export { default as StatusBar } from '@uppy/status-bar'
 export { default as Dropbox } from '@uppy/dropbox'
 export { default as GoogleDrive } from '@uppy/google-drive'
 export { default as Instagram } from '@uppy/instagram'
+export { default as OneDrive } from '@uppy/onedrive'
 export { default as Url } from '@uppy/url'
 export { default as Webcam } from '@uppy/webcam'
 
