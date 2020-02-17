385201dd5749d882c7e40fc2f9c43e54db474035
Artur Paikin <artur@arturpaikin.com>
2020-02-17 16:36:40 +0100

    use execa

--- bin/update-contributors.js
+++ bin/update-contributors.js
@@ -1,17 +1,22 @@
-const { execSync } = require('child_process')
+const execa = require('execa')
 const fs = require('fs')
 
 const README_FILE_NAME = 'README.md'
 
-const readme = fs.readFileSync(README_FILE_NAME, 'utf-8')
-const listOfContributors = execSync(
-  './node_modules/.bin/githubcontrib --owner transloadit --repo uppy --cols 6 $([ \"${GITHUB_TOKEN:-}\" = \"\" ] && echo \"\" || echo \"--authToken ${GITHUB_TOKEN:-}\") --showlogin true --sortOrder desc', // eslint-disable-line
-  { encoding: 'utf-8' }
-)
+async function updateContributorsListInReadme () {
+  const readme = fs.readFileSync(README_FILE_NAME, 'utf-8')
+  const args = [
+    '--owner', 'transloadit',
+    '--repo', 'uppy',
+    '--cols', '6',
+    '--authToken', process.env.GITHUB_TOKEN ? `--authToken ${process.env.GITHUB_TOKEN}` : ''
+  ]
+  const { stdout } = await execa('githubcontrib', args) // eslint-disable-line
+  const readmeWithUpdatedContributors = readme.replace(
+    /<!--contributors-->[\s\S]+<!--\/contributors-->/,
+    `<!--contributors-->\n${stdout}\n<!--/contributors-->`
+  )
+  fs.writeFileSync(README_FILE_NAME, readmeWithUpdatedContributors)
+}
 
-const readmeWithUpdatedContributors = readme.replace(
-  /<!--contributors-->[\s\S]+<!--\/contributors-->/,
-  `<!--contributors-->\n${listOfContributors}\n<!--/contributors-->`
-)
-
-fs.writeFileSync(README_FILE_NAME, readmeWithUpdatedContributors)
+updateContributorsListInReadme()
