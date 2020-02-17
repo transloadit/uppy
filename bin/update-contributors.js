9ba6b7352075be9558487070ed90b87e8c6019fb
Artur Paikin <artur@arturpaikin.com>
2020-02-17 17:47:20 +0100

    yes, it’s not pretty, but it works?

--- bin/update-contributors.js
+++ bin/update-contributors.js
@@ -1,17 +1,33 @@
-const execa = require('execa')
+const { execSync } = require('child_process')
 const fs = require('fs')
 
 const README_FILE_NAME = 'README.md'
 
 async function updateContributorsListInReadme () {
   const readme = fs.readFileSync(README_FILE_NAME, 'utf-8')
-  const args = [
-    '--owner', 'transloadit',
-    '--repo', 'uppy',
-    '--cols', '6',
-    '--authToken', process.env.GITHUB_TOKEN ? `--authToken ${process.env.GITHUB_TOKEN}` : ''
-  ]
-  const { stdout } = await execa('githubcontrib', args) // eslint-disable-line
+  // const args = [
+  //   '--owner', 'transloadit',
+  //   '--repo', 'uppy',
+  //   '--cols', '6',
+  //   '--format', 'md',
+  //   '--showLogin', 'true',
+  //   '--sortOrder', 'desc'
+  // ]
+
+  // if (process.env.GITHUB_TOKEN) {
+  //   args.push('--authToken', process.env.GITHUB_TOKEN)
+  // }
+
+  // const stdout = spawnSync('./node_modules/.bin/githubcontrib', args, { encoding: 'utf-8' }).stdout
+  const stdout = execSync(
+    './node_modules/.bin/githubcontrib --owner transloadit --repo uppy --cols 6 $([ \"${GITHUB_TOKEN:-}\" = \"\" ] && echo \"\" || echo \"--authToken ${GITHUB_TOKEN:-}\") --showlogin true --sortOrder desc', // eslint-disable-line
+    { encoding: 'utf-8' }
+  )
+  console.log(stdout)
+  if (stdout === '' || stdout === null) {
+    console.log('Empty response from githubcontrib. GitHub’s rate limit?')
+    return
+  }
   const readmeWithUpdatedContributors = readme.replace(
     /<!--contributors-->[\s\S]+<!--\/contributors-->/,
     `<!--contributors-->\n${stdout}\n<!--/contributors-->`
