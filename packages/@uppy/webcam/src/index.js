b7ddc49c34a524320ed9b4022f58ebbc9b7b71cb
Tsy-Jon Lau <tlau@parsys.com>
2020-02-24 18:50:39 +0100

    webcam: update video device list when adding/removing a video device

--- packages/@uppy/webcam/src/index.js
+++ packages/@uppy/webcam/src/index.js
@@ -488,7 +488,9 @@ module.exports = class Webcam extends Plugin {
   }
 
   getVideoSources () {
+    this.videoSources = []
     this.mediaDevices.enumerateDevices().then(res => {
+      this.videoSources = []
       res.forEach((device) => {
         if (device.kind === 'videoinput') {
           this.videoSources.push(device)
@@ -549,6 +551,25 @@ module.exports = class Webcam extends Plugin {
     }
 
     this.getVideoSources()
+
+    this.mediaDevices.ondevicechange = (event) => {
+      this.getVideoSources()
+
+      if (this.stream) {
+        let restartStream = true
+
+        this.videoSources.forEach((videoSource) => {
+          if (this.currentDeviceId === videoSource.deviceId) {
+            restartStream = false
+          }
+        })
+
+        if (restartStream) {
+          this._stop()
+          this._start()
+        }
+      }
+    }
   }
 
   uninstall () {
