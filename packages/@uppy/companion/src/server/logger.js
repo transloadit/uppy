81f61d3c3399b1ddf2e2496cd4e2bb4e81b50b41
Ifedapo .A. Olarewaju <ifedapoolarewaju@gmail.com>
2020-04-30 10:37:31 +0100

    companion: mask secrets present in log messages (#2214)
    * companion: mask secrets present in log messages
    
    * companion: escape secrets that should be masked
    
    * companion: log error stacks optionally
    
    * companion: restore old node enging compatibility
    
    * companion: refactor conditional error response

--- packages/@uppy/companion/src/server/logger.js
+++ packages/@uppy/companion/src/server/logger.js
@@ -1,4 +1,19 @@
 const chalk = require('chalk')
+const escapeStringRegexp = require('escape-string-regexp')
+
+const valuesToMask = []
+/**
+ * Adds a list of strings that should be masked by the logger.
+ * This function can only be called once through out the life of the server.
+ * @param {Array} maskables a list of strings to be masked
+ */
+exports.setMaskables = (maskables) => {
+  maskables.forEach((i) => {
+    valuesToMask.push(escapeStringRegexp(i))
+  })
+
+  Object.freeze(valuesToMask)
+}
 
 /**
  * INFO level log
@@ -26,10 +41,11 @@ exports.warn = (msg, tag, traceId) => {
  * @param {string | Error} msg the message to log
  * @param {string=} tag a unique tag to easily search for this message
  * @param {string=} traceId a unique id to easily trace logs tied to a request
+ * @param {boolean=} shouldLogStackTrace when set to true, errors will be logged with their stack trace
  */
-exports.error = (msg, tag, traceId) => {
+exports.error = (msg, tag, traceId, shouldLogStackTrace) => {
   // @ts-ignore
-  log(msg, tag, 'error', traceId, chalk.bold.red)
+  log(msg, tag, 'error', traceId, chalk.bold.red, shouldLogStackTrace)
 }
 
 /**
@@ -51,14 +67,42 @@ exports.debug = (msg, tag, traceId) => {
  * @param {string} level error | info | debug
  * @param {function=} color function to display the log in appropriate color
  * @param {string=} id a unique id to easily trace logs tied to a request
+ * @param {boolean=} shouldLogStackTrace when set to true, errors will be logged with their stack trace
  */
-const log = (msg, tag, level, id, color) => {
+const log = (msg, tag, level, id, color, shouldLogStackTrace) => {
   const time = new Date().toISOString()
   tag = tag || ''
   id = id || ''
   const whitespace = tag && id ? ' ' : ''
   color = color || ((message) => message)
+  if (typeof msg === 'string') {
+    msg = maskMessage(msg)
+  } else if (msg && typeof msg.message === 'string') {
+    msg.message = maskMessage(msg.message)
+  }
+
+  if (shouldLogStackTrace && msg instanceof Error && typeof msg.stack === 'string') {
+    msg.stack = maskMessage(msg.stack)
+    // exclude msg from template string so values such as error objects
+    // can be well formatted
+    console.log(color(`companion: ${time} [${level}] ${id}${whitespace}${tag}`), color(msg.stack))
+    return
+  }
+
   // exclude msg from template string so values such as error objects
   // can be well formatted
   console.log(color(`companion: ${time} [${level}] ${id}${whitespace}${tag}`), color(msg))
+}
+
+/**
+ * Mask the secret content of a message
+ * @param {string} msg the message whose content should be masked
+ * @returns {string}
+ */
+const maskMessage = (msg) => {
+  for (const toBeMasked of valuesToMask) {
+    const toBeReplaced = new RegExp(toBeMasked, 'gi')
+    msg = msg.replace(toBeReplaced, '******')
+  }
+  return msg
 }
