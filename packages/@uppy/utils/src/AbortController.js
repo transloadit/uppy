bbf66cf1d28c9e5e768b79c71419a239357f0076
Renée Kooi <renee@kooi.me>
2020-06-08 13:41:28 +0200

    aws-s3-multipart: implement retries

---
+++ packages/@uppy/utils/src/AbortController.js
@@ -0,0 +1,64 @@
+/**
+ * A small AbortController ponyfill.
+ */
+
+const emitter = require('namespace-emitter')
+
+function createAbortEvent () {
+  try {
+    return new Event('abort')
+  } catch {
+    // For Internet Explorer
+    const event = document.createEvent('Event')
+    event.initEvent('abort', false, false)
+    return event
+  }
+}
+
+function createAbortError () {
+  try {
+    return new DOMException('Aborted', 'AbortError')
+  } catch {
+    // For Internet Explorer
+    const error = new Error('Aborted')
+    error.name = 'AbortError'
+    return error
+  }
+}
+
+class AbortSignalPolyfill {
+  constructor () {
+    this._emitter = emitter()
+    this.aborted = false
+  }
+
+  _abort () {
+    this._emitter.emit('abort', createAbortEvent())
+    this.aborted = true
+  }
+
+  addEventListener (event, callback) {
+    this._emitter.on(event, callback)
+  }
+
+  removeEventListener (event, callback) {
+    this._emitter.off(event, callback)
+  }
+}
+
+class AbortControllerPolyfill {
+  constructor () {
+    this.signal = new AbortSignalPolyfill()
+  }
+
+  abort () {
+    this.signal._abort()
+  }
+}
+
+const AbortSignal = window.AbortSignal || AbortSignalPolyfill
+const AbortController = window.AbortController || AbortControllerPolyfill
+
+exports.AbortController = AbortController
+exports.AbortSignal = AbortSignal
+exports.createAbortError = createAbortError
