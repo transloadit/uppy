02c7e9577f62acec6cba34374bd3d12a749c887b
Renée Kooi <renee@kooi.me>
2020-06-10 12:51:03 +0200

    utils: add test for AbortController

---
+++ packages/@uppy/utils/src/AbortController.test.js
@@ -0,0 +1,42 @@
+const { AbortController, AbortSignal } = require('./AbortController')
+
+describe('AbortController', () => {
+  it('has the expected shape', () => {
+    const controller = new AbortController()
+    expect(typeof controller.abort).toBe('function')
+    expect(controller.signal).toBeInstanceOf(AbortSignal)
+  })
+
+  it('emits "abort" when abort() is called', () => {
+    const controller = new AbortController()
+    const callback = jest.fn()
+
+    controller.signal.addEventListener('abort', callback)
+    controller.abort()
+
+    expect(callback).toHaveBeenCalled()
+    expect(callback.mock.calls[0][0]).toBeInstanceOf(Event)
+  })
+
+  it('add and remove events', () => {
+    const controller = new AbortController()
+    const callback = jest.fn()
+    const callback2 = jest.fn()
+
+    controller.signal.addEventListener('abort', callback)
+    controller.signal.addEventListener('abort', callback2)
+    controller.signal.removeEventListener('abort', callback)
+    controller.abort()
+
+    expect(callback2).toHaveBeenCalled()
+    expect(callback2.mock.calls[0][0]).toBeInstanceOf(Event)
+  })
+
+  it('sets `signal.aborted` property when abort() is called', () => {
+    const controller = new AbortController()
+
+    expect(controller.signal.aborted).toBe(false)
+    controller.abort()
+    expect(controller.signal.aborted).toBe(true)
+  })
+})
