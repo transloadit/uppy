f76f05b9e1d4ad2f61d7378daffd237aff4a6b19
Renée Kooi <renee@kooi.me>
2020-06-29 15:01:47 +0200

    tus: fill in pr url

--- packages/@uppy/tus/src/index.js
+++ packages/@uppy/tus/src/index.js
@@ -681,7 +681,7 @@ module.exports = class Tus extends Plugin {
     this.uppy.on('reset-progress', this.handleResetProgress)
 
     if (this.opts.autoRetry) {
-      this.uppy.log('[Tus] The `autoRetry` option may be removed in Uppy 2.0. See [PR-URL] for alternatives.', 'warning')
+      this.uppy.log('[Tus] The `autoRetry` option may be removed in Uppy 2.0. See https://github.com/transloadit/uppy/pull/2347 for alternatives.', 'warning')
 
       this.uppy.on('back-online', this.uppy.retryAll)
     }
