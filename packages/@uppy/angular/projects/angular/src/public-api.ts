31ff661c0bd11ab9e052884090e20c68970eda6a
Adam Medford <adammedford@gmail.com>
2020-07-21 16:54:35 -0500

    feature(integration): initial Angular framework implementation

---
+++ packages/@uppy/angular/projects/angular/src/public-api.ts
@@ -0,0 +1,12 @@
+/*
+ * Public API Surface of @uppy/angular
+ */
+
+export { UppyAngularDashboardModule } from './lib/components/dashboard/dashboard.module';
+export { UppyAngularProgressBarModule } from './lib/components/progress-bar/progress-bar.module';
+export { UppyAngularStatusBarModule } from './lib/components/status-bar/status-bar.module';
+export { UppyAngularDragDropModule } from './lib/components/drag-drop/drag-drop.module';
+export { StatusBarComponent } from './lib/components/status-bar/status-bar.component';
+export { ProgressBarComponent } from './lib/components/progress-bar/progress-bar.component';
+export { DragDropComponent } from './lib/components/drag-drop/drag-drop.component';
+export { DashboardComponent } from './lib/components/dashboard/dashboard.component';
