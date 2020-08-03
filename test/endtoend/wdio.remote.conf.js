7ebbc5b3e1cd051637fc626a9ae2e7a0be123e51
Renée Kooi <renee@kooi.me>
2020-08-03 16:37:43 +0200

    ci: update safari to non broken version
    safari 11.0 simply does not work with webdriver on saucelabs.

--- test/endtoend/wdio.remote.conf.js
+++ test/endtoend/wdio.remote.conf.js
@@ -29,7 +29,7 @@ exports.config = {
     { browserName: 'chrome', version: '70.0', platform: 'Windows 10' },
     { browserName: 'MicrosoftEdge', version: '14', platform: 'Windows 10' },
     { browserName: 'MicrosoftEdge', version: '17', platform: 'Windows 10' },
-    { browserName: 'safari', version: '11.0', platform: 'macOS 10.12' },
+    { browserName: 'safari', version: '11.1', platform: 'macOS 10.12' },
     { browserName: 'Safari', platformName: 'iOS', platformVersion: '12.2', deviceOrientation: 'portrait', deviceName: 'iPhone 8 Simulator' },
     { browserName: 'chrome', platformName: 'Android', platformVersion: '6.0', deviceOrientation: 'portrait', deviceName: 'Android Emulator' }
   ].map(createCapability),
