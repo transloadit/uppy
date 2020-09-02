46c366389f7e8aeee43cb3187a10ddabf0506192
Renée Kooi <renee@kooi.me>
2020-09-02 13:21:38 +0200

    react-native: fix lint

--- packages/@uppy/react-native/file-picker/takePicture.js
+++ packages/@uppy/react-native/file-picker/takePicture.js
@@ -3,6 +3,8 @@ import * as ImagePicker from 'expo-image-picker'
 
 function takePictureWithExpo (options) {
   return new Promise((resolve, reject) => {
+    // This is a different `Permissions` object than eslint-plugin-compat thinks it is
+    // eslint-disable-next-line compat/compat
     return Permissions.askAsync(Permissions.CAMERA).then((isAllowed) => {
       if (!isAllowed) {
         return reject(new Error('Permissions denied'))
