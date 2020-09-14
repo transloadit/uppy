3956e1256631846c1ecdff8a333d3d0e3c79812b
Renée Kooi <renee@kooi.me>
2020-09-14 13:36:32 +0200

    dashboard: set dir attribute if page did not provide one

--- packages/@uppy/dashboard/src/index.js
+++ packages/@uppy/dashboard/src/index.js
@@ -8,6 +8,7 @@ const ThumbnailGenerator = require('@uppy/thumbnail-generator')
 const findAllDOMElements = require('@uppy/utils/lib/findAllDOMElements')
 const toArray = require('@uppy/utils/lib/toArray')
 const getDroppedFiles = require('@uppy/utils/lib/getDroppedFiles')
+const getTextDirection = require('@uppy/utils/lib/getTextDirection')
 const trapFocus = require('./utils/trapFocus')
 const cuid = require('cuid')
 const ResizeObserver = require('resize-observer-polyfill').default || require('resize-observer-polyfill')
@@ -946,6 +947,15 @@ module.exports = class Dashboard extends Plugin {
         this.addTarget(plugin)
       }
     })
+  }
+
+  onMount () {
+    // Set the text direction if the page has not defined one.
+    const element = this.el
+    const direction = getTextDirection(element)
+    if (!direction) {
+      element.dir = 'ltr'
+    }
   }
 
   install = () => {
