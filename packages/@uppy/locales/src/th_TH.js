9cc38a962d5fec83879b447c0fc417dec7ebc64f
Kanin Peanviriyakulkit <dogrocker@users.noreply.github.com>
2020-10-06 18:50:42 +0700

    Update th_TH.js (#2571)
    Update TH language same as master branch.

--- packages/@uppy/locales/src/th_TH.js
+++ packages/@uppy/locales/src/th_TH.js
@@ -1,16 +1,24 @@
 const th_TH = {}
 
 th_TH.strings = {
+  addBulkFilesFailed: {
+    '0': 'ไม่สามารถเพิ่ม %{smart_count} ไฟล์ เนื่องจากข้อผิดพลาดภายใน',
+    '1': 'ไม่สามารถเพิ่ม %{smart_count} ไฟล์ เนื่องจากข้อผิดพลาดภายใน'
+  },
   addMore: 'เพิ่ม',
   addMoreFiles: 'เพิ่มไฟล์',
   addingMoreFiles: 'กำลังเพิ่มไฟล์',
   allowAccessDescription: 'ในการถ่ายภาพหรือบันทึกวิดีโอด้วยกล้องของคุณ โปรดอนุญาตการเข้าถึงกล้อง',
   allowAccessTitle: 'กรุณากดปุ่มยอมรับเพื่อใช้งานกล้อง',
+  aspectRatioLandscape: 'ครอบตัดภาพทิวทัศน์ (16:9)',
+  aspectRatioPortrait: 'ครอบตัดภาพบุคคล (9:16)',
+  aspectRatioSquare: 'ครอบตัดสี่เหลี่ยม',
   authenticateWith: 'เชื่อมต่อกับ %{pluginName}',
   authenticateWithTitle: 'กรุณาเข้าใช้งานกับ %{pluginName} เพื่อเลือกไฟล์',
   back: 'ย้อนกลับ',
   browse: 'เรียกดู',
   browseFiles: 'เรียกดู',
+  browseFolders: 'เรียกดูทั้งโฟลเดอร์',
   cancel: 'ยกเลิก',
   cancelUpload: 'ยกเลิกการอัปโหลด',
   chooseFiles: 'เลือกไฟล์',
@@ -43,56 +51,72 @@ th_TH.strings = {
   enterCorrectUrl: 'URL ไม่ถูกต้อง: โปรดตรวจสอบให้แน่ใจว่าคุณป้อน direct link',
   enterUrlToImport: 'ป้อน URL เพื่อนำเข้าไฟล์',
   exceedsSize: 'ไฟล์นี้มีขนาดเกินขนาดสูงสุดที่อนุญาต',
+  exceedsSize2: '%{backwardsCompat} %{size}',
   failedToFetch: 'Companion ไม่สามารถเรียก URL นี้ได้, กรุณาตรวจสอบว่า URL ถูกต้อง',
   failedToUpload: 'ไม่สามารถอัปโหลด %{file}',
   fileSource: 'ตำแหน่งของไฟล์: %{name}',
   filesUploadedOfTotal: {
     '0': 'อัพโหลดสำเร็จ %{complete} จากทั้งหมด %{smart_count} ไฟล์',
-    '1': 'อัพโหลดสำเร็จ %{complete} จากทั้งหมด %{smart_count} ไฟล์',
-    '2': 'อัพโหลดสำเร็จ %{complete} จากทั้งหมด %{smart_count} ไฟล์'
+    '1': 'อัพโหลดสำเร็จ %{complete} จากทั้งหมด %{smart_count} ไฟล์'
   },
   filter: 'ตัวกรอง',
   finishEditingFile: 'แก้ไขไฟล์สำเร็จ',
+  flipHorizontal: 'กลับตามแนวนอน',
   folderAdded: {
     '0': 'เพิ่ม %{smart_count} ไฟล์จาก %{folder}',
-    '1': 'เพิ่ม %{smart_count} ไฟล์จาก %{folder}',
-    '2': 'เพิ่ม %{smart_count} ไฟล์จาก %{folder}'
+    '1': 'เพิ่ม %{smart_count} ไฟล์จาก %{folder}'
   },
   generatingThumbnails: 'กำลังสร้างภาพขนาดย่อ...',
   import: 'นำเข้า',
   importFrom: 'นำเข้าจาก %{name}',
+  inferiorSize: 'ไฟล์นี้มีขนาดเล็กกว่าขนาดไฟล์ที่กำหนด %{size}',
   loading: 'กำลังโหลด...',
   logOut: 'ออกจากระบบ',
+  micDisabled: 'ไมโครโฟนถูกปิดโดยผู้ใช้',
   myDevice: 'ภาพในเครื่อง',
+  noCameraDescription: 'ในการถ่ายภาพหรือบันทึกวิดีโอโปรดเชื่อมต่ออุปกรณ์กล้อง',
+  noCameraTitle: 'กล้องไม่พร้อมใช้งาน',
+  noDuplicates: 'ไม่สามารถเพิ่มไฟล์ซ้ำได้ ไฟล์ \'%{fileName}\', มีอยู่แล้ว',
   noFilesFound: 'คุณไม่มีไฟล์หรือโฟลเดอร์ที่นี่',
   noInternetConnection: 'ไม่มีการเชื่อมต่ออินเทอร์เน็ต',
+  noNewAlreadyUploading: 'ไม่สามารถเพิ่มไฟล์ใหม่: กำลังอยู่ระหว่างอัพโหลด',
   openFolderNamed: 'เปิดโฟลเดอร์ %{name}',
   pause: 'หยุดชั่วคราว',
   pauseUpload: 'หยุดการอัปโหลดชั่วคราว',
   paused: 'หยุดชั่วคราว',
   poweredBy: 'ขับเคลื่อนโดย',
+  poweredBy2: '%{backwardsCompat} %{uppy}',
   processingXFiles: {
     '0': 'กำลังประมวลผล %{smart_count} ไฟล์',
-    '1': 'กำลังประมวลผล %{smart_count} ไฟล์',
-    '2': 'กำลังประมวลผล %{smart_count} ไฟล์'
+    '1': 'กำลังประมวลผล %{smart_count} ไฟล์'
   },
+  recording: 'กำลังบันทึก',
+  recordingLength: 'ความยาว %{recording_length}',
+  recordingStoppedMaxSize: 'หยุดการบันทึกเนื่องจากขนาดไฟล์กำลังเกินขีดจำกัด',
   removeFile: 'ลบไฟล์',
   resetFilter: 'รีเซ็ตตัวกรอง',
   resume: 'ทำต่อ',
   resumeUpload: 'ทำการอัปโหลดต่อ',
   retry: 'ลองใหม่อีกครั้ง',
   retryUpload: 'ลองอัพโหลดใหม่',
+  revert: 'เปลี่ยนกลับ',
+  rotate: 'หมุน',
+  save: 'บันทึก',
   saveChanges: 'บันทึกการแก้ไข',
   selectAllFilesFromFolderNamed: 'เลือกไฟล์ทั้งหมดจากโฟลเดอร์ %{name}',
   selectFileNamed: 'เลือกไฟล์ %{name}',
   selectX: {
     '0': 'เลือก %{smart_count}',
-    '1': 'เลือก %{smart_count}',
-    '2': 'เลือก %{smart_count}'
+    '1': 'เลือก %{smart_count}'
   },
   smile: 'ยิ้ม!',
+  startCapturing: 'เริ่มการจับภาพหน้าจอ',
   startRecording: 'เริ่มการบันทึกวิดีโอ',
+  stopCapturing: 'หยุดการจับภาพหน้าจอ',
   stopRecording: 'หยุดการบันทึกวิดีโอ',
+  streamActive: 'สตรีมทำงานอยู่',
+  streamPassive: 'สตรีมแบบพาสซีฟ',
+  submitRecordedFile: 'ส่งวิดีโอที่ถ่าย',
   takePicture: 'ถ่ายภาพ',
   timedOut: 'ยกเลิก, ไม่สามารถอัพโหลดไฟล์ได้เป็นเวลา %{seconds} วินาที',
   unselectAllFilesFromFolderNamed: 'ยกเลิกการเลือกไฟล์ทั้งหมดจากโฟลเดอร์ %{name}',
@@ -103,42 +127,37 @@ th_TH.strings = {
   uploadPaused: 'หยุดอัปโหลดไฟล์ชั่วคราว',
   uploadXFiles: {
     '0': 'กำลังอัพโหลด %{smart_count} ไฟล์',
-    '1': 'กำลังอัพโหลด %{smart_count} ไฟล์',
-    '2': 'กำลังอัพโหลด %{smart_count} ไฟล์'
+    '1': 'กำลังอัพโหลด %{smart_count} ไฟล์'
   },
   uploadXNewFiles: {
     '0': 'กำลังอัพโหลด +%{smart_count} ไฟล์',
-    '1': 'กำลังอัพโหลด +%{smart_count} ไฟล์',
-    '2': 'กำลังอัพโหลด +%{smart_count} ไฟล์'
+    '1': 'กำลังอัพโหลด +%{smart_count} ไฟล์'
   },
   uploading: 'Uploading',
   uploadingXFiles: {
     '0': 'กำลังอัพโหลด %{smart_count} ไฟล์',
-    '1': 'กำลังอัพโหลด %{smart_count} ไฟล์',
-    '2': 'กำลังอัพโหลด %{smart_count} ไฟล์'
+    '1': 'กำลังอัพโหลด %{smart_count} ไฟล์'
   },
   xFilesSelected: {
     '0': '%{smart_count} ไฟล์ถูกเลือก',
-    '1': '%{smart_count} ไฟล์ถูกเลือก',
-    '2': '%{smart_count} ไฟล์ถูกเลือก'
+    '1': '%{smart_count} ไฟล์ถูกเลือก'
   },
   xMoreFilesAdded: {
     '0': '%{smart_count} ไฟล์ที่สามารถเพิ่มได้',
-    '1': '%{smart_count} ไฟล์ที่สามารถเพิ่มได้',
-    '2': '%{smart_count} ไฟล์ที่สามารถเพิ่มได้'
+    '1': '%{smart_count} ไฟล์ที่สามารถเพิ่มได้'
   },
   xTimeLeft: 'เหลืออีก %{time}',
   youCanOnlyUploadFileTypes: 'คุณสามารถอัพโหลดได้แค่: %{types}',
   youCanOnlyUploadX: {
     '0': 'คุณสามารถอัพโหลดได้แค่ %{smart_count} ไฟล์',
-    '1': 'คุณสามารถอัพโหลดได้แค่ %{smart_count} ไฟล์',
-    '2': 'คุณสามารถอัพโหลดได้แค่ %{smart_count} ไฟล์'
+    '1': 'คุณสามารถอัพโหลดได้แค่ %{smart_count} ไฟล์'
   },
   youHaveToAtLeastSelectX: {
     '0': 'คุณต้องเลือกอย่างน้อย %{smart_count} ไฟล์',
-    '1': 'คุณต้องเลือกอย่างน้อย %{smart_count} ไฟล์',
-    '2': 'คุณต้องเลือกอย่างน้อย %{smart_count} ไฟล์'
-  }
+    '1': 'คุณต้องเลือกอย่างน้อย %{smart_count} ไฟล์'
+  },
+  zoomIn: 'ซูมเข้า',
+  zoomOut: 'ซูมออก'
 }
 
 th_TH.pluralize = function (n) {
