9d0585671be0e6ed396cecd55f68e0b78eb4821b
lamartire <lamartire@gmail.com>
2020-11-05 18:58:46 +0300

    add types for image editor actions

--- packages/@uppy/image-editor/types/index.d.ts
+++ packages/@uppy/image-editor/types/index.d.ts
@@ -2,9 +2,21 @@ import Uppy = require('@uppy/core')
 import ImageEditorLocale = require('./generatedLocale')
 
 declare module ImageEditor {
+  type Actions {
+    revert: boolean
+    rotate: boolean
+    flip: boolean
+    zoomIn: boolean
+    zoomOut: boolean
+    cropSquare: boolean
+    cropWidescreen: boolean
+    cropWidescreenVertical: boolean
+  }
+
   export interface ImageEditorOptions extends Uppy.PluginOptions {
     cropperOptions?: object
-    quality?: number,
+    actions?: Actions
+    quality?: number
     target?: Uppy.PluginTarget
     locale?: ImageEditorLocale
   }
