e336d64d2b49b86dacd2e712ac9a72d653234663
Adam Bannach <43150303+abannach@users.noreply.github.com>
2020-11-09 12:22:54 -0600

    expand result as body on success (#1)

--- packages/@uppy/aws-s3-multipart/src/index.js
+++ packages/@uppy/aws-s3-multipart/src/index.js
@@ -159,6 +159,9 @@ module.exports = class AwsS3Multipart extends Plugin {
 
       const onSuccess = (result) => {
         const uploadResp = {
+          body: {
+            ...result
+          },
           uploadURL: result.location
         }
 
