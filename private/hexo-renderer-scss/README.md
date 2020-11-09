4eeac1beed3e95c5021f22e699fb0e47b13775e6
Renée Kooi <renee@kooi.me>
2020-11-09 12:26:40 +0100

    upgrade node-sass to v5

---
+++ private/hexo-renderer-scss/README.md
@@ -0,0 +1,5 @@
+# hexo-renderer-scss
+A fork of https://github.com/mamboer/hexo-renderer-scss, using node-sass 5.x.
+
+## License
+[MIT, © 2015 AOTU Labs](./LICENSE)
---
+++ private/hexo-renderer-scss/index.js
@@ -0,0 +1,4 @@
+var renderer = require('./lib/renderer');
+// associate the Sass renderer with .scss AND .sass extensions
+hexo.extend.renderer.register('scss', 'css', renderer);
+hexo.extend.renderer.register('sass', 'css', renderer);
