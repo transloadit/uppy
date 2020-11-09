5eb42dba20a37adf4170997a0ce5b29d29b281f6
Renée Kooi <renee@kooi.me>
2020-11-09 13:48:04 +0100

    Revert "upgrade node-sass to v5"
    This reverts commit 4eeac1beed3e95c5021f22e699fb0e47b13775e6.

--- private/hexo-renderer-scss/README.md
+++
@@ -1,5 +0,0 @@
-# hexo-renderer-scss
-A fork of https://github.com/mamboer/hexo-renderer-scss, using node-sass 5.x.
-
-## License
-[MIT, © 2015 AOTU Labs](./LICENSE)
--- private/hexo-renderer-scss/index.js
+++
@@ -1,4 +0,0 @@
-var renderer = require('./lib/renderer');
-// associate the Sass renderer with .scss AND .sass extensions
-hexo.extend.renderer.register('scss', 'css', renderer);
-hexo.extend.renderer.register('sass', 'css', renderer);
