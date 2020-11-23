7b2fab943068648a14952efda09018023d71a9ed
Ali San Kaya <akaya2@binghamton.edu>
2020-11-23 12:43:11 -0500

    more speakercount options

--- packages/@uppy/dashboard/src/components/FileItem/index.js
+++ packages/@uppy/dashboard/src/components/FileItem/index.js
@@ -134,6 +134,10 @@ module.exports = class FileItem extends Component {
               <option value="3">3</option>
               <option value="4">4</option>
               <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
             </select>
           </div>
         </div>
