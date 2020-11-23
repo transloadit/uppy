404a1896504bc844ab15878b34045b40b4194041
Renée Kooi <renee@kooi.me>
2020-11-23 11:51:44 +0100

    react: add useUppy() hook

--- packages/@uppy/react/types/index.d.ts
+++ packages/@uppy/react/types/index.d.ts
@@ -5,3 +5,6 @@ export { default as DashboardModal } from '../src/DashboardModal'
 export { default as DragDrop } from '../src/DragDrop'
 export { default as ProgressBar } from '../src/ProgressBar'
 export { default as StatusBar } from '../src/StatusBar'
+
+import useUppy = require('../src/useUppy')
+export { useUppy }
