b082e54029aec08cb91b2f14f5ea5eaa65ad0e9f
Andrew <ajkachnic@protonmail.com>
2020-11-25 17:59:06 -0500

    Vue integration (#2500)
    * Basic setup
    
    * Updated some documentation
    
    * Cleanup some useless files
    
    * Added docs to website
    
    * Re-add package.json
    
    * Added module
    
    * Update vue.md
    
    * Update DOCS.md
    
    * Starting writing docs
    
    * Fix my own spelling mistake
    
    * Add a proper README and LICENSE
    
    * Some quick fixes
    
    * Update Dashboard.vue
    
    * Added shallow-equal
    
    * Try adding an example to test vue integration locally
    
    * Temporarily fixed build and example
    
    * Small documentation fixes
    
    * Added Vue 3 example
    
    * Added proper bundling and typescript for vue
    
    * Added full typescript support for integration
    
    All @uppy/vue components now support TypeScript and are strongly typed.
    
    * Massive docs improvement for @uppy/vue
    
    * Fix small type error in dashboard-modal
    
    * Slight documentation changes
    
    * Add typescript types file to package.json
    
    * Trim down dependencies
    
    * Fix building with Vue
    
    I added it to a list of ignored packages as it builds a little weird
    
    * Updated example
    
    * Converted .vue files to .ts
    
    * Setup .js files for @uppy/vue
    
    * Fixed small rendering bug
    
    * Delete package-lock.json
    
    * Delete package-lock.json
    
    * Delete package-lock.json
    
    * Update packages/@uppy/vue/package.json
    
    Co-authored-by: Artur Paikin <artur@arturpaikin.com>
    
    * Some cleaning for @uppy/vue
    
    * Delete components.d.ts
    
    * Removed Vue 3 example
    
    * Update typescript stuff for @uppy/vue
    
    * Documentation update for @uppy/vue
    
    I changed to menu text to 'Other Integrations'
    
    Co-authored-by: Artur Paikin <artur@arturpaikin.com>

---
+++ packages/@uppy/vue/src/dashboard.js
@@ -0,0 +1,62 @@
+import DashboardPlugin from '@uppy/dashboard'
+import { shallowEqualObjects } from 'shallow-equal'
+
+export default {
+  data () {
+    return {
+      plugin: {}
+    }
+  },
+  props: {
+    uppy: {
+      required: true
+    },
+    props: {
+      type: Object
+    },
+    plugins: {
+      type: Array
+    }
+  },
+  mounted () {
+    this.installPlugin()
+  },
+  methods: {
+    installPlugin () {
+      const uppy = this.uppy
+      const options = {
+        id: 'vue:Dashboard',
+        inline: true,
+        plugins: this.plugins,
+        ...this.props,
+        target: this.$refs.container
+      }
+      uppy.use(DashboardPlugin, options)
+      this.plugin = uppy.getPlugin(options.id)
+    },
+    uninstallPlugin (uppy) {
+      uppy.removePlugin(this.plugin)
+    }
+  },
+  beforeDestroy () {
+    this.uninstallPlugin(this.uppy)
+  },
+  watch: {
+    uppy (current, old) {
+      if (old !== current) {
+        this.uninstallPlugin(old)
+        this.installPlugin()
+      }
+    },
+    props (current, old) {
+      if (!shallowEqualObjects(current, old)) {
+        this.plugin.setOptions({ ...current })
+      }
+    }
+  },
+  render (createElement) {
+    return createElement('div', {
+      ref: 'container'
+    })
+  }
+}
---
+++ packages/@uppy/vue/src/progress-bar.js
@@ -0,0 +1,57 @@
+import ProgressBarPlugin from '@uppy/progress-bar'
+import { shallowEqualObjects } from 'shallow-equal'
+
+export default {
+  data () {
+    return {
+      plugin: {}
+    }
+  },
+  props: {
+    uppy: {
+      required: true
+    },
+    props: {
+      type: Object
+    }
+  },
+  mounted () {
+    this.installPlugin()
+  },
+  methods: {
+    installPlugin () {
+      const uppy = this.uppy
+      const options = {
+        id: 'vue:ProgressBar',
+        ...this.props,
+        target: this.$refs.container
+      }
+      uppy.use(ProgressBarPlugin, options)
+      this.plugin = uppy.getPlugin(options.id)
+    },
+    uninstallPlugin (uppy) {
+      uppy.removePlugin(this.plugin)
+    }
+  },
+  beforeDestroy () {
+    this.uninstallPlugin(this.uppy)
+  },
+  watch: {
+    uppy (current, old) {
+      if (old !== current) {
+        this.uninstallPlugin(old)
+        this.installPlugin()
+      }
+    },
+    props (current, old) {
+      if (!shallowEqualObjects(current, old)) {
+        this.plugin.setOptions({ ...current })
+      }
+    }
+  },
+  render (createElement) {
+    return createElement('div', {
+      ref: 'container'
+    })
+  }
+}
