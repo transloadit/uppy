fbc54481912951cf3e8b8be3a0f0b9403f7570f2
Denys Nosov <denys@joomla-ua.org>
2020-12-30 02:33:49 +0200

    Update uk_UA.js

--- packages/@uppy/locales/src/uk_UA.js
+++ packages/@uppy/locales/src/uk_UA.js
@@ -141,18 +141,18 @@ uk_UA.strings = {
 
 uk_UA.pluralize = function (n) {
     if (n % 10 === 1 && n % 100 !== 11) {
-        return 0;
+        return 0
     }
 
     if (n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 >= 20)) {
-        return 1;
+        return 1
     }
 
-    return 2;
+    return 2
 }
 
 if (typeof window !== 'undefined' && typeof window.Uppy !== 'undefined') {
-    window.Uppy.locales.uk_UA = uk_UA;
+    window.Uppy.locales.uk_UA = uk_UA
 }
 
-module.exports = uk_UA;
+module.exports = uk_UA
