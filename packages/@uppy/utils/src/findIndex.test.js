502a755458acc9166f0a21da82323a4b1e099535
Artur Paikin <artur@arturpaikin.com>
2021-03-08 18:16:01 +0000

    Use Array.findIndex instead of Array.indexOf in uppy.removePlugin (#2793)
    * move findIndex to @uppy/utils, add tests
    
    * use Array.findIndex instead of Array.indexOf in uppy.removePlugin
    
    because strict comparison started failing with Vue3 Proxies

---
+++ packages/@uppy/utils/src/findIndex.test.js
@@ -0,0 +1,23 @@
+const findIndex = require('./findIndex')
+
+describe('findIndex', () => {
+  it('should return index of an object in an array, that matches a predicate', () => {
+    const arr = [
+      { name: 'foo' },
+      { name: 'bar' },
+      { name: '123' }
+    ]
+    const index = findIndex(arr, item => item.name === 'bar')
+    expect(index).toEqual(1)
+  })
+
+  it('should return -1 when no object in an array matches a predicate', () => {
+    const arr = [
+      { name: 'foo' },
+      { name: 'bar' },
+      { name: '123' }
+    ]
+    const index = findIndex(arr, item => item.name === 'hello')
+    expect(index).toEqual(-1)
+  })
+})
