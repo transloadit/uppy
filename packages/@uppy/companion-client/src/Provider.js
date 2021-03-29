bdc76e556f0015ff08c50e0f3f8fe9410c0a52dc
Renée Kooi <renee@kooi.me>
2021-03-29 11:50:00 +0200

    IE compat

--- packages/@uppy/companion-client/src/Provider.js
+++ packages/@uppy/companion-client/src/Provider.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const qsStringify = require('qs-stringify')
+const URL = require('url-parse')
 const RequestClient = require('./RequestClient')
 const tokenStorage = require('./tokenStorage')
 
