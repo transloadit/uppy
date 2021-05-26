706747cd73b09340e9895a080e4c0534d35f1c84
Antoine du Hamel <duhamelantoine1995@gmail.com>
2021-05-26 11:45:36 +0200

    Translator: refactor interpolate
    Fixes types and small code edits:
    `<regex>[Symbol.split](<string>)` is equivalent to
    `String.prototype.split.call(<string>, <regex>)` and potentially more
    readable (the downside it's not compatible with IE, which may be why we
    currently use the latter).
    
    Refs: https://github.com/transloadit/uppy/pull/2899

--- packages/@uppy/utils/src/Translator.js
+++ packages/@uppy/utils/src/Translator.js
@@ -50,12 +50,11 @@ module.exports = class Translator {
    * @license https://github.com/airbnb/polyglot.js/blob/master/LICENSE
    * taken from https://github.com/airbnb/polyglot.js/blob/master/lib/polyglot.js#L299
    *
-   * @param {string} phrase that needs interpolation, with placeholders
+   * @param {any} phrase that needs interpolation, with placeholders
    * @param {object} options with values that will be used to replace placeholders
-   * @returns {string} interpolated
+   * @returns {any[]} interpolated
    */
   interpolate (phrase, options) {
-    const { split, replace } = String.prototype
     const dollarRegex = /\$/g
     const dollarBillsYall = '$$$$'
     let interpolated = [phrase]
@@ -67,7 +66,7 @@ module.exports = class Translator {
         // be escaped with "$" itself, and we need two in the resulting output.
         var replacement = options[arg]
         if (typeof replacement === 'string') {
-          replacement = replace.call(options[arg], dollarRegex, dollarBillsYall)
+          replacement = dollarRegex[Symbol.replace](replacement, dollarBillsYall)
         }
         // We create a new `RegExp` each time instead of using a more-efficient
         // string replace so that the same argument can be replaced multiple times
@@ -89,7 +88,7 @@ module.exports = class Translator {
           return newParts.push(chunk)
         }
 
-        split.call(chunk, rx).forEach((raw, i, list) => {
+        rx[Symbol.split](chunk).forEach((raw, i, list) => {
           if (raw !== '') {
             newParts.push(raw)
           }
