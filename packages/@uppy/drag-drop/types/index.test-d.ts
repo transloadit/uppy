33dbe58cb3bd9f36b488d83d7508f0c422fb7627
Merlijn Vos <merlijn@soverin.net>
2021-06-14 13:22:01 +0200

    Expose drag-drop events (#2914)
    * Expose drag-drop events
    
    * we need to call DragDrop.renderArrowSvg without this, since it’s now static
    
    * Update docs based on feedback
    
    Co-authored-by: Artur Paikin <artur@arturpaikin.com>

--- packages/@uppy/drag-drop/types/index.test-d.ts
+++ packages/@uppy/drag-drop/types/index.test-d.ts
@@ -1,2 +1,26 @@
-import DragDrop = require('../')
-// TODO implement
+import Uppy = require("@uppy/core");
+
+import DragDrop = require("../");
+
+{
+  const uppy = Uppy<Uppy.StrictTypes>();
+
+  uppy.use(DragDrop, {
+    replaceTargetContent: true,
+    target: "body",
+    inputName: "test",
+    allowMultipleFiles: true,
+    width: 100,
+    height: "100",
+    note: "note",
+    locale: {
+      strings: {
+        dropHereOr: "test",
+        browse: "test"
+      }
+    },
+    onDragOver: event => event.clientX,
+    onDragLeave: event => event.clientY,
+    onDrop: event => event
+  });
+}
