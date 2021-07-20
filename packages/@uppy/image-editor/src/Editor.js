48905ee7fcb4fa7ee620d1a3b8df7c5726c0082c
Artur Paikin <artur@arturpaikin.com>
2021-07-20 16:45:25 +0100

    Remove Save button from Image Editor toolbar

--- packages/@uppy/image-editor/src/Editor.js
+++ packages/@uppy/image-editor/src/Editor.js
@@ -231,20 +231,6 @@ module.exports = class Editor extends Component {
         </div>
 
         <div className="uppy-ImageCropper-controls">
-          <button
-            type="button"
-            className="uppy-u-reset uppy-c-btn"
-            aria-label={i18n('save')}
-            data-microtip-position="top"
-            role="tooltip"
-            onClick={() => this.props.save()}
-          >
-            <svg aria-hidden="true" className="uppy-c-icon" width="24" height="24" viewBox="0 0 24 24">
-              <path d="M0 0h24v24H0z" fill="none" />
-              <path d="M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z" />
-            </svg>
-          </button>
-
           {actions.revert && this.renderRevert()}
           {actions.rotate && this.renderRotate()}
           {actions.granularRotate && this.renderGranularRotate()}
