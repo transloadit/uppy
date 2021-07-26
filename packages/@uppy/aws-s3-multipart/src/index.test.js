00cf364ec21d363e3d25ae6d6f0ccbeed3c6f5e7
Martin Brennan <martin@discourse.org>
2021-07-26 16:06:39 +1000

    Add another test for GH actions run

--- packages/@uppy/aws-s3-multipart/src/index.test.js
+++ packages/@uppy/aws-s3-multipart/src/index.test.js
@@ -10,4 +10,14 @@ describe('AwsS3Multipart', () => {
     const pluginNames = core.plugins.uploader.map((plugin) => plugin.constructor.name)
     expect(pluginNames).toContain('AwsS3Multipart')
   })
+
+  describe('prepareUploadPart', () => {
+    it('Throws an error if configured without companionUrl', () => {
+      const core = new Core()
+      core.use(AwsS3Multipart)
+      const awsS3Multipart = core.getPlugin('AwsS3Multipart')
+
+      expect(awsS3Multipart.opts.prepareUploadPart).toThrow()
+    })
+  })
 })
