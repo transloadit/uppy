00a0286c7f4e8be4b35171e1ea347f6fedb5a2bb
Martin Brennan <martin@discourse.org>
2021-07-28 18:07:06 +1000

    Remove spec and refactor to beforeEach

--- packages/@uppy/aws-s3-multipart/src/index.test.js
+++ packages/@uppy/aws-s3-multipart/src/index.test.js
@@ -36,27 +36,28 @@ describe('AwsS3Multipart', () => {
   })
 
   describe('without companionUrl (custom main functions)', () => {
-    const core = new Core()
-    core.use(AwsS3Multipart, {
-      createMultipartUpload: jest.fn(() => {
-        return {
-          uploadId: '6aeb1980f3fc7ce0b5454d25b71992',
-          key: 'test/upload/multitest.bat',
-        }
-      }),
-      completeMultipartUpload: jest.fn(() => Promise.resolve({ location: 'test' })),
-      abortMultipartUpload: jest.fn(),
-      // TOOD (martin) Use a proper presigned URL for a part number here
-      prepareUploadPart: jest.fn(() => {
-        return {
-          url: 'https://bucket.s3.us-east-2.amazonaws.com/test/upload/multitest.bat',
-        }
-      }),
-    })
-    const awsS3Multipart = core.getPlugin('AwsS3Multipart')
+    let core
+    let awsS3Multipart
 
-    afterEach(() => {
-      core.reset()
+    beforeEach(() => {
+      core = new Core()
+      core.use(AwsS3Multipart, {
+        createMultipartUpload: jest.fn(() => {
+          return {
+            uploadId: '6aeb1980f3fc7ce0b5454d25b71992',
+            key: 'test/upload/multitest.bat',
+          }
+        }),
+        completeMultipartUpload: jest.fn(() => Promise.resolve({ location: 'test' })),
+        abortMultipartUpload: jest.fn(),
+        // TOOD (martin) Use a proper presigned URL for a part number here
+        prepareUploadPart: jest.fn(() => {
+          return {
+            url: 'https://bucket.s3.us-east-2.amazonaws.com/test/upload/multitest.bat',
+          }
+        }),
+      })
+      awsS3Multipart = core.getPlugin('AwsS3Multipart')
     })
 
     it('Calls prepareUploadPart N times, where N is fileSize / getChunkSize()', (done) => {
@@ -104,30 +105,6 @@ describe('AwsS3Multipart', () => {
           awsS3Multipart.opts.prepareUploadPart = originalPrepareFn
           done()
         })
-    })
-
-    it('Emits an s3-multipart:part-uploaded event for each part', (done) => {
-      const scope = nock('https://bucket.s3.us-east-2.amazonaws.com').defaultReplyHeaders({
-        'access-control-allow-method': 'PUT',
-        'access-control-allow-origin': '*',
-        'access-control-expose-headers': 'ETag',
-      })
-      scope.options((uri) => uri.includes('test/upload/multitest.bat')).reply(200, '')
-      scope.put((uri) => uri.includes('test/upload/multitest.bat')).reply(200, '', { ETag: 'test' })
-      scope.persist()
-      const fileSize = 5 * MB + 1 * MB
-      core.addFile({
-        source: 'jest',
-        name: 'multitest.dat',
-        type: 'application/octet-stream',
-        data: new File([Buffer.alloc(fileSize)], { type: 'application/octet-stream' }),
-      })
-      const eventHandler = jest.fn()
-      core.on('s3-multipart:part-uploaded', eventHandler)
-      core.upload().then(() => {
-        expect(eventHandler.mock.calls.length).toEqual(2)
-        done()
-      })
     })
   })
 })
