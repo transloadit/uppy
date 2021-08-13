02ca2e6bd9579a168881a15259c297244be1e22e
Mikael Finstad <finstaden@gmail.com>
2021-08-13 17:58:23 +0700

    Improve logging
    When logging an object it would print [object Object] if `color` was set.
    e.g.: google drive callback: "callback.oauth.resp [object Object]"
    Now we will instead use util.inspect before calling color
    Also fix some linting warnings

--- packages/@uppy/companion/src/server/logger.js
+++ packages/@uppy/companion/src/server/logger.js
@@ -1,5 +1,6 @@
 const chalk = require('chalk')
 const escapeStringRegexp = require('escape-string-regexp')
+const util = require('util')
 
 const valuesToMask = []
 /**
@@ -22,47 +23,44 @@ exports.setMaskables = (maskables) => {
  * @param {string} msg the message whose content should be masked
  * @returns {string}
  */
-const maskMessage = (msg) => {
+function maskMessage (msg) {
+  let out = msg
   for (const toBeMasked of valuesToMask) {
     const toBeReplaced = new RegExp(toBeMasked, 'gi')
-    msg = msg.replace(toBeReplaced, '******')
+    out = out.replace(toBeReplaced, '******')
   }
-  return msg
+  return out
 }
 
 /**
  * message log
  *
- * @param {string | Error} msg the message to log
+ * @param {string | Error} msgIn the message to log
  * @param {string} tag a unique tag to easily search for this message
  * @param {string} level error | info | debug
- * @param {Function=} color function to display the log in appropriate color
  * @param {string=} id a unique id to easily trace logs tied to a request
+ * @param {Function=} color function to display the log in appropriate color
  * @param {boolean=} shouldLogStackTrace when set to true, errors will be logged with their stack trace
  */
-const log = (msg, tag, level, id, color, shouldLogStackTrace) => {
+const log = (msgIn, tag = '', level, id = '', color = (message) => message, shouldLogStackTrace) => {
   const time = new Date().toISOString()
-  tag = tag || ''
-  id = id || ''
   const whitespace = tag && id ? ' ' : ''
-  color = color || ((message) => message)
+  let msg = msgIn
   if (typeof msg === 'string') {
     msg = maskMessage(msg)
   } else if (msg && typeof msg.message === 'string') {
     msg.message = maskMessage(msg.message)
   }
 
+  // eslint-disable-next-line no-console
+  const logMsg = (msg2) => console.log(color(`companion: ${time} [${level}] ${id}${whitespace}${tag}`), color(util.inspect(msg2)))
+
   if (shouldLogStackTrace && msg instanceof Error && typeof msg.stack === 'string') {
     msg.stack = maskMessage(msg.stack)
-    // exclude msg from template string so values such as error objects
-    // can be well formatted
-    console.log(color(`companion: ${time} [${level}] ${id}${whitespace}${tag}`), color(msg.stack))
+    logMsg(msg.stack)
     return
   }
-
-  // exclude msg from template string so values such as error objects
-  // can be well formatted
-  console.log(color(`companion: ${time} [${level}] ${id}${whitespace}${tag}`), color(msg))
+  logMsg(msg)
 }
 
 /**
