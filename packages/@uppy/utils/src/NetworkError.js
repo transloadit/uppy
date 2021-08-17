405157ea49a892973170eabffad82904835e6039
Antoine du Hamel <duhamelantoine1995@gmail.com>
2021-08-17 20:14:49 +0200

    @uppy/aws-s3: refactor to use private fields (#3094)

--- packages/@uppy/utils/src/NetworkError.js
+++ packages/@uppy/utils/src/NetworkError.js
@@ -1,7 +1,8 @@
 class NetworkError extends Error {
   constructor (error, xhr = null) {
-    super(`This looks like a network error, the endpoint might be blocked by an internet provider or a firewall.\n\nSource error: [${error}]`)
+    super(`This looks like a network error, the endpoint might be blocked by an internet provider or a firewall.`)
 
+    this.cause = error
     this.isNetworkError = true
     this.request = xhr
   }
