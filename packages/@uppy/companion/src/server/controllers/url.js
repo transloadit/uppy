0ba83499482f7352780dcb8bf35224bc9de1044f
Mikael Finstad <finstaden@gmail.com>
2021-08-31 16:39:58 +0700

    fix lint by reordering code

--- packages/@uppy/companion/src/server/controllers/url.js
+++ packages/@uppy/companion/src/server/controllers/url.js
@@ -7,80 +7,6 @@ const Uploader = require('../Uploader')
 const { getURLMeta, getRedirectEvaluator, getProtectedHttpAgent } = require('../helpers/request')
 const logger = require('../logger')
 
-module.exports = () => router()
-  .post('/meta', meta)
-  .post('/get', get)
-
-/**
- * Fteches the size and content type of a URL
- *
- * @param {object} req expressJS request object
- * @param {object} res expressJS response object
- */
-const meta = async (req, res) => {
-  try {
-    logger.debug('URL file import handler running', null, req.id)
-    const { debug } = req.companion.options
-    if (!validateURL(req.body.url, debug)) {
-      logger.debug('Invalid request body detected. Exiting url meta handler.', null, req.id)
-      return res.status(400).json({ error: 'Invalid request body' })
-    }
-
-    const urlMeta = await getURLMeta(req.body.url, !debug)
-    res.json(urlMeta)
-  } catch (err) {
-    logger.error(err, 'controller.url.meta.error', req.id)
-    // @todo send more meaningful error message and status code to client if possible
-    return res.status(err.status || 500).json({ message: 'failed to fetch URL metadata' })
-  }
-}
-
-/**
- * Handles the reques of import a file from a remote URL, and then
- * subsequently uploading it to the specified destination.
- *
- * @param {object} req expressJS request object
- * @param {object} res expressJS response object
- */
-const get = async (req, res) => {
-  try {
-    logger.debug('URL file import handler running', null, req.id)
-    const { debug } = req.companion.options
-    if (!validateURL(req.body.url, debug)) {
-      logger.debug('Invalid request body detected. Exiting url import handler.', null, req.id)
-      res.status(400).json({ error: 'Invalid request body' })
-      return
-    }
-
-    const { size } = await getURLMeta(req.body.url, !debug)
-
-    // @ts-ignore
-    logger.debug('Instantiating uploader.', null, req.id)
-    const uploader = new Uploader(Uploader.reqToOptions(req, size))
-
-    if (uploader.hasError()) {
-      const response = uploader.getResponse()
-      res.status(response.status).json(response.body)
-      return
-    }
-
-    logger.debug('Waiting for socket connection before beginning remote download.', null, req.id)
-    uploader.onSocketReady(() => {
-      logger.debug('Socket connection received. Starting remote download.', null, req.id)
-      downloadURL(req.body.url, uploader.handleChunk.bind(uploader), !debug, req.id)
-    })
-
-    const response = uploader.getResponse()
-
-    // NOTE: Uploader will continue running after the http request is responded
-    res.status(response.status).json(response.body)
-  } catch (err) {
-    logger.error(err, 'controller.url.get.error', req.id)
-    // @todo send more meaningful error message and status code to client if possible
-    res.status(err.status || 500).json({ message: 'failed to fetch URL metadata' })
-  }
-}
-
 /**
  * Validates that the download URL is secure
  *
@@ -142,3 +68,77 @@ const downloadURL = (url, onDataChunk, blockLocalIPs, traceId) => {
       onDataChunk(err, null)
     })
 }
+
+/**
+ * Fteches the size and content type of a URL
+ *
+ * @param {object} req expressJS request object
+ * @param {object} res expressJS response object
+ */
+const meta = async (req, res) => {
+  try {
+    logger.debug('URL file import handler running', null, req.id)
+    const { debug } = req.companion.options
+    if (!validateURL(req.body.url, debug)) {
+      logger.debug('Invalid request body detected. Exiting url meta handler.', null, req.id)
+      return res.status(400).json({ error: 'Invalid request body' })
+    }
+
+    const urlMeta = await getURLMeta(req.body.url, !debug)
+    return res.json(urlMeta)
+  } catch (err) {
+    logger.error(err, 'controller.url.meta.error', req.id)
+    // @todo send more meaningful error message and status code to client if possible
+    return res.status(err.status || 500).json({ message: 'failed to fetch URL metadata' })
+  }
+}
+
+/**
+ * Handles the reques of import a file from a remote URL, and then
+ * subsequently uploading it to the specified destination.
+ *
+ * @param {object} req expressJS request object
+ * @param {object} res expressJS response object
+ */
+const get = async (req, res) => {
+  try {
+    logger.debug('URL file import handler running', null, req.id)
+    const { debug } = req.companion.options
+    if (!validateURL(req.body.url, debug)) {
+      logger.debug('Invalid request body detected. Exiting url import handler.', null, req.id)
+      res.status(400).json({ error: 'Invalid request body' })
+      return
+    }
+
+    const { size } = await getURLMeta(req.body.url, !debug)
+
+    // @ts-ignore
+    logger.debug('Instantiating uploader.', null, req.id)
+    const uploader = new Uploader(Uploader.reqToOptions(req, size))
+
+    if (uploader.hasError()) {
+      const response = uploader.getResponse()
+      res.status(response.status).json(response.body)
+      return
+    }
+
+    logger.debug('Waiting for socket connection before beginning remote download.', null, req.id)
+    uploader.onSocketReady(() => {
+      logger.debug('Socket connection received. Starting remote download.', null, req.id)
+      downloadURL(req.body.url, uploader.handleChunk.bind(uploader), !debug, req.id)
+    })
+
+    const response = uploader.getResponse()
+
+    // NOTE: Uploader will continue running after the http request is responded
+    res.status(response.status).json(response.body)
+  } catch (err) {
+    logger.error(err, 'controller.url.get.error', req.id)
+    // @todo send more meaningful error message and status code to client if possible
+    res.status(err.status || 500).json({ message: 'failed to fetch URL metadata' })
+  }
+}
+
+module.exports = () => router()
+  .post('/meta', meta)
+  .post('/get', get)
