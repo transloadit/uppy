f6c5cd3f0f473420656c864aff28bb2b9e2a89f7
Murderlon <merlijn@soverin.net>
2021-08-31 14:04:59 +0200

    Use private fields in `xhr-upload` and update types

--- packages/@uppy/xhr-upload/types/index.d.ts
+++ packages/@uppy/xhr-upload/types/index.d.ts
@@ -1,26 +1,32 @@
 import type { PluginOptions, BasePlugin, UppyFile } from '@uppy/core'
 import XHRUploadLocale from './generatedLocale'
 
-  type Headers = {
-    [name: string]: string | number
-  }
-
-export interface XHRUploadOptions extends PluginOptions {
-    limit?: number
-    bundle?: boolean
-    formData?: boolean
-    headers?: Headers | ((file: UppyFile) => Headers)
-    metaFields?: string[]
-    fieldName?: string
-    timeout?: number
-    responseUrlFieldName?: string
-    endpoint: string
-    method?: 'GET' | 'POST' | 'PUT' | 'HEAD' | 'get' | 'post' | 'put' | 'head'
-    locale?: XHRUploadLocale
-    responseType?: string
-    withCredentials?: boolean
+type Headers = {
+  [name: string]: string | number
 }
 
-declare class XHRUpload extends BasePlugin<XHRUploadOptions> {}
+export interface XHRUploadOptions extends PluginOptions {
+  limit?: number
+  bundle?: boolean
+  formData?: boolean
+  headers?: Headers | ((file: UppyFile) => Headers)
+  metaFields?: string[]
+  fieldName?: string
+  timeout?: number
+  responseUrlFieldName?: string
+  endpoint: string
+  method?: 'GET' | 'POST' | 'PUT' | 'HEAD' | 'get' | 'post' | 'put' | 'head'
+  locale?: XHRUploadLocale
+  responseType?: string
+  withCredentials?: boolean
+}
+
+declare class XHRUpload extends BasePlugin<XHRUploadOptions> {
+  public validateStatus: (statusCode: number, responseText: string, response: unknown) => boolean
+
+  public getResponseData: (responseText: string, response: unknown) => any
+
+  public getResponseError: (responseText: string, response: unknown) => Error
+}
 
 export default XHRUpload
