c7e0a5a88392d96302da8309e55b6b28fd44a025
Marcelo Luiz Onhate <onhate@users.noreply.github.com>
2021-09-13 09:55:10 -0300

    fix: AbortController is not defined on Node.js (Server Side Render) (#3169)
    * fix: AbortController is not defined
    
    * use globalThis
    
    Co-authored-by: Antoine du Hamel <duhamelantoine1995@gmail.com>
    
    Co-authored-by: Antoine du Hamel <duhamelantoine1995@gmail.com>

--- packages/@uppy/utils/src/AbortController.js
+++ packages/@uppy/utils/src/AbortController.js
@@ -1,6 +1,6 @@
 /**
  * Little AbortController proxy module so we can swap out the implementation easily later.
  */
-exports.AbortController = AbortController
-exports.AbortSignal = AbortSignal
+exports.AbortController = globalThis.AbortController
+exports.AbortSignal = globalThis.AbortSignal
 exports.createAbortError = (message = 'Aborted') => new DOMException(message, 'AbortError')
