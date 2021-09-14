4f21687d378ccb6fc524cf45e54b8597d1d8dd04
Murderlon <merlijn@soverin.net>
2021-09-14 16:51:22 +0200

    Inline `Itemlist` in `Browser`

--- packages/@uppy/provider-views/src/Browser.js
+++ packages/@uppy/provider-views/src/Browser.js
@@ -1,10 +1,17 @@
-const classNames = require('classnames')
 const { h } = require('preact')
-const Filter = require('./Filter')
-const ItemList = require('./ItemList')
-const FooterActions = require('./FooterActions')
+const classNames = require('classnames')
 
-const Browser = (props) => {
+const remoteFileObjToLocal = require('@uppy/utils/lib/remoteFileObjToLocal')
+
+const Filter = require('./Filter')
+const FooterActions = require('./FooterActions')
+const Item = require('./Item/index')
+
+const VIRTUAL_SHARED_DIR = 'shared-with-me'
+
+module.exports = Browser
+
+function Browser (props) {
   const {
     currentSelection,
     folders,
@@ -14,48 +21,107 @@ const Browser = (props) => {
     headerComponent,
     showBreadcrumbs,
     isChecked,
-    getNextFolder,
     toggleCheckbox,
     handleScroll,
     showTitles,
-    title,
     i18n,
     validateRestrictions,
     showFilter,
+    filterQuery,
+    filterInput,
+    getNextFolder,
+    cancel,
+    done,
+    columns,
   } = props
 
   const selected = currentSelection.length
 
   return (
-    <div className={classNames('uppy-ProviderBrowser', `uppy-ProviderBrowser-viewType--${viewType}`)}>
+    <div
+      className={classNames(
+        'uppy-ProviderBrowser',
+        `uppy-ProviderBrowser-viewType--${viewType}`
+      )}
+    >
       <div className="uppy-ProviderBrowser-header">
-        <div className={classNames('uppy-ProviderBrowser-headerBar', !showBreadcrumbs && 'uppy-ProviderBrowser-headerBar--simple')}>
+        <div
+          className={classNames(
+            'uppy-ProviderBrowser-headerBar',
+            !showBreadcrumbs && 'uppy-ProviderBrowser-headerBar--simple'
+          )}
+        >
           {headerComponent}
         </div>
       </div>
-      {showFilter && <Filter {...props} />}
-      <ItemList
-        columns={[{
-          name: 'Name',
-          key: 'title',
-        }]}
-        folders={folders}
-        files={files}
-        isChecked={isChecked}
-        handleFolderClick={getNextFolder}
-        toggleCheckbox={toggleCheckbox}
-        handleScroll={handleScroll}
-        title={title}
-        showTitles={showTitles}
-        i18n={i18n}
-        viewType={viewType}
-        validateRestrictions={validateRestrictions}
-        uppyFiles={uppyFiles}
-        currentSelection={currentSelection}
-      />
-      {selected > 0 && <FooterActions selected={selected} {...props} />}
+
+      {showFilter && (
+        <Filter
+          i18n={i18n}
+          filterQuery={filterQuery}
+          filterInput={filterInput}
+        />
+      )}
+
+      <div className="uppy-ProviderBrowser-body">
+        <ul
+          className="uppy-ProviderBrowser-list"
+          onScroll={handleScroll}
+          role="listbox"
+          // making <ul> not focusable for firefox
+          tabIndex="-1"
+        >
+          {folders.map((folder) => {
+            return Item({
+              columns,
+              showTitles,
+              viewType,
+              i18n,
+              id: folder.id,
+              title: folder.name,
+              getItemIcon: () => folder.icon,
+              isChecked: isChecked(folder),
+              toggleCheckbox: (event) => toggleCheckbox(event, folder),
+              type: 'folder',
+              isDisabled: isChecked(folder) ? isChecked(folder).loading : false,
+              isCheckboxDisabled: folder.id === VIRTUAL_SHARED_DIR,
+              handleFolderClick: () => getNextFolder(folder),
+            })
+          })}
+
+          {files.map((file) => {
+            const validated = validateRestrictions(remoteFileObjToLocal(file), [
+              ...uppyFiles,
+              ...currentSelection,
+            ])
+
+            return Item({
+              id: file.id,
+              title: file.name,
+              author: file.author,
+              getItemIcon: () => file.icon,
+              isChecked: isChecked(file),
+              toggleCheckbox: (event) => toggleCheckbox(event, file),
+              columns,
+              showTitles,
+              viewType,
+              i18n,
+              type: 'file',
+              isDisabled: !validated.result && isChecked(file),
+              restrictionReason: validated.reason,
+            })
+          })}
+        </ul>
+      </div>
+
+      {selected > 0 && (
+        <FooterActions
+          selected={selected}
+          done={done}
+          cancel={cancel}
+          i18n={i18n}
+        />
+      )}
     </div>
   )
 }
-
-module.exports = Browser
