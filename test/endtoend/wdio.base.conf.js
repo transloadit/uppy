bc37c548ddac3e6a9fe468af7027d878c004ecd9
Antoine du Hamel <duhamelantoine1995@gmail.com>
2021-10-19 15:50:27 +0200

    Re-enable all e2e tests

--- test/endtoend/wdio.base.conf.js
+++ test/endtoend/wdio.base.conf.js
@@ -18,8 +18,7 @@ exports.config = {
   // directory is where your package.json resides, so `wdio` will be called from there.
   //
   specs: [
-    // './*/test.js',
-    './transloadit2/test.js',
+    './*/test.js',
   ],
 
   // Patterns to exclude.
