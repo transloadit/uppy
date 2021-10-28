21b9d641d71bb351ae0d834ca23d43ec47d8b9a8
Mikael Finstad <finstaden@gmail.com>
2021-10-28 22:18:11 +0700

    reduce nesting

--- packages/@uppy/companion/src/server/helpers/upload.js
+++ packages/@uppy/companion/src/server/helpers/upload.js
@@ -19,18 +19,14 @@ async function startDownUpload ({ req, res, getSize, download, onUnhandledError 
 
     // "Forking" off the upload operation to background, so we can return the http request:
     ;(async () => {
-      try {
-        // wait till the client has connected to the socket, before starting
-        // the download, so that the client can receive all download/upload progress.
-        logger.debug('Waiting for socket connection before beginning remote download/upload.', null, req.id)
-        await uploader.awaitReady()
-        logger.debug('Socket connection received. Starting remote download/upload.', null, req.id)
+      // wait till the client has connected to the socket, before starting
+      // the download, so that the client can receive all download/upload progress.
+      logger.debug('Waiting for socket connection before beginning remote download/upload.', null, req.id)
+      await uploader.awaitReady()
+      logger.debug('Socket connection received. Starting remote download/upload.', null, req.id)
 
-        await uploader.uploadStream(stream)
-      } catch (err) {
-        logger.error(err)
-      }
-    })()
+      await uploader.uploadStream(stream)
+    })().catch((err) => logger.error(err))
 
     // Respond the request
     // NOTE: Uploader will continue running after the http request is responded
