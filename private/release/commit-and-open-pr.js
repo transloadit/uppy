819f69de869fb56475e50a3e540d74568f2b6693
Antoine du Hamel <duhamelantoine1995@gmail.com>
2021-11-23 00:15:45 +0100

    Fix git command and workaround deleted branch

--- private/release/commit-and-open-pr.js
+++ private/release/commit-and-open-pr.js
@@ -1,6 +1,7 @@
 import { spawnSync } from 'node:child_process'
 import { fileURLToPath } from 'node:url'
 import prompts from 'prompts'
+import { REPO_OWNER, REPO_NAME } from './config.js'
 
 export default async function commit (spawnOptions, ...files) {
   console.log(`Now is the time to do manual edits to ${files.join(',')}.`)
@@ -16,6 +17,9 @@ export default async function commit (spawnOptions, ...files) {
   spawnSync('git', ['add', ...files.map(url => fileURLToPath(url))], spawnOptions)
   spawnSync('git', ['commit', '-n', '-m', 'Prepare next release'], { ...spawnOptions, stdio: 'inherit' })
   const sha = spawnSync('git', ['rev-parse', 'HEAD'], spawnOptions).stdout.toString().trim()
+  const getRemoteCommamnd = `git remote -v | grep '${REPO_OWNER}/${REPO_NAME}' | awk '($3 == "(push)") { print $1; exit }'`
+  const remote = spawnSync('/bin/sh', ['-c', getRemoteCommamnd]).stdout.toString().trim()
+                 || `git@github.com:${REPO_OWNER}/${REPO_NAME}.git`
 
-  console.log(`Please run \`git push upstream ${sha}:release\`.`)
+  console.log(`Please run \`git push ${remote} ${sha}:refs/heads/release\`.`)
 }
