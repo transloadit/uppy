47eff7487791687923c61a9f5ee717c01d6f80d6
Murderlon <merlijn@soverin.net>
2022-01-24 18:01:17 +0300

    Improve tus tests for reliability

--- e2e/cypress/integration/dashboard-tus.spec.ts
+++ e2e/cypress/integration/dashboard-tus.spec.ts
@@ -9,6 +9,8 @@ describe('Dashboard with Tus', () => {
     cy.visit('/dashboard-tus')
     cy.get('.uppy-Dashboard-input').as('file-input')
     cy.intercept('/files/*').as('tus')
+    cy.intercept('http://localhost:3020/url/*').as('url')
+    cy.intercept('http://localhost:3020/search/unsplash/*').as('unsplash')
   })
 
   it('should upload cat image successfully', () => {
@@ -46,6 +48,7 @@ describe('Dashboard with Tus', () => {
     cy.get('.uppy-Url-input').type('https://via.placeholder.com/600x400')
     cy.get('.uppy-Url-importButton').click()
     cy.get('.uppy-StatusBar-actionBtn--upload').click()
+    cy.wait('@url')
     cy.get('.uppy-StatusBar-statusPrimary').should('contain', 'Complete')
   })
 
@@ -53,14 +56,23 @@ describe('Dashboard with Tus', () => {
     cy.get('[data-cy="Unsplash"]').click()
     cy.get('.uppy-SearchProvider-input').type('book')
     cy.get('.uppy-SearchProvider-searchButton').click()
+    cy.wait('@unsplash')
     // Test that the author link is visible
     cy.get('.uppy-ProviderBrowserItem')
       .first()
-      .click()
-      .find('label a')
-      .should('have.css', 'display', 'block')
+      .within(() => {
+        cy.root().click()
+        // We have hover states that show the author
+        // but we don't have hover in e2e, so we focus after the click
+        // to get the same effect. Also tests keyboard users this way.
+        cy.get('input[type="checkbox"]').focus()
+        cy.get('a').should('have.css', 'display', 'block')
+      })
     cy.get('.uppy-c-btn-primary').click()
     cy.get('.uppy-StatusBar-actionBtn--upload').click()
+    cy.wait('@unsplash')
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.wait(2000) // bad practice, but the request is successful before the status bar updates here
     cy.get('.uppy-StatusBar-statusPrimary').should('contain', 'Complete')
   })
 })
