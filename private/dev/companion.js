027259704c07375fc83a620fe860a981e4e7fe39
Murderlon <merlijn@soverin.net>
2022-01-31 19:14:43 +0300

    Back to companion.sh because that works on CI too

--- private/dev/companion.js
+++
@@ -1,7 +0,0 @@
-import 'dotenv/config'
-import nodemon from 'nodemon'
-
-nodemon({
-  watch: 'packages/@uppy/companion/src',
-  exec: 'node packages/@uppy/companion/src/standalone/start-server.js',
-})
