d31f09f1f17f0b0b67ee774a1761e1137f052353
Murderlon <merlijn@soverin.net>
2022-02-03 16:25:51 +0300

    Use top level await in generate-test

--- e2e/generate-test.mjs
+++ e2e/generate-test.mjs
@@ -4,41 +4,41 @@ import prompts from 'prompts'
 import fs from 'node:fs/promises'
 import dedent from 'dedent'
 
-(async () => {
-  const packageNames = await fs.readdir(new URL('../packages/@uppy', import.meta.url))
-  const unwantedPackages = ['core', 'companion', 'redux-dev-tools', 'utils']
+const packageNames = await fs.readdir(new URL('../packages/@uppy', import.meta.url))
+const unwantedPackages = ['core', 'companion', 'redux-dev-tools', 'utils']
 
-  const { name } = await prompts({
-    type: 'text',
-    name: 'name',
-    message: 'What should the name of the test be (e.g `dashboard-tus`)?',
-    validate: (value) => /^[a-z|-]+$/i.test(value),
-  })
+const { name } = await prompts({
+  type: 'text',
+  name: 'name',
+  message: 'What should the name of the test be (e.g `dashboard-tus`)?',
+  validate: (value) => /^[a-z|-]+$/i.test(value),
+})
 
-  const { packages } = await prompts({
-    type: 'multiselect',
-    name: 'packages',
-    message: 'What packages do you want to test?',
-    hint: '@uppy/core is automatically included',
-    choices: packageNames
-      .filter((pkg) => !unwantedPackages.includes(pkg))
-      .map((pkg) => ({ title: pkg, value: pkg })),
-  })
+const { packages } = await prompts({
+  type: 'multiselect',
+  name: 'packages',
+  message: 'What packages do you want to test?',
+  hint: '@uppy/core is automatically included',
+  choices: packageNames
+    .filter((pkg) => !unwantedPackages.includes(pkg))
+    .map((pkg) => ({ title: pkg, value: pkg })),
+})
 
-  const camelcase = (str) => str
+const camelcase = (str) =>
+  str
     .toLowerCase()
     .replace(/([-][a-z])/g, (group) => group.toUpperCase().replace('-', ''))
 
-  const testUrl = new URL(`cypress/integration/${name}.spec.ts`, import.meta.url)
-  const test = dedent`
+const testUrl = new URL(`cypress/integration/${name}.spec.ts`, import.meta.url)
+const test = dedent`
     describe('${name}', () => {
       beforeEach(() => {
         cy.visit('/${name}')
       })
     })
   `
-  const htmlUrl = new URL(`clients/${name}/index.html`, import.meta.url)
-  const html = dedent`
+const htmlUrl = new URL(`clients/${name}/index.html`, import.meta.url)
+const html = dedent`
     <!doctype html>
     <html lang="en">
       <head>
@@ -52,9 +52,9 @@ import dedent from 'dedent'
     </html>
   `
 
-  const appUrl = new URL(`clients/${name}/app.js`, import.meta.url)
-  // dedent is acting weird for this one but this formatting fixes it.
-  const app = dedent`
+const appUrl = new URL(`clients/${name}/app.js`, import.meta.url)
+// dedent is acting weird for this one but this formatting fixes it.
+const app = dedent`
 import Uppy from '@uppy/core'
 ${packages.map((pgk) => `import ${camelcase(pgk)} from '@uppy/${pgk}'`).join('\n')}
 
@@ -65,20 +65,22 @@ const uppy = new Uppy()
 window.uppy = uppy
   `
 
-  await fs.writeFile(testUrl, test)
-  await fs.mkdir(new URL(`clients/${name}`, import.meta.url))
-  await fs.writeFile(htmlUrl, html)
-  await fs.writeFile(appUrl, app)
+await fs.writeFile(testUrl, test)
+await fs.mkdir(new URL(`clients/${name}`, import.meta.url))
+await fs.writeFile(htmlUrl, html)
+await fs.writeFile(appUrl, app)
 
-  const homeUrl = new URL('clients/index.html', import.meta.url)
-  const home = await fs.readFile(homeUrl, 'utf8')
-  const newHome = home.replace('</ul>', `\t<li><a href="${name}/index.html">${name}</a></li>\n\t\t\t</ul>`)
-  await fs.writeFile(homeUrl, newHome)
+const homeUrl = new URL('clients/index.html', import.meta.url)
+const home = await fs.readFile(homeUrl, 'utf8')
+const newHome = home.replace(
+  '</ul>',
+  `\t<li><a href="${name}/index.html">${name}</a></li>\n\t\t\t</ul>`
+)
+await fs.writeFile(homeUrl, newHome)
 
-  const prettyPath = url => url.toString().split('uppy', 2)[1]
+const prettyPath = (url) => url.toString().split('uppy', 2)[1]
 
-  console.log(`✅ Generated ${prettyPath(testUrl)}`)
-  console.log(`✅ Generated ${prettyPath(htmlUrl)}`)
-  console.log(`✅ Generated ${prettyPath(appUrl)}`)
-  console.log(`✅ Updated ${prettyPath(homeUrl)}`)
-})()
+console.log(`✅ Generated ${prettyPath(testUrl)}`)
+console.log(`✅ Generated ${prettyPath(htmlUrl)}`)
+console.log(`✅ Generated ${prettyPath(appUrl)}`)
+console.log(`✅ Updated ${prettyPath(homeUrl)}`)
