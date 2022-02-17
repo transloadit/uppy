03ce60675263a77fc7243942c40d34be409b2894
Mikael Finstad <finstaden@gmail.com>
2022-02-17 21:26:28 +0800

    meta: use a single `.env` file for config
    Fixes: https://github.com/transloadit/uppy/issues/3473
    Co-authored-by: Antoine du Hamel <duhamelantoine1995@gmail.com>
    Co-authored-by: Mikael Finstad <finstaden@gmail.com>

--- bin/companion.sh
+++ bin/companion.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env bash
 
 # Load local env vars. In CI, these are injected.
-if [ -f .env.local ]; then
-  # https://gist.github.com/mihow/9c7f559807069a03e302605691f85572?permalink_comment_id=3625310#gistcomment-3625310
-  set -a
-  source <(sed -e '/^#/d;/^\s*$/d' -e "s/'/'\\\''/g" -e "s/=\(.*\)/='\1'/g" .env.local)
-  set +a
-  nodemon --watch packages/@uppy/companion/src --exec node ./packages/@uppy/companion/src/standalone/start-server.js
+if [ -f .env ]; then
+  nodemon --watch packages/@uppy/companion/src --exec node -r dotenv/config ./packages/@uppy/companion/src/standalone/start-server.js
 else
   env \
     COMPANION_DATADIR="./output" \
