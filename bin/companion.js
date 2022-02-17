57f6c7899d97c2c8e68692a2d7f3f8ce39c997c7
Mikael Finstad <finstaden@gmail.com>
2022-02-17 21:26:28 +0800

    fix broken companion dev #3473

---
+++ bin/companion.js
@@ -0,0 +1,2 @@
+require('dotenv').config({ path: '.env.local' })
+require('../packages/@uppy/companion/src/standalone/start-server')
--- bin/companion.sh
+++ bin/companion.sh
@@ -2,11 +2,7 @@
 
 # Load local env vars. In CI, these are injected.
 if [ -f .env.local ]; then
-  # https://gist.github.com/mihow/9c7f559807069a03e302605691f85572?permalink_comment_id=3625310#gistcomment-3625310
-  set -a
-  source <(sed -e '/^#/d;/^\s*$/d' -e "s/'/'\\\''/g" -e "s/=\(.*\)/='\1'/g" .env.local)
-  set +a
-  nodemon --watch packages/@uppy/companion/src --exec node ./packages/@uppy/companion/src/standalone/start-server.js
+  nodemon --watch packages/@uppy/companion/src ./bin/companion.js
 else
   env \
     COMPANION_DATADIR="./output" \
