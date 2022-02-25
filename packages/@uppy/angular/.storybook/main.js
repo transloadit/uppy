a2acad80ccea1ff74377ce1a97b2c3fa86fa4149
Antoine du Hamel <duhamelantoine1995@gmail.com>
2022-02-25 21:10:16 +0100

    Add Storybook back

---
+++ packages/@uppy/angular/.storybook/main.js
@@ -0,0 +1,15 @@
+module.exports = {
+  "stories": [
+    "../stories/**/*.stories.mdx",
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions"
+  ],
+  "framework": "@storybook/angular",
+  "core": {
+    "builder": "webpack5"
+  }
+}
---
+++ packages/@uppy/angular/.storybook/preview.js
@@ -0,0 +1,14 @@
+import { setCompodocJson } from "@storybook/addon-docs/angular";
+import docJson from "../documentation.json";
+setCompodocJson(docJson);
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+  docs: { inlineStories: true },
+}
