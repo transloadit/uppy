68842fb8701c60a118dc6364ae3731640c01e51f
Antoine du Hamel <duhamelantoine1995@gmail.com>
2022-03-03 11:16:27 +0100

    @uppy/companion: reorder reqToOptions
    Giving a clear separation between client-provided info and server
    provided ones.

--- packages/@uppy/companion/src/server/Uploader.js
+++ packages/@uppy/companion/src/server/Uploader.js
@@ -255,22 +255,24 @@ class Uploader {
     const useFormData = useFormDataIsSet ? req.body.useFormData : true
 
     return {
-      companionOptions: req.companion.options,
+      // Client provided info.
+      headers: req.body.headers,
+      httpMethod: req.body.httpMethod,
+      protocol: req.body.protocol,
       endpoint: req.body.endpoint,
       uploadUrl: req.body.uploadUrl,
-      protocol: req.body.protocol,
       metadata: req.body.metadata,
-      httpMethod: req.body.httpMethod,
-      useFormData,
-      size,
       fieldname: req.body.fieldname,
+      useFormData,
+      // Server provided info.
+      size,
+      companionOptions: req.companion.options,
       pathPrefix: `${req.companion.options.filePath}`,
       storage: redis.client(),
       s3: req.companion.s3Client ? {
         client: req.companion.s3Client,
         options: req.companion.options.providerOptions.s3,
       } : null,
-      headers: req.body.headers,
       chunkSize: req.companion.options.chunkSize,
     }
   }
