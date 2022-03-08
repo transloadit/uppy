92da854730c8a51f0365a91ee00b3bb171e05756
Mikael Finstad <finstaden@gmail.com>
2022-03-08 15:40:48 +0800

    fix lint

--- packages/@uppy/companion/src/companion.js
+++ packages/@uppy/companion/src/companion.js
@@ -47,112 +47,6 @@ const defaultOptions = {
   streamingUpload: false,
 }
 
-// make the errors available publicly for custom providers
-module.exports.errors = { ProviderApiError, ProviderAuthError }
-module.exports.socket = require('./server/socket')
-
-/**
- * Entry point into initializing the Companion app.
- *
- * @param {object} options
- * @returns {import('express').Express}
- */
-module.exports.app = (options = {}) => {
-  validateConfig(options)
-
-  options = merge({}, defaultOptions, options)
-  const providers = providerManager.getDefaultProviders()
-  const searchProviders = providerManager.getSearchProviders()
-  providerManager.addProviderOptions(options, grantConfig)
-
-  const { customProviders } = options
-  if (customProviders) {
-    providerManager.addCustomProviders(customProviders, providers, grantConfig)
-  }
-
-  // mask provider secrets from log messages
-  maskLogger(options)
-
-  // create singleton redis client
-  if (options.redisUrl) {
-    redis.client(merge({ url: options.redisUrl }, options.redisOptions || {}))
-  }
-  emitter(options.multipleInstances && options.redisUrl, options.redisPubSubScope)
-
-  const app = express()
-
-  if (options.metrics) {
-    app.use(middlewares.metrics({ path: options.server.path }))
-
-    // backward compatibility
-    // TODO remove in next major semver
-    if (options.server.path) {
-      const buildUrl = getURLBuilder(options)
-      app.get('/metrics', (req, res) => {
-        process.emitWarning('/metrics is deprecated when specifying a path to companion')
-        const metricsUrl = buildUrl('/metrics', true)
-        res.redirect(metricsUrl)
-      })
-    }
-  }
-
-  app.use(cookieParser()) // server tokens are added to cookies
-
-  app.use(interceptGrantErrorResponse)
-  // override provider credentials at request time
-  app.use('/connect/:authProvider/:override?', getCredentialsOverrideMiddleware(providers, options))
-  app.use(Grant(grantConfig))
-
-  app.use((req, res, next) => {
-    if (options.sendSelfEndpoint) {
-      const { protocol } = options.server
-      res.header('i-am', `${protocol}://${options.sendSelfEndpoint}`)
-    }
-    next()
-  })
-
-  app.use(middlewares.cors(options))
-
-  // add uppy options to the request object so it can be accessed by subsequent handlers.
-  app.use('*', getOptionsMiddleware(options))
-  app.use('/s3', s3(options.providerOptions.s3))
-  app.use('/url', url())
-
-  app.post('/:providerName/preauth', middlewares.hasSessionAndProvider, controllers.preauth)
-  app.get('/:providerName/connect', middlewares.hasSessionAndProvider, controllers.connect)
-  app.get('/:providerName/redirect', middlewares.hasSessionAndProvider, controllers.redirect)
-  app.get('/:providerName/callback', middlewares.hasSessionAndProvider, controllers.callback)
-  app.post('/:providerName/deauthorization/callback', middlewares.hasSessionAndProvider, controllers.deauthorizationCallback)
-  app.get('/:providerName/logout', middlewares.hasSessionAndProvider, middlewares.gentleVerifyToken, controllers.logout)
-  app.get('/:providerName/send-token', middlewares.hasSessionAndProvider, middlewares.verifyToken, controllers.sendToken)
-  app.get('/:providerName/list/:id?', middlewares.hasSessionAndProvider, middlewares.verifyToken, controllers.list)
-  app.post('/:providerName/get/:id', middlewares.hasSessionAndProvider, middlewares.verifyToken, controllers.get)
-  app.get('/:providerName/thumbnail/:id', middlewares.hasSessionAndProvider, middlewares.cookieAuthToken, middlewares.verifyToken, controllers.thumbnail)
-  // @ts-ignore Type instantiation is excessively deep and possibly infinite.
-  app.get('/search/:searchProviderName/list', middlewares.hasSearchQuery, middlewares.loadSearchProviderToken, controllers.list)
-  app.post('/search/:searchProviderName/get/:id', middlewares.loadSearchProviderToken, controllers.get)
-
-  app.param('providerName', providerManager.getProviderMiddleware(providers, true))
-  app.param('searchProviderName', providerManager.getProviderMiddleware(searchProviders))
-
-  if (app.get('env') !== 'test') {
-    jobs.startCleanUpJob(options.filePath)
-  }
-
-  const processId = uuid.v4()
-
-  jobs.startPeriodicPingJob({
-    urls: options.periodicPingUrls,
-    interval: options.periodicPingInterval,
-    count: options.periodicPingCount,
-    staticPayload: options.periodicPingStaticPayload,
-    version,
-    processId,
-  })
-
-  return app
-}
-
 // intercepts grantJS' default response error when something goes
 // wrong during oauth process.
 const interceptGrantErrorResponse = interceptor((req, res) => {
@@ -289,4 +183,110 @@ const validateConfig = (companionOptions) => {
   )) {
     throw new TypeError('Invalid periodicPingUrls')
   }
+}
+
+// make the errors available publicly for custom providers
+module.exports.errors = { ProviderApiError, ProviderAuthError }
+module.exports.socket = require('./server/socket')
+
+/**
+ * Entry point into initializing the Companion app.
+ *
+ * @param {object} optionsArg
+ * @returns {import('express').Express}
+ */
+module.exports.app = (optionsArg = {}) => {
+  validateConfig(optionsArg)
+
+  const options = merge({}, defaultOptions, optionsArg)
+  const providers = providerManager.getDefaultProviders()
+  const searchProviders = providerManager.getSearchProviders()
+  providerManager.addProviderOptions(options, grantConfig)
+
+  const { customProviders } = options
+  if (customProviders) {
+    providerManager.addCustomProviders(customProviders, providers, grantConfig)
+  }
+
+  // mask provider secrets from log messages
+  maskLogger(options)
+
+  // create singleton redis client
+  if (options.redisUrl) {
+    redis.client(merge({ url: options.redisUrl }, options.redisOptions || {}))
+  }
+  emitter(options.multipleInstances && options.redisUrl, options.redisPubSubScope)
+
+  const app = express()
+
+  if (options.metrics) {
+    app.use(middlewares.metrics({ path: options.server.path }))
+
+    // backward compatibility
+    // TODO remove in next major semver
+    if (options.server.path) {
+      const buildUrl = getURLBuilder(options)
+      app.get('/metrics', (req, res) => {
+        process.emitWarning('/metrics is deprecated when specifying a path to companion')
+        const metricsUrl = buildUrl('/metrics', true)
+        res.redirect(metricsUrl)
+      })
+    }
+  }
+
+  app.use(cookieParser()) // server tokens are added to cookies
+
+  app.use(interceptGrantErrorResponse)
+  // override provider credentials at request time
+  app.use('/connect/:authProvider/:override?', getCredentialsOverrideMiddleware(providers, options))
+  app.use(Grant(grantConfig))
+
+  app.use((req, res, next) => {
+    if (options.sendSelfEndpoint) {
+      const { protocol } = options.server
+      res.header('i-am', `${protocol}://${options.sendSelfEndpoint}`)
+    }
+    next()
+  })
+
+  app.use(middlewares.cors(options))
+
+  // add uppy options to the request object so it can be accessed by subsequent handlers.
+  app.use('*', getOptionsMiddleware(options))
+  app.use('/s3', s3(options.providerOptions.s3))
+  app.use('/url', url())
+
+  app.post('/:providerName/preauth', middlewares.hasSessionAndProvider, controllers.preauth)
+  app.get('/:providerName/connect', middlewares.hasSessionAndProvider, controllers.connect)
+  app.get('/:providerName/redirect', middlewares.hasSessionAndProvider, controllers.redirect)
+  app.get('/:providerName/callback', middlewares.hasSessionAndProvider, controllers.callback)
+  app.post('/:providerName/deauthorization/callback', middlewares.hasSessionAndProvider, controllers.deauthorizationCallback)
+  app.get('/:providerName/logout', middlewares.hasSessionAndProvider, middlewares.gentleVerifyToken, controllers.logout)
+  app.get('/:providerName/send-token', middlewares.hasSessionAndProvider, middlewares.verifyToken, controllers.sendToken)
+  app.get('/:providerName/list/:id?', middlewares.hasSessionAndProvider, middlewares.verifyToken, controllers.list)
+  app.post('/:providerName/get/:id', middlewares.hasSessionAndProvider, middlewares.verifyToken, controllers.get)
+  app.get('/:providerName/thumbnail/:id', middlewares.hasSessionAndProvider, middlewares.cookieAuthToken, middlewares.verifyToken, controllers.thumbnail)
+  // @ts-ignore Type instantiation is excessively deep and possibly infinite.
+  app.get('/search/:searchProviderName/list', middlewares.hasSearchQuery, middlewares.loadSearchProviderToken, controllers.list)
+  app.post('/search/:searchProviderName/get/:id', middlewares.loadSearchProviderToken, controllers.get)
+
+  app.param('providerName', providerManager.getProviderMiddleware(providers, true))
+  app.param('searchProviderName', providerManager.getProviderMiddleware(searchProviders))
+
+  if (app.get('env') !== 'test') {
+    jobs.startCleanUpJob(options.filePath)
+  }
+
+  const processId = uuid.v4()
+
+  jobs.startPeriodicPingJob({
+    urls: options.periodicPingUrls,
+    interval: options.periodicPingInterval,
+    count: options.periodicPingCount,
+    staticPayload: options.periodicPingStaticPayload,
+    version,
+    processId,
+  })
+
+  return app
 }
