e0f3479ec71408930d7b90dc3f48f37c46db93a0
Antoine du Hamel <duhamelantoine1995@gmail.com>
2022-04-25 17:23:37 +0200

    @uppy/facebook: refactor to ESM (#3653)

--- .eslintrc.js
+++ .eslintrc.js
@@ -200,10 +200,11 @@ module.exports = {
         // Packages that have switched to ESM sources:
         'packages/@uppy/audio/src/**/*.js',
         'packages/@uppy/box/src/**/*.js',
+        'packages/@uppy/compressor/src/**/*.js',
         'packages/@uppy/drag-drop/src/**/*.js',
         'packages/@uppy/drop-target/src/**/*.js',
         'packages/@uppy/dropbox/src/**/*.js',
-        'packages/@uppy/compressor/src/**/*.js',
+        'packages/@uppy/facebook/src/**/*.js',
         'packages/@uppy/file-input/src/**/*.js',
         'packages/@uppy/form/src/**/*.js',
         'packages/@uppy/vue/src/**/*.js',
