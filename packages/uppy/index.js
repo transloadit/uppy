651522c455e9618d384ec81fe8f9ee3dc822b6c5
Antoine du Hamel <duhamelantoine1995@gmail.com>
2022-05-17 17:34:02 +0200

    uppy: add Zoom plugin to the bundle and fix ESM exports
    Fixes: https://github.com/transloadit/uppy/issues/3684

--- packages/uppy/index.js
+++ packages/uppy/index.js
@@ -19,23 +19,24 @@ exports.Dashboard = require('@uppy/dashboard')
 exports.DragDrop = require('@uppy/drag-drop')
 exports.DropTarget = require('@uppy/drop-target')
 exports.FileInput = require('@uppy/file-input')
+exports.ImageEditor = require('@uppy/image-editor')
 exports.Informer = require('@uppy/informer')
 exports.ProgressBar = require('@uppy/progress-bar')
 exports.StatusBar = require('@uppy/status-bar')
-exports.ImageEditor = require('@uppy/image-editor')
 
 // Acquirers
+exports.Audio = require('@uppy/audio')
+exports.Box = require('@uppy/box')
 exports.Dropbox = require('@uppy/dropbox')
+exports.Facebook = require('@uppy/facebook')
 exports.GoogleDrive = require('@uppy/google-drive')
 exports.Instagram = require('@uppy/instagram')
 exports.OneDrive = require('@uppy/onedrive')
-exports.Box = require('@uppy/box')
-exports.Facebook = require('@uppy/facebook')
+exports.ScreenCapture = require('@uppy/screen-capture')
 exports.Unsplash = require('@uppy/unsplash')
 exports.Url = require('@uppy/url')
 exports.Webcam = require('@uppy/webcam')
-exports.ScreenCapture = require('@uppy/screen-capture')
-exports.Audio = require('@uppy/audio')
+exports.Zoom = require('@uppy/zoom')
 
 // Uploaders
 exports.AwsS3 = require('@uppy/aws-s3')
@@ -45,10 +46,10 @@ exports.Tus = require('@uppy/tus')
 exports.XHRUpload = require('@uppy/xhr-upload')
 
 // Miscellaneous
+exports.Compressor = require('@uppy/compressor')
 exports.Form = require('@uppy/form')
 exports.GoldenRetriever = require('@uppy/golden-retriever')
 exports.ReduxDevTools = require('@uppy/redux-dev-tools')
 exports.ThumbnailGenerator = require('@uppy/thumbnail-generator')
-exports.Compressor = require('@uppy/compressor')
 
 exports.locales = {}
