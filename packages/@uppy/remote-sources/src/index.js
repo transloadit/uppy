18b1172d5dd548a605288cf1e998dfe49c7780d6
Artur Paikin <artur@arturpaikin.com>
2022-06-01 17:59:24 +0100

    Store installedPlugins in a Set

--- packages/@uppy/remote-sources/src/index.js
+++ packages/@uppy/remote-sources/src/index.js
@@ -27,6 +27,8 @@ const availablePlugins = [
 export default class RemoteSources extends BasePlugin {
   static VERSION = packageJson.version
 
+  #installedPlugins = new Set()
+
   constructor (uppy, opts) {
     super(uppy, opts)
     this.id = this.opts.id || 'RemoteSources'
@@ -55,17 +57,19 @@ export default class RemoteSources extends BasePlugin {
   }
 
   install () {
-    this.opts.sources.forEach((pluginName) => {
+    this.opts.sources.forEach((pluginId) => {
       const optsForRemoteSourcePlugin = { ...this.opts, sources: undefined }
-      const plugin = availablePlugins.find(p => p.name === pluginName)
+      const plugin = availablePlugins.find(p => p.name === pluginId)
       this.uppy.use(plugin, optsForRemoteSourcePlugin)
+      this.#installedPlugins.add(pluginId)
     })
   }
 
   uninstall () {
-    this.opts.sources.forEach((pluginName) => {
-      const plugin = this.uppy.getPlugin(pluginName)
+    for (const pluginId of this.#installedPlugins) {
+      const plugin = this.uppy.getPlugin(pluginId)
       this.uppy.removePlugin(plugin)
-    })
+      this.#installedPlugins.delete(pluginId)
+    }
   }
 }
