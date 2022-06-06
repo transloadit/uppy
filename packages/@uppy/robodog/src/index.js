304164b4f69d7af4dd77f17d67f2aa6262ecbed1
Antoine du Hamel <duhamelantoine1995@gmail.com>
2022-06-06 18:11:37 +0200

    I keep forgetting about `babel-plugin-inline-package-json`

--- packages/@uppy/robodog/src/index.js
+++ packages/@uppy/robodog/src/index.js
@@ -2,12 +2,13 @@ const form = require('./form')
 const dashboard = require('./dashboard')
 const pick = require('./pick')
 const upload = require('./upload')
-const { version } = require('../package.json')
 
 module.exports = {
   dashboard,
   form,
   pick,
   upload,
-  VERSION: version,
+  // We need to keep the require here because we're using `babel-plugin-inline-package-json`.
+  // eslint-disable-next-line global-require
+  VERSION: require('../package.json').version,
 }
