9d9c2108cf0341a14c47303064262c750cdb7aa0
Murderlon <merlijn@soverin.net>
2022-07-27 17:28:03 +0200

    Check value of rejects in test

--- packages/@uppy/aws-s3-multipart/src/index.test.js
+++ packages/@uppy/aws-s3-multipart/src/index.test.js
@@ -261,7 +261,7 @@ describe('AwsS3Multipart', () => {
         }),
       })
 
-      await expect(core.upload()).rejects.toBeDefined()
+      await expect(core.upload()).rejects.toEqual({ source: { status: 500 } })
 
       expect(awsS3Multipart.opts.prepareUploadParts.mock.calls.length).toEqual(2)
       expect(mock.mock.calls.length).toEqual(1)
