268cbc2f18f5916fae6c6e4bbb0362f400416ea3
Murderlon <merlijn@soverin.net>
2022-07-27 17:11:58 +0200

    Add test

--- e2e/cypress/integration/dashboard-ui.spec.ts
+++ e2e/cypress/integration/dashboard-ui.spec.ts
@@ -4,6 +4,14 @@ describe('dashboard-ui', () => {
     cy.get('.uppy-Dashboard-input:first').as('file-input')
   })
 
+  it('should not throw when calling uppy.close()', () => {
+    cy.get('@file-input').selectFile(['cypress/fixtures/images/cat.jpg', 'cypress/fixtures/images/traffic.jpg'], { force:true })
+
+    cy.window().then(({ uppy }) => {
+      expect(uppy.close()).to.not.throw
+    })
+  })
+
   it('should render thumbnails', () => {
     cy.get('@file-input').selectFile(['cypress/fixtures/images/cat.jpg', 'cypress/fixtures/images/traffic.jpg'], { force:true })
     cy.get('.uppy-Dashboard-Item-previewImg')
