ce8b631e18de1698ee89306d1ad39cedd15fa38e
Antoine du Hamel <duhamelantoine1995@gmail.com>
2022-08-02 22:52:28 +0200

    split flaky and non-flaky parts of the test

--- e2e/cypress/integration/dashboard-transloadit.spec.ts
+++ e2e/cypress/integration/dashboard-transloadit.spec.ts
@@ -23,7 +23,34 @@ describe('Dashboard with Transloadit', () => {
     cy.get('.uppy-StatusBar-statusPrimary').should('contain', 'Complete')
   })
 
-  it('should close assembly polling when cancelled', FLAKY, () => {
+  it('should close assembly polling when cancelled', () => {
+    cy.get('@file-input').selectFile(['cypress/fixtures/images/cat.jpg', 'cypress/fixtures/images/traffic.jpg'], { force:true })
+    cy.get('.uppy-StatusBar-actionBtn--upload').click()
+
+    cy.intercept({
+      method: 'GET',
+      url: '/assemblies/*',
+    }).as('assemblyPolling')
+    cy.intercept(
+      { method: 'PATCH', pathname: '/files/*', times: 1 },
+      { statusCode: 204, body: {} },
+    )
+    cy.intercept(
+      { method: 'DELETE', pathname: '/resumable/files/*', times: 1 },
+      { statusCode: 204, body: {} },
+    )
+    cy.wait('@assemblyPolling')
+    cy.window().then(({ uppy }) => {
+      expect(Object.values(uppy.getPlugin('Transloadit').activeAssemblies).every((a: any) => a.pollInterval)).to.equal(true)
+    })
+    cy.get('button[data-cy=cancel]').click()
+
+    cy.window().then(({ uppy }) => {
+      expect(Object.values(uppy.getPlugin('Transloadit').activeAssemblies).some((a: any) => a.pollInterval)).to.equal(false)
+    })
+  })
+
+  it('should emit one assembly-cancelled event when cancelled', FLAKY, () => {
     const spy = cy.spy()
 
     cy.get('@file-input').selectFile(['cypress/fixtures/images/cat.jpg', 'cypress/fixtures/images/traffic.jpg'], { force:true })
@@ -47,13 +74,9 @@ describe('Dashboard with Transloadit', () => {
     cy.wait('@assemblyPolling')
     cy.window().then(({ uppy }) => {
       uppy.on('transloadit:assembly-cancelled', spy)
-      expect(Object.values(uppy.getPlugin('Transloadit').activeAssemblies).every((a: any) => a.pollInterval)).to.equal(true)
     })
     cy.get('button[data-cy=cancel]').click()
 
-    cy.window().then(({ uppy }) => {
-      expect(Object.values(uppy.getPlugin('Transloadit').activeAssemblies).some((a: any) => a.pollInterval)).to.equal(false)
-    })
     cy.wait('@assemblyDeletion').then(() => {
       expect(spy).to.be.calledOnce
     })
