1336565467249e5b26e2ee1c83363da0e16c9e71
Antoine du Hamel <duhamelantoine1995@gmail.com>
2022-08-03 16:01:10 +0200

    Update examples/aws-companion/server.cjs
    Co-authored-by: Merlijn Vos <merlijn@soverin.net>

--- examples/aws-companion/server.cjs
+++ examples/aws-companion/server.cjs
@@ -1,5 +1,6 @@
 const fs = require('node:fs')
 const path = require('node:path')
+const crypto = require('node:crypto')
 const companion = require('@uppy/companion')
 
 require('dotenv').config({ path: path.join(__dirname, '..', '..', '.env') })
