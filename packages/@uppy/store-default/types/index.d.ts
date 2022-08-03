6f3380b0568bed3541ac49ed7481df89adad3169
Antoine du Hamel <duhamelantoine1995@gmail.com>
2022-08-03 11:19:09 +0200

    @uppy/store-default: export the class, don't expose `.callbacks` (#3928)

--- packages/@uppy/store-default/types/index.d.ts
+++ packages/@uppy/store-default/types/index.d.ts
@@ -3,7 +3,7 @@ import type { Store } from '@uppy/utils'
 type State = Record<string, unknown>
 type StateChangeListener = (prevState: State, nextState: State, patch: State) => void
 
-declare class DefaultStore implements Store {
+export default class DefaultStore implements Store {
   constructor ()
 
   getState (): State
@@ -12,7 +12,3 @@ declare class DefaultStore implements Store {
 
   subscribe (listener: StateChangeListener): () => void
 }
-
-declare function createDefaultStore (): DefaultStore
-
-export default createDefaultStore
