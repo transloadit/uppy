8b114226041aa997aad3599bd7b13edb45bf4261
tyndria <tyndria23@gmail.com>
2022-08-05 21:04:02 +0400

    Export AwsS3UploadParameters & AwsS3Options interfaces

--- packages/@uppy/aws-s3/types/index.d.ts
+++ packages/@uppy/aws-s3/types/index.d.ts
@@ -2,14 +2,14 @@ import type { PluginOptions, BasePlugin, UppyFile } from '@uppy/core'
 
 type MaybePromise<T> = T | Promise<T>
 
-interface AwsS3UploadParameters {
+export interface AwsS3UploadParameters {
     method?: string
     url: string
     fields?: { [type: string]: string }
     headers?: { [type: string]: string }
 }
 
-interface AwsS3Options extends PluginOptions {
+export interface AwsS3Options extends PluginOptions {
     companionUrl?: string
     getUploadParameters?: (file: UppyFile) => MaybePromise<AwsS3UploadParameters>
     metaFields?: string[]
