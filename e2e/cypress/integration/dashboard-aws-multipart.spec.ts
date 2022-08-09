4211d66f36f384c60f5d3734a8de04f2cc3d5d22
Murderlon <merlijn@soverin.net>
2022-08-09 15:56:14 +0200

    Fix selectFile

--- e2e/cypress/integration/dashboard-aws-multipart.spec.ts
+++ e2e/cypress/integration/dashboard-aws-multipart.spec.ts
@@ -1,11 +1,11 @@
 describe('dashboard-aws', () => {
   beforeEach(() => {
     cy.visit('/dashboard-aws')
-    cy.get('.uppy-Dashboard-input').as('file-input')
+    cy.get('.uppy-Dashboard-input:first').as('file-input')
   })
 
   it('should upload cat image successfully', () => {
-    cy.get('@file-input').attachFile('images/cat.jpg')
+    cy.get('@file-input').selectFile(['cypress/fixtures/images/cat.jpg', 'cypress/fixtures/images/traffic.jpg'], { force:true })
     cy.get('.uppy-StatusBar-actionBtn--upload').click()
 
     cy.get('.uppy-StatusBar-statusPrimary').should('contain', 'Complete')
