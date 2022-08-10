transloadit/uppy

commit 22c0218f18dc7f4d116a8d8194472a6eb3795127

do not use a default upload protocol (#3834)

packages/@uppy/companion/src/server/Uploader.js
@@ -93,9 +93,8 @@ function validateOptions (options) {
   }
 
   // validate protocol
-  // @todo this validation should not be conditional once the protocol field is mandatory
-  if (options.protocol && !Object.keys(PROTOCOLS).some((key) => PROTOCOLS[key] === options.protocol)) {
-    throw new ValidationError('unsupported protocol specified')
+  if (options.protocol == null || !Object.keys(PROTOCOLS).some((key) => PROTOCOLS[key] === options.protocol)) {
+    throw new ValidationError('please specify a valid protocol')
   }
 
   // s3 uploads don't require upload destination
@@ -208,8 +207,7 @@ class Uploader {
   }
 
   async _uploadByProtocol () {
-    // @todo a default protocol should not be set. We should ensure that the user specifies their protocol.
-    const protocol = this.options.protocol || PROTOCOLS.multipart
+    const { protocol } = this.options
 
     switch (protocol) {
       case PROTOCOLS.multipart:

packages/@uppy/companion/test/__tests__/companion.js
@@ -36,7 +36,7 @@ describe('validate upload data', () => {
         protocol: 'tusInvalid',
       })
       .expect(400)
-      .then((res) => expect(res.body.message).toBe('unsupported protocol specified'))
+      .then((res) => expect(res.body.message).toBe('please specify a valid protocol'))
   })
 
   test('invalid upload fieldname gets rejected', () => {

packages/@uppy/companion/test/__tests__/uploader.js
@@ -20,10 +20,13 @@ process.env.COMPANION_DATADIR = './test/output'
 process.env.COMPANION_DOMAIN = 'localhost:3020'
 const { companionOptions } = standalone()
 
+const protocol = 'tus'
+
 describe('uploader with tus protocol', () => {
   test('uploader respects uploadUrls', async () => {
     const opts = {
       endpoint: 'http://localhost/files',
+      protocol,
       companionOptions: { ...companionOptions, uploadUrls: [/^http:\/\/url.myendpoint.com\//] },
     }
 
@@ -33,6 +36,7 @@ describe('uploader with tus protocol', () => {
   test('uploader respects uploadUrls, valid', async () => {
     const opts = {
       endpoint: 'http://url.myendpoint.com/files',
+      protocol,
       companionOptions: { ...companionOptions, uploadUrls: [/^http:\/\/url.myendpoint.com\//] },
     }
 
@@ -43,6 +47,7 @@ describe('uploader with tus protocol', () => {
   test('uploader respects uploadUrls, localhost', async () => {
     const opts = {
       endpoint: 'http://localhost:1337/',
+      protocol,
       companionOptions: { ...companionOptions, uploadUrls: [/^http:\/\/localhost:1337\//] },
     }
 
@@ -226,6 +231,7 @@ describe('uploader with tus protocol', () => {
     const opts = {
       companionOptions,
       endpoint: 'http://localhost',
+      protocol,
     }
 
     // eslint-disable-next-line no-new
@@ -247,6 +253,7 @@ describe('uploader with tus protocol', () => {
   test('uploader respects maxFileSize correctly', async () => {
     const opts = {
       endpoint: 'http://url.myendpoint.com/files',
+      protocol,
       companionOptions: { ...companionOptions, maxFileSize: 100 },
       size: 99,
     }

commit 66698a9842329ee7245fef460a15a5598f292c8e

add (failing) e2e test for remote xhr (multipart)
regression caused by #3834

e2e/clients/dashboard-xhr/app.js
@@ -1,0 +1,18 @@
+import { Uppy } from '@uppy/core'
+import Dashboard from '@uppy/dashboard'
+import XHRUpload from '@uppy/xhr-upload'
+import Unsplash from '@uppy/unsplash'
+import Url from '@uppy/url'
+
+import '@uppy/core/dist/style.css'
+import '@uppy/dashboard/dist/style.css'
+
+const companionUrl = 'http://localhost:3020'
+const uppy = new Uppy()
+  .use(Dashboard, { target: '#app', inline: true })
+  .use(XHRUpload, { endpoint: 'https://xhr-server.herokuapp.com/upload', limit: 6 })
+  .use(Url, { target: Dashboard, companionUrl })
+  .use(Unsplash, { target: Dashboard, companionUrl })
+
+// Keep this here to access uppy in tests
+window.uppy = uppy

e2e/clients/dashboard-xhr/index.html
@@ -1,0 +1,11 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8"/>
+    <title>dashboard-xhr</title>
+    <script defer type="module" src="app.js"></script>
+  </head>
+  <body>
+    <div id="app"></div>
+  </body>
+</html>

e2e/clients/index.html
@@ -12,6 +12,7 @@
 				<li><a href="react/index.html">react</a></li>
         <li><a href="dashboard-transloadit/index.html">dashboard-transloadit</a></li>
         <li><a href="dashboard-tus/index.html">dashboard-tus</a></li>
+        <li><a href="dashboard-xhr/index.html">dashboard-xhr</a></li>
 				<li><a href="dashboard-ui/index.html">dashboard-ui</a></li>
 				<li><a href="dashboard-vue/index.html">dashboard-vue</a></li>
 			</ul>

e2e/cypress/integration/dashboard-xhr.spec.ts
@@ -1,0 +1,42 @@
+// NOTE: we have to use different files to upload per test
+// because we are uploading to https://tusd.tusdemo.net,
+// constantly uploading the same images gives a different cached result (or something).
+describe('Dashboard with XHR', () => {
+  beforeEach(() => {
+    cy.visit('/dashboard-xhr')
+    cy.get('.uppy-Dashboard-input:first').as('file-input')
+    cy.intercept('http://localhost:3020/url/*').as('url')
+    cy.intercept('http://localhost:3020/search/unsplash/*').as('unsplash')
+  })
+
+  it('should upload remote image with URL plugin', () => {
+    cy.get('[data-cy="Url"]').click()
+    cy.get('.uppy-Url-input').type('https://raw.githubusercontent.com/transloadit/uppy/main/e2e/cypress/fixtures/images/cat.jpg')
+    cy.get('.uppy-Url-importButton').click()
+    cy.get('.uppy-StatusBar-actionBtn--upload').click()
+    cy.wait('@url')
+    cy.get('.uppy-StatusBar-statusPrimary').should('contain', 'Complete')
+  })
+
+  it('should upload remote image with Unsplash plugin', () => {
+    cy.get('[data-cy="Unsplash"]').click()
+    cy.get('.uppy-SearchProvider-input').type('book')
+    cy.get('.uppy-SearchProvider-searchButton').click()
+    cy.wait('@unsplash')
+    // Test that the author link is visible
+    cy.get('.uppy-ProviderBrowserItem')
+      .first()
+      .within(() => {
+        cy.root().click()
+        // We have hover states that show the author
+        // but we don't have hover in e2e, so we focus after the click
+        // to get the same effect. Also tests keyboard users this way.
+        cy.get('input[type="checkbox"]').focus()
+        cy.get('a').should('have.css', 'display', 'block')
+      })
+    cy.get('.uppy-c-btn-primary').click()
+    cy.get('.uppy-StatusBar-actionBtn--upload').click()
+    cy.wait('@unsplash')
+    cy.get('.uppy-StatusBar-statusPrimary').should('contain', 'Complete')
+  })
+})