4c19f364bd8d76f93e8929edde737dc67955e8a3
mos <mostafakhaki00@gmail.com>
2022-08-16 17:09:56 +0430

    Add @uppy/companion to uppy-with-companion's package.json

--- examples/uppy-with-companion/server/index.js
+++ examples/uppy-with-companion/server/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const session = require('express-session')
-const companion = require('../../../packages/@uppy/companion/src/companion')
+const companion = require('@uppy/companion/src/companion')
 
 const app = express()
 
