396795e7cc0adba15b2924209715cd80b98b692a
Antoine du Hamel <duhamelantoine1995@gmail.com>
2022-08-18 11:05:26 +0200

    example: upgrade React example to use React 18

---
+++ examples/react-example/main.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable */
+import React from 'react'
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx'
+
+createRoot(document.querySelector('#app')).render(
+  <App />
+)
