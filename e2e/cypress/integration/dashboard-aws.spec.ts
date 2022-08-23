ade8e123bc4674da6914ced11072a33fed9c38f7
Antoine du Hamel <duhamelantoine1995@gmail.com>
2022-08-23 16:44:49 +0200

    fixup! log errors

--- e2e/cypress/integration/dashboard-aws.spec.ts
+++ e2e/cypress/integration/dashboard-aws.spec.ts
@@ -9,8 +9,5 @@ describe('Dashboard with @uppy/aws-s3', () => {
     cy.get('.uppy-StatusBar-actionBtn--upload').click()
 
     cy.get('.uppy-StatusBar-statusPrimary').should('contain', 'Complete')
-    cy.window().then(({ uppy }) => {
-      uppy.on('error', err => cy.task('log', String(err)))
-    })
   })
 })
