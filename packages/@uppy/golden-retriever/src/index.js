81ba3bf7e5eddd51dbfc9fe0197c5b4ccc865980
Merlijn Vos <merlijn@soverin.net>
2022-09-15 18:28:25 +0200

    Check for `!== null`
    Co-authored-by: Antoine du Hamel <duhamelantoine1995@gmail.com>

--- packages/@uppy/golden-retriever/src/index.js
+++ packages/@uppy/golden-retriever/src/index.js
@@ -113,7 +113,7 @@ export default class GoldenRetriever extends BasePlugin {
 
     // If all files have been removed by the user, clear recovery state
     if (Object.keys(filesToSave).length === 0) {
-      if (this.uppy.getState().recoveredState) {
+      if (this.uppy.getState().recoveredState !== null) {
         this.uppy.setState({ recoveredState: null })
         MetaDataStore.cleanup(this.uppy.opts.id)
       }
