33beeab901d45e32e187e0718d1f7a6d71f2c75b
Antoine du Hamel <duhamelantoine1995@gmail.com>
2022-10-19 14:00:06 -0500

    @uppy/core: do not crash if a file is removed before the upload starts (#4148)

--- packages/@uppy/xhr-upload/src/index.js
+++ packages/@uppy/xhr-upload/src/index.js
@@ -385,6 +385,8 @@ export default class XHRUpload extends BasePlugin {
       }
       const serverToken = await this.#queueRequestSocketToken(file)
 
+      if (this.getState().files[file.id]) return undefined
+
       this.uppy.setFileState(file.id, { serverToken })
       return this.connectToServerSocket(this.uppy.getFile(file.id))
     } catch (err) {
