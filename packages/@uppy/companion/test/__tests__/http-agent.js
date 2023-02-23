3e5c4317896a65f4a8422fd4bcdc48dd1c32013c
Mikael Finstad <finstaden@gmail.com>
2023-02-23 13:21:42 +0800

    add more ip block tests

--- packages/@uppy/companion/test/__tests__/http-agent.js
+++ packages/@uppy/companion/test/__tests__/http-agent.js
@@ -62,9 +62,38 @@ describe('test protected request Agent', () => {
     await expect(promise).rejects.toThrow(new Error(FORBIDDEN_IP_ADDRESS))
   })
 
-  test('blocks localhost IP address', async () => {
-    const url = 'http://127.0.0.1:8090'
-    const promise = getProtectedGot({ url, blockLocalIPs: true }).get(url)
-    await expect(promise).rejects.toThrow(new Error(FORBIDDEN_IP_ADDRESS))
+  test('blocks various private IP addresses', async () => {
+    // eslint-disable-next-line max-len
+    // taken from: https://github.com/transloadit/uppy/blob/4aeef4dac0490ebb1d1fccd5582ba42c6c0fb87d/packages/%40uppy/companion/src/server/helpers/request.js#L14
+    const ipv4s = [
+      '0.0.0.0',
+      '0.0.0.1',
+      '127.0.0.1',
+      '127.16.0.1',
+      '192.168.1.1',
+      '169.254.1.1',
+      '10.0.0.1',
+    ]
+
+    const ipv6s = [
+      'fd80::1234:5678:abcd:0123',
+      'fe80::1234:5678:abcd:0123',
+      'ff00::1234',
+      '::ffff:192.168.1.10',
+      '::1',
+      '0:0:0:0:0:0:0:1',
+      'fda1:3f9f:dbf7::1c8d',
+    ]
+
+    for (const ip of ipv4s) {
+      const url = `http://${ip}:8090`
+      const promise = getProtectedGot({ url, blockLocalIPs: true }).get(url)
+      await expect(promise).rejects.toThrow(new Error(FORBIDDEN_IP_ADDRESS))
+    }
+    for (const ip of ipv6s) {
+      const url = `http://[${ip}]:8090`
+      const promise = getProtectedGot({ url, blockLocalIPs: true }).get(url)
+      await expect(promise).rejects.toThrow(new Error(FORBIDDEN_IP_ADDRESS))
+    }
   })
 })
