b06f24341addd2068f72ba644ff0059c47aa7448
Alexander Zaytsev <nqst@users.noreply.github.com>
2023-03-06 17:05:36 +0100

    Cleanup

--- packages/@uppy/zoom/src/Zoom.jsx
+++ packages/@uppy/zoom/src/Zoom.jsx
@@ -17,7 +17,6 @@ export default class Zoom extends UIPlugin {
     this.title = this.opts.title || 'Zoom'
     this.icon = () => (
       <svg aria-hidden="true" focusable="false" width="32" height="32" viewBox="0 0 32 32">
-        {/* <rect className="uppy-ProviderIconBg" width="32" height="32" rx="16" fill="#0E71EB" /> */}
         <path d="M24.5 11.125l-2.75 2.063c-.473.353-.75.91-.75 1.5v3.124c0 .59.277 1.147.75 1.5l2.75 2.063a.938.938 0 001.5-.75v-8.75a.938.938 0 00-1.5-.75zm-4.75 9.5c0 1.035-.84 1.875-1.875 1.875H9.75A3.75 3.75 0 016 18.75v-6.875C6 10.84 6.84 10 7.875 10H16a3.75 3.75 0 013.75 3.75v6.875z" fill="#2E8CFF" fill-rule="evenodd" />
       </svg>
     )
