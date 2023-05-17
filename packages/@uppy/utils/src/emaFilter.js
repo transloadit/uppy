73bcf42c8094be1fed23e1412b51d7b97a810e6f
Stéphane du Hamel <stephduh@live.fr>
2023-05-17 18:43:29 +0200

    @uppy/status-bar: Document emaFilter

--- packages/@uppy/utils/src/emaFilter.js
+++ packages/@uppy/utils/src/emaFilter.js
@@ -1,7 +1,17 @@
-export default function emaFilter (newValue, previousFilteredValue, halfLife, dt) {
-  if (halfLife === 0 || newValue === previousFilteredValue) return newValue
-  if (dt === 0) return previousFilteredValue
+/**
+ * Low-pass filter using Exponential Moving Averages (aka exponential smoothing)
+ * Filters a sequence of values by updating the mixing the previous output value
+ * with the new input using the exponential window function
+ *
+ * @param {*} newValue the n-th value of the sequence
+ * @param {*} previousSmoothedValue the exponential average of the first n-1 values
+ * @param {*} halfLife value of `dt` to move the smoothed value halfway between `previousFilteredValue` and `newValue`
+ * @param {*} dt time elapsed between adding the (n-1)th and the n-th values
+ * @returns the exponential average of the first n values
+ */
+export default function emaFilter (newValue, previousSmoothedValue, halfLife, dt) {
+  if (halfLife === 0 || newValue === previousSmoothedValue) return newValue
+  if (dt === 0) return previousSmoothedValue
 
-  const k = 2 ** (-1 / halfLife)
-  return newValue + (previousFilteredValue - newValue) * (k ** dt)
+  return newValue + (previousSmoothedValue - newValue) * (2 ** (-dt / halfLife))
 }
