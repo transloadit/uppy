e651205480d4d1c8a47e86bb7d0acc95d243f16b
Artur Paikin <artur@arturpaikin.com>
2023-06-07 17:44:54 +0100

    Add e2e test for Companion /url/meta file name + mock server with Content-Disposition
    Co-authored-by: Antoine du Hamel <antoine@transloadit.com>

--- e2e/cypress.config.mjs
+++ e2e/cypress.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'cypress'
 import installLogsPrinter from 'cypress-terminal-report/src/installLogsPrinter.js'
+import startMockServer from './mock-server.mjs'
 
 export default defineConfig({
   defaultCommandTimeout: 16000,
@@ -11,6 +12,8 @@ export default defineConfig({
     setupNodeEvents (on) {
       // implement node event listeners here
       installLogsPrinter(on)
+
+      startMockServer('localhost', 4678)
     },
   },
 })
---
+++ e2e/mock-server.mjs
@@ -0,0 +1,30 @@
+import http from 'node:http'
+
+const fileName = `DALL·E IMG_9078 - 学中文 🤑`
+
+const requestListener = (req, res) => {
+  const endpoint = req.url
+
+  switch (endpoint) {
+    case '/file-with-content-disposition':
+      res.setHeader('Content-Disposition', `attachment; filename="ASCII-name.zip"; filename*=UTF-8''${encodeURIComponent(fileName)}`)
+      res.setHeader('Content-Type', 'image/jpeg')
+      res.setHeader('Content-Length', '86500')
+      break
+    case '/file-no-headers':
+      break
+    default:
+      res.end('My first server!')
+  }
+
+  res.end()
+}
+
+export default function startMockServer (host, port) {
+  const server = http.createServer(requestListener)
+  server.listen(port, host, () => {
+    console.log(`Server is running on http://${host}:${port}`)
+  })
+}
+
+// startMockServer('localhost', 4678)
