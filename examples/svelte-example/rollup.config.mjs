1f91e50a0a4cb1a824b6ee5ec9bdd31e1bb8d574
Antoine du Hamel <duhamelantoine1995@gmail.com>
2023-06-09 12:07:26 +0200

    @uppy/svelte: use ESM as export format

--- examples/svelte-example/rollup.config.mjs
+++ examples/svelte-example/rollup.config.mjs
@@ -1,49 +1,53 @@
-import svelte from 'rollup-plugin-svelte'
-import commonjs from '@rollup/plugin-commonjs'
-import resolve from '@rollup/plugin-node-resolve'
-import livereload from 'rollup-plugin-livereload'
-import { terser } from 'rollup-plugin-terser'
-import sveltePreprocess from 'svelte-preprocess'
-import typescript from '@rollup/plugin-typescript'
-import css from 'rollup-plugin-css-only'
+import { spawn } from "node:child_process";
+import svelte from "rollup-plugin-svelte";
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+import livereload from "rollup-plugin-livereload";
+import { terser } from "rollup-plugin-terser";
+import sveltePreprocess from "svelte-preprocess";
+import typescript from "@rollup/plugin-typescript";
+import css from "rollup-plugin-css-only";
 
-const production = !process.env.ROLLUP_WATCH
+const production = !process.env.ROLLUP_WATCH;
 
-function serve () {
-  let server
+function serve() {
+  let server;
 
-  function toExit () {
-    if (server) server.kill(0)
+  function toExit() {
+    if (server) server.kill(0);
   }
 
   return {
-    writeBundle () {
-      if (server) return
+    writeBundle() {
+      if (server) return;
       // eslint-disable-next-line global-require
-      server = require('node:child_process').spawn('npm', ['run', 'serve', '--', '--dev'], {
-        stdio: ['ignore', 'inherit', 'inherit'],
+      server = spawn("corepack", ["yarn", "serve", "--", "--dev"], {
+        stdio: ["ignore", "inherit", "inherit"],
         shell: true,
-      })
+      });
 
-      process.on('SIGTERM', toExit)
-      process.on('exit', toExit)
+      process.on("SIGTERM", toExit);
+      process.on("exit", toExit);
     },
-  }
+  };
 }
 
 export default {
-  input: 'src/main.ts',
+  input: "src/main.ts",
   output: {
     sourcemap: true,
-    format: 'iife',
-    name: 'app',
-    file: 'public/build/bundle.js',
+    format: "iife",
+    name: "app",
+    file: "public/build/bundle.js",
   },
   plugins: [
     svelte({
       preprocess: sveltePreprocess({
         postcss: true,
       }),
+
+      include: ["src/*.svelte", "../../../**/*.svelte"],
+
       compilerOptions: {
         // enable run-time checks when not in production
         dev: !production,
@@ -51,7 +55,7 @@ export default {
     }),
     // we'll extract any component CSS out into
     // a separate file - better for performance
-    css({ output: 'bundle.css' }),
+    css({ output: "bundle.css" }),
 
     // If you have external dependencies installed from
     // npm, you'll most likely need these plugins. In
@@ -60,7 +64,9 @@ export default {
     // https://github.com/rollup/plugins/tree/master/packages/commonjs
     resolve({
       browser: true,
-      dedupe: ['svelte', '@uppy/core'],
+      dedupe: ["svelte", "@uppy/core"],
+      exportConditions: ["svelte"],
+      extensions: [".svelte"],
     }),
     commonjs(),
     typescript({
@@ -74,7 +80,7 @@ export default {
 
     // Watch the `public` directory and refresh the
     // browser on changes when not in production
-    !production && livereload('public'),
+    !production && livereload("public"),
 
     // If we're building for production (npm run build
     // instead of npm run dev), minify
@@ -83,4 +89,4 @@ export default {
   watch: {
     clearScreen: false,
   },
-}
+};
