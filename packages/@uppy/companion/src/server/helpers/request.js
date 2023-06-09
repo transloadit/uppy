d979f5cf917232071cbda7a5dfb5372567419729
Artur Paikin <artur@arturpaikin.com>
2023-06-09 19:22:20 +0100

    @uppy/companion: Use filename from content-disposition instead of relying on url, with fallback (#4489)
    * Use filename from content-disposition instead of relying on url, with fallback
    
    * Update packages/@uppy/companion/src/server/helpers/request.js
    
    * Update packages/@uppy/companion/src/server/helpers/request.js
    
    * Add random string to file name coming from basename in case ?param is a differentiator
    
    * Add e2e test for Companion /url/meta file name + mock server with Content-Disposition
    
    Co-authored-by: Antoine du Hamel <antoine@transloadit.com>
    
    * Update e2e/cypress/integration/dashboard-xhr.spec.ts
    
    Co-authored-by: Mikael Finstad <finstaden@gmail.com>
    
    * Update e2e/mock-server.mjs
    
    Co-authored-by: Antoine du Hamel <antoine@transloadit.com>
    
    * Update package.json
    
    Co-authored-by: Antoine du Hamel <antoine@transloadit.com>
    
    * Update e2e/mock-server.mjs
    
    Co-authored-by: Antoine du Hamel <antoine@transloadit.com>
    
    * Update e2e/cypress/integration/dashboard-xhr.spec.ts
    
    Co-authored-by: Mikael Finstad <finstaden@gmail.com>
    
    * Add comment about why randomUUID is added to file name
    
    * yarn
    
    * res vs response
    
    ---------
    
    Co-authored-by: Antoine du Hamel <antoine@transloadit.com>
    Co-authored-by: Mikael Finstad <finstaden@gmail.com>

--- packages/@uppy/companion/src/server/helpers/request.js
+++ packages/@uppy/companion/src/server/helpers/request.js
@@ -5,6 +5,9 @@ const { URL } = require('node:url')
 const dns = require('node:dns')
 const ipaddr = require('ipaddr.js')
 const got = require('got').default
+const path = require('node:path')
+const { randomUUID } = require('node:crypto')
+const contentDisposition = require('content-disposition')
 
 const logger = require('../logger')
 
@@ -119,7 +122,7 @@ module.exports.getProtectedGot = getProtectedGot
  *
  * @param {string} url
  * @param {boolean} blockLocalIPs
- * @returns {Promise<{type: string, size: number}>}
+ * @returns {Promise<{name: string, type: string, size: number}>}
  */
 exports.getURLMeta = async (url, blockLocalIPs = false) => {
   async function requestWithMethod (method) {
@@ -131,11 +134,18 @@ exports.getURLMeta = async (url, blockLocalIPs = false) => {
         .on('response', (response) => {
           // Can be undefined for unknown length URLs, e.g. transfer-encoding: chunked
           const contentLength = parseInt(response.headers['content-length'], 10)
+          // If Content-Disposition with file name is missing, fallback to the URL path for the name,
+          // but if multiple files are served via query params like foo.com?file=file-1, foo.com?file=file-2,
+          // we add random string to avoid duplicate files
+          const filename = response.headers['content-disposition']
+            ? contentDisposition.parse(response.headers['content-disposition']).parameters.filename
+            : `${path.basename(response.request.requestUrl)}-${randomUUID()}`
 
           // No need to get the rest of the response, as we only want header (not really relevant for HEAD, but why not)
           stream.destroy()
 
           resolve({
+            name: filename,
             type: response.headers['content-type'],
             size: Number.isNaN(contentLength) ? null : contentLength,
             statusCode: response.statusCode,
@@ -167,6 +177,6 @@ exports.getURLMeta = async (url, blockLocalIPs = false) => {
     throw new Error(`URL server responded with status: ${urlMeta.statusCode}`)
   }
 
-  const { size, type } = urlMeta
-  return { size, type }
+  const { name, size, type } = urlMeta
+  return { name, size, type }
 }
