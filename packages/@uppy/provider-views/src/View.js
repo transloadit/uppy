32cc925dbb72e58795251ab64efdcb865b5d862b
Mikael Finstad <finstaden@gmail.com>
2023-07-13 00:56:58 +0200

    restore relativePath null behavior

--- packages/@uppy/provider-views/src/View.js
+++ packages/@uppy/provider-views/src/View.js
@@ -93,7 +93,7 @@ export default class View {
     }
 
     // add relativePath similar to non-remote files: https://github.com/transloadit/uppy/pull/4486#issuecomment-1579203717
-    if (file.relDirPath != null) tagFile.meta.relativePath = file.relDirPath ? `${file.relDirPath}/${tagFile.name}` : tagFile.name
+    if (file.relDirPath != null) tagFile.meta.relativePath = file.relDirPath ? `${file.relDirPath}/${tagFile.name}` : null
     // and absolutePath (with leading slash) https://github.com/transloadit/uppy/pull/4537#issuecomment-1614236655
     if (file.absDirPath != null) tagFile.meta.absolutePath = file.absDirPath ? `/${file.absDirPath}/${tagFile.name}` : `/${tagFile.name}`
 
