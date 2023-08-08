3bd2588cbe410ee13492410ee800b06414056737
Antoine du Hamel <duhamelantoine1995@gmail.com>
2023-08-08 14:47:41 +0200

    add test

--- packages/@uppy/aws-s3/types/index.test-d.ts
+++ packages/@uppy/aws-s3/types/index.test-d.ts
@@ -1,5 +1,6 @@
 import { Uppy, UppyFile } from '@uppy/core'
 import { expectType, expectError } from 'tsd'
+import type { AwsS3Part } from '@uppy/aws-s3-multipart'
 import AwsS3 from '..'
 
 {
@@ -22,6 +23,50 @@ import AwsS3 from '..'
     getUploadParameters (file) {
       expectType<UppyFile>(file)
       return { method: 'POST', url: '', fields: {} }
+    },
+  })
+  expectError(uppy.use(AwsS3, {
+    shouldUseMultipart: true,
+    getUploadParameters (file) {
+      expectType<UppyFile>(file)
+      return { method: 'PUT', url: '' }
+    },
+  }))
+  uppy.use(AwsS3, {
+    shouldUseMultipart: () => Math.random() > 0.5,
+    getUploadParameters (file) {
+      expectType<UppyFile>(file)
+      return { method: 'PUT', url: '' }
+    },
+    createMultipartUpload (file) {
+      expectType<UppyFile>(file)
+      return { uploadId: '', key: '' }
+    },
+    listParts (file, opts) {
+      expectType<UppyFile>(file)
+      expectType<string>(opts.uploadId)
+      expectType<string>(opts.key)
+      return []
+    },
+    signPart (file, opts) {
+      expectType<UppyFile>(file)
+      expectType<string>(opts.uploadId)
+      expectType<string>(opts.key)
+      expectType<Blob>(opts.body)
+      expectType<AbortSignal>(opts.signal)
+      return { url: '' }
+    },
+    abortMultipartUpload (file, opts) {
+      expectType<UppyFile>(file)
+      expectType<string>(opts.uploadId)
+      expectType<string>(opts.key)
+    },
+    completeMultipartUpload (file, opts) {
+      expectType<UppyFile>(file)
+      expectType<string>(opts.uploadId)
+      expectType<string>(opts.key)
+      expectType<AwsS3Part>(opts.parts[0])
+      return {}
     },
   })
 }
