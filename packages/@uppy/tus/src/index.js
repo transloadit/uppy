dc69a5bbdc05dbe6fb3054a828c6f6e79b8136bf
cgoing <neo.cgoing@gmail.com>
2023-09-15 00:19:18 +0900

    @uppy/tus Fix ESLint issues

--- packages/@uppy/tus/src/index.js
+++ packages/@uppy/tus/src/index.js
@@ -258,7 +258,7 @@ export default class Tus extends BasePlugin {
 
       uploadOptions.onProgress = (bytesUploaded, bytesTotal) => {
         this.onReceiveUploadUrl(file, upload.url)
-        if (typeof opts.onProgress === 'function'){
+        if (typeof opts.onProgress === 'function') {
           opts.onProgress(bytesUploaded, bytesTotal)
         }
         this.uppy.emit('upload-progress', file, {
