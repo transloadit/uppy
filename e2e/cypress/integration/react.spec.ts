7d4021477b45a1ab93b97a791f4b228b3d9e19ec
Artur Paikin <artur@arturpaikin.com>
2023-09-22 18:08:43 +0100

    Add RemoteSources to React test

--- e2e/cypress/integration/react.spec.ts
+++ e2e/cypress/integration/react.spec.ts
@@ -13,6 +13,16 @@ describe('@uppy/react', () => {
       .each((element) => expect(element).attr('src').to.include('blob:'))
   })
 
+  it('should render Dashboard with Remote Sources plugin pack', () => {
+    const sources = ['My Device', 'Google Drive', 'OneDrive', 'Unsplash', 'Zoom', 'Link']
+    cy.get('#dashboard .uppy-DashboardTab-name')
+      .each((item, index, list) => {
+        expect(list).to.have.length(6)
+        // Returns the current element from the loop
+        expect(Cypress.$(item).text()).to.eq(sources[index])
+      })
+  })
+
   it('should render Modal in React and show thumbnails', () => {
     cy.get('#open').click()
     cy.get('@modal-input').selectFile(['cypress/fixtures/images/cat.jpg', 'cypress/fixtures/images/traffic.jpg'], { force:true })
