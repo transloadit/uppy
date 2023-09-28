8533b7af246347f4986e5231f3742cfdab4f537d
Murderlon <merlijn@soverin.net>
2023-09-28 11:08:12 +0200

    Add hook to e2e test

--- e2e/clients/react/App.jsx
+++ e2e/clients/react/App.jsx
@@ -2,27 +2,42 @@
 import Uppy from '@uppy/core'
 /* eslint-disable-next-line no-unused-vars */
 import React, { useState } from 'react'
-import { Dashboard, DashboardModal, DragDrop } from '@uppy/react'
+import {
+  Dashboard,
+  DashboardModal,
+  DragDrop,
+  useUppyStateSelector,
+} from '@uppy/react'
 import ThumbnailGenerator from '@uppy/thumbnail-generator'
 
 import '@uppy/core/dist/style.css'
 import '@uppy/dashboard/dist/style.css'
 import '@uppy/drag-drop/dist/style.css'
 
+const uppyDashboard = new Uppy({ id: 'dashboard' })
+const uppyModal = new Uppy({ id: 'modal' })
+const uppyDragDrop = new Uppy({ id: 'drag-drop' }).use(ThumbnailGenerator)
+
 export default function App () {
-  const uppyDashboard = new Uppy({ id: 'dashboard' })
-  const uppyModal = new Uppy({ id: 'modal' })
-  const uppyDragDrop = new Uppy({ id: 'drag-drop' }).use(ThumbnailGenerator)
   const [open, setOpen] = useState(false)
+  const files = useUppyStateSelector(uppyDashboard, (state) => state.files)
 
   // drag-drop has no visual output so we test it via the uppy instance
   window.uppy = uppyDragDrop
 
   return (
-    <div style={{ maxWidth: '30em', margin: '5em 0', display: 'grid', gridGap: '2em' }}>
+    <div
+      style={{
+        maxWidth: '30em',
+        margin: '5em 0',
+        display: 'grid',
+        gridGap: '2em',
+      }}
+    >
       <button type="button" id="open" onClick={() => setOpen(!open)}>
         Open Modal
       </button>
+      <p>Dashboard file count: {Object.keys(files).length}</p>
 
       <Dashboard id="dashboard" uppy={uppyDashboard} />
       <DashboardModal id="modal" open={open} uppy={uppyModal} />
