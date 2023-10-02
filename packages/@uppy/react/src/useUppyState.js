191c873c68a602e7aa2ac44ee3d05974c532c6b4
Murderlon <merlijn@soverin.net>
2023-10-02 12:03:09 +0200

    useMemo instead of useCallback for subscribe

--- packages/@uppy/react/src/useUppyState.js
+++ packages/@uppy/react/src/useUppyState.js
@@ -1,16 +1,15 @@
-import { useSyncExternalStore, useCallback } from 'react'
+import { useSyncExternalStore, useMemo, useCallback } from 'react'
 
-export default function useUppyState (uppy, selector) {
-  // Eslint complains the dependencies are unknown and we should use an inline function
-  // but we put uppy.store.subscribe in there ourselves.
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  const subscribe = useCallback(uppy.store.subscribe.bind(uppy.store), [
-    uppy.store.subscribe,
-  ])
+export default function useUppyState(uppy, selector) {
+  const subscribe = useMemo(
+    () => uppy.store.subscribe.bind(uppy.store),
+    [uppy.store],
+  )
   const getSnapshot = useCallback(
     () => selector(uppy.store.getState()),
     // Eslint wants to put `uppy.store` as the dependency
     // but that seems unnecessary as we can be more specific.
+    // `uppy.store` can theoretically never be `undefined`.
     // eslint-disable-next-line react-hooks/exhaustive-deps
     [uppy.store.getState, selector],
   )
