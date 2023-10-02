5b13aac7e02276eebc07df4d3e2fbadd7d746d8b
Murderlon <merlijn@soverin.net>
2023-10-02 11:16:00 +0200

    fixup! Apply feedback

--- packages/@uppy/react/src/useUppyState.js
+++ packages/@uppy/react/src/useUppyState.js
@@ -1,15 +1,16 @@
 import { useSyncExternalStore, useCallback } from 'react'
 
 export default function useUppyState (uppy, selector) {
-  const subscribe = useCallback(
-    () => uppy.store.subscribe(),
-    // Eslint wants us to put `uppy.store` in the dependency array,
-    // but it seems to make more sense to be more specific.
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    [uppy.store.subscribe],
-  )
+  // Eslint complains the dependencies are unknown and we should use an inline function
+  // but we put uppy.store.subscribe in there ourselves.
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const subscribe = useCallback(uppy.store.subscribe.bind(uppy.store), [
+    uppy.store.subscribe,
+  ])
   const getSnapshot = useCallback(
     () => selector(uppy.store.getState()),
+    // Eslint wants to put `uppy.store` as the dependency
+    // but that seems unnecessary as we can be more specific.
     // eslint-disable-next-line react-hooks/exhaustive-deps
     [uppy.store.getState, selector],
   )
