656f014f72aedb06908d17f7ba898132b68b1e12
Antoine du Hamel <duhamelantoine1995@gmail.com>
2023-10-15 20:38:08 +0200

    `getDroppedFile.ts`

--- packages/@uppy/utils/src/getDroppedFiles/index.ts
+++ packages/@uppy/utils/src/getDroppedFiles/index.ts
@@ -1,5 +1,5 @@
-import webkitGetAsEntryApi from './utils/webkitGetAsEntryApi/index.js'
-import fallbackApi from './utils/fallbackApi.js'
+import webkitGetAsEntryApi from './utils/webkitGetAsEntryApi/index.ts'
+import fallbackApi from './utils/fallbackApi.ts'
 
 /**
  * Returns a promise that resolves to the array of dropped files (if a folder is
@@ -8,19 +8,26 @@ import fallbackApi from './utils/fallbackApi.js'
  * Each file has .relativePath prop appended to it (e.g. "/docs/Prague/ticket_from_prague_to_ufa.pdf")
  * if browser supports it. Otherwise it's undefined.
  *
- * @param {DataTransfer} dataTransfer
- * @param {Function} logDropError - a function that's called every time some
+ * @param dataTransfer
+ * @param options
+ * @param options.logDropError - a function that's called every time some
  * folder or some file error out (e.g. because of the folder name being too long
  * on Windows). Notice that resulting promise will always be resolved anyway.
  *
  * @returns {Promise} - Array<File>
  */
-export default async function getDroppedFiles (dataTransfer, { logDropError = () => {} } = {}) {
+export default async function getDroppedFiles (
+  dataTransfer: DataTransfer,
+  options?: {
+    logDropError?: any
+  }
+  ): Promise<File[]> {
   // Get all files from all subdirs. Works (at least) in Chrome, Mozilla, and Safari
+  const logDropError = options?.logDropError ?? Function.prototype
   try {
     const accumulator = []
     for await (const file of webkitGetAsEntryApi(dataTransfer, logDropError)) {
-      accumulator.push(file)
+      accumulator.push(file as File)
     }
     return accumulator
   // Otherwise just return all first-order files
