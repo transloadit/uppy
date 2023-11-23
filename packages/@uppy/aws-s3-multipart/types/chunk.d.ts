a2b3820405260e69b06f81f10085ab50e0be63f4
Antoine du Hamel <duhamelantoine1995@gmail.com>
2023-11-23 14:37:28 +0100

    remove manual types

--- packages/@uppy/aws-s3-multipart/types/chunk.d.ts
+++
@@ -1,7 +0,0 @@
-export interface Chunk {
-  getData: () => Blob
-  onProgress: (ev: ProgressEvent) => void
-  onComplete: (etag: string) => void
-  shouldUseMultipart: boolean
-  setAsUploaded?: () => void
-}
