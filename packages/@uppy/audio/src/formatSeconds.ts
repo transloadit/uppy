d7bf44507e793caa6f24e3da222555ca6956a890
Antoine du Hamel <duhamelantoine1995@gmail.com>
2024-01-16 12:40:28 +0100

    `formatSeconds`

--- packages/@uppy/audio/src/formatSeconds.ts
+++ packages/@uppy/audio/src/formatSeconds.ts
@@ -1,12 +1,7 @@
 /**
- * Takes an Integer value of seconds (e.g. 83) and converts it into a human-readable formatted string (e.g. '1:23').
- *
- * @param {Integer} seconds
- * @returns {string} the formatted seconds (e.g. '1:23' for 1 minute and 23 seconds)
- *
+ * Takes an Integer value of seconds (e.g. 83) and converts it into a
+ * human-readable formatted string (e.g. '1:23').
  */
-export default function formatSeconds (seconds) {
-  return `${Math.floor(
-    seconds / 60,
-  )}:${String(seconds % 60).padStart(2, 0)}`
+export default function formatSeconds(seconds: number): string {
+  return `${Math.floor(seconds / 60)}:${String(seconds % 60).padStart(2, '0')}`
 }
