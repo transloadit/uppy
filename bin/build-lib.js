bb758d097d7302e1f601303bb8c8faabf09c304a
Mikael Finstad <finstaden@gmail.com>
2024-01-19 11:28:22 +0700

    remove comments in output code

--- bin/build-lib.js
+++ bin/build-lib.js
@@ -116,7 +116,8 @@ async function buildLib () {
     const isTSX = file.endsWith('.tsx')
     if (isTSX || file.endsWith('.ts')) { plugins.push(['@babel/plugin-transform-typescript', { disallowAmbiguousJSXLike: true, isTSX, jsxPragma: 'h' }]) }
 
-    const { code, map } = await babel.transformFileAsync(file, { sourceMaps: true, plugins })
+    // compact and no comments because https://github.com/transloadit/uppy/pull/4868#issuecomment-1897717779
+    const { code, map } = await babel.transformFileAsync(file, { sourceMaps: true, plugins, comments: false, compact: true })
     const [{ default: chalk }] = await Promise.all([
       import('chalk'),
       writeFile(libFile, code),
