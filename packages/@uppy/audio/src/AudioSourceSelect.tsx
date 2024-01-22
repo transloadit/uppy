c56d41205744c6f449050a2159e2a827e74c74df
Antoine du Hamel <antoine@transloadit.com>
2024-01-22 15:04:01 +0100

    @uppy/audio: refactor to TypeScript (#4860)

--- packages/@uppy/audio/src/AudioSourceSelect.tsx
+++ packages/@uppy/audio/src/AudioSourceSelect.tsx
@@ -1,11 +1,23 @@
 import { h } from 'preact'
 
-export default ({ currentDeviceId, audioSources, onChangeSource }) => {
+export interface AudioSourceSelectProps {
+  currentDeviceId: string
+  audioSources: MediaDeviceInfo[]
+  onChangeSource: (value: string) => void
+}
+
+export default ({
+  currentDeviceId,
+  audioSources,
+  onChangeSource,
+}: AudioSourceSelectProps): JSX.Element => {
   return (
     <div className="uppy-Audio-videoSource">
       <select
         className="uppy-u-reset uppy-Audio-audioSource-select"
-        onChange={(event) => { onChangeSource(event.target.value) }}
+        onChange={(event) => {
+          onChangeSource(event.target.value)
+        }}
       >
         {audioSources.map((audioSource) => (
           <option
