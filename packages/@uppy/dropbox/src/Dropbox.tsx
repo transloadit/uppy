f72ec1b7ef27ec249d5fa14e8594ad2d111c317f
Murderlon <merlijn@soverin.net>
2024-03-04 10:50:07 +0100

    @uppy/dropbox: refactor to TypeScript (#4979)

--- packages/@uppy/dropbox/src/Dropbox.tsx
+++ packages/@uppy/dropbox/src/Dropbox.tsx
@@ -15,10 +15,10 @@ import locale from './locale.ts'
 // @ts-ignore We don't want TS to generate types for the package.json
 import packageJson from '../package.json'
 
-export type BoxOptions = CompanionPluginOptions
+export type DropboxOptions = CompanionPluginOptions
 
-export default class Box<M extends Meta, B extends Body> extends UIPlugin<
-  BoxOptions,
+export default class Dropbox<M extends Meta, B extends Body> extends UIPlugin<
+  DropboxOptions,
   M,
   B,
   UnknownProviderPluginState
@@ -35,25 +35,26 @@ export default class Box<M extends Meta, B extends Body> extends UIPlugin<
 
   files: UppyFile<M, B>[]
 
-  constructor(uppy: Uppy<M, B>, opts: BoxOptions) {
+  constructor(uppy: Uppy<M, B>, opts: DropboxOptions) {
     super(uppy, opts)
-    this.id = this.opts.id || 'Box'
+    this.id = this.opts.id || 'Dropbox'
     this.type = 'acquirer'
     this.storage = this.opts.storage || tokenStorage
     this.files = []
     this.icon = () => (
       <svg
-        className="uppy-DashboardTab-iconBox"
+        className="uppy-DashboardTab-iconDropbox"
         aria-hidden="true"
         focusable="false"
         width="32"
         height="32"
         viewBox="0 0 32 32"
       >
-        <g fill="currentcolor" fillRule="nonzero">
-          <path d="m16.4 13.5c-1.6 0-3 0.9-3.7 2.2-0.7-1.3-2.1-2.2-3.7-2.2-1 0-1.8 0.3-2.5 0.8v-3.6c-0.1-0.3-0.5-0.7-1-0.7s-0.8 0.4-0.8 0.8v7c0 2.3 1.9 4.2 4.2 4.2 1.6 0 3-0.9 3.7-2.2 0.7 1.3 2.1 2.2 3.7 2.2 2.3 0 4.2-1.9 4.2-4.2 0.1-2.4-1.8-4.3-4.1-4.3m-7.5 6.8c-1.4 0-2.5-1.1-2.5-2.5s1.1-2.5 2.5-2.5 2.5 1.1 2.5 2.5-1.1 2.5-2.5 2.5m7.5 0c-1.4 0-2.5-1.1-2.5-2.5s1.1-2.5 2.5-2.5 2.5 1.1 2.5 2.5-1.1 2.5-2.5 2.5" />
-          <path d="m27.2 20.6l-2.3-2.8 2.3-2.8c0.3-0.4 0.2-0.9-0.2-1.2s-1-0.2-1.3 0.2l-2 2.4-2-2.4c-0.3-0.4-0.9-0.4-1.3-0.2-0.4 0.3-0.5 0.8-0.2 1.2l2.3 2.8-2.3 2.8c-0.3 0.4-0.2 0.9 0.2 1.2s1 0.2 1.3-0.2l2-2.4 2 2.4c0.3 0.4 0.9 0.4 1.3 0.2 0.4-0.3 0.4-0.8 0.2-1.2" />
-        </g>
+        <path
+          d="M10.5 7.5L5 10.955l5.5 3.454 5.5-3.454 5.5 3.454 5.5-3.454L21.5 7.5 16 10.955zM10.5 21.319L5 17.864l5.5-3.455 5.5 3.455zM16 17.864l5.5-3.455 5.5 3.455-5.5 3.455zM16 25.925l-5.5-3.455 5.5-3.454 5.5 3.454z"
+          fill="currentcolor"
+          fillRule="nonzero"
+        />
       </svg>
     )
 
@@ -66,15 +67,15 @@ export default class Box<M extends Meta, B extends Body> extends UIPlugin<
       companionHeaders: this.opts.companionHeaders,
       companionKeysParams: this.opts.companionKeysParams,
       companionCookiesRule: this.opts.companionCookiesRule,
-      provider: 'box',
+      provider: 'dropbox',
       pluginId: this.id,
-      supportsRefreshToken: false,
+      supportsRefreshToken: true,
     })
 
     this.defaultLocale = locale
 
     this.i18nInit()
-    this.title = this.i18n('pluginNameBox')
+    this.title = this.opts.title || this.i18n('pluginNameDropbox')
 
     this.onFirstRender = this.onFirstRender.bind(this)
     this.render = this.render.bind(this)
