095c900a39dafc8425158fb5830c2422aadf51bd
Evgenia Karunus <lakesare@gmail.com>
2024-04-10 14:31:18 +0400

    `PartialTreeUtils.ts` - factor out `getPartialTreeAfterTogglingCheckboxes()`

--- packages/@uppy/provider-views/src/View.ts
+++ packages/@uppy/provider-views/src/View.ts
@@ -12,6 +12,7 @@ import getFileType from '@uppy/utils/lib/getFileType'
 import isPreviewSupported from '@uppy/utils/lib/isPreviewSupported'
 import remoteFileObjToLocal from '@uppy/utils/lib/remoteFileObjToLocal'
 import type { RestrictionError } from '@uppy/core/lib/Restricter'
+import PartialTreeUtils from './utils/PartialTreeUtils'
 
 type PluginType = 'Provider' | 'SearchProvider'
 
@@ -243,76 +244,8 @@ export default class View<
     document.getSelection()?.removeAllRanges()
 
     const { partialTree, currentFolderId } = this.plugin.getPluginState()
-    const newPartialTree : PartialTree = JSON.parse(JSON.stringify(partialTree))
 
-    // if newStatus is "checked" - percolate down "checked"
-    // if newStatus is "unchecked" - percolate down "unchecked"
-    const percolateDown = (clickedItem: PartialTreeFolderNode | PartialTreeFile, status: 'checked' | 'unchecked') => {
-      const children = newPartialTree.filter((item) => item.type !== 'root' && item.parentId === clickedItem.id) as (PartialTreeFolderNode | PartialTreeFile)[]
-      children.forEach((item) => {
-        if (item.type === 'file') {
-          item.status = status === 'checked' && this.validateRestrictions(item.data) ? 'unchecked' : status
-        } else {
-          item.status = status
-        }
-        percolateDown(item, status)
-      })
-    }
-    // we do something to all of its parents.
-    const percolateUp = (currentItem: PartialTreeFolderNode | PartialTreeFile) => {
-      const parentFolder = newPartialTree.find((item) => item.id === currentItem.parentId)! as PartialTreeFolder
-      if (parentFolder.type === 'root') return
-
-      const parentsChildren = newPartialTree.filter((item) => item.type !== 'root' && item.parentId === parentFolder.id) as (PartialTreeFile | PartialTreeFolderNode)[]
-      const parentsValidChildren = parentsChildren.filter((item) =>
-        !this.validateRestrictions(item.data)
-      )
-      const areAllChildrenChecked = parentsValidChildren.every((item) => item.status === "checked")
-      const areAllChildrenUnchecked = parentsValidChildren.every((item) => item.status === "unchecked")
-
-      if (areAllChildrenChecked) {
-        parentFolder.status = "checked"
-      } else if (areAllChildrenUnchecked) {
-        parentFolder.status = "unchecked"
-      } else {
-        parentFolder.status = "partial"
-      }
-
-      percolateUp(parentFolder)
-    }
-
-    // Shift-clicking selects a single consecutive list of items
-    // starting at the previous click.
-    const inThisFolder  = this.filterItems(newPartialTree.filter((item) => item.type !== 'root' && item.parentId === currentFolderId)) as (PartialTreeFile | PartialTreeFolderNode)[]
-    const prevIndex = inThisFolder.findIndex((item) => item.id === this.lastCheckbox)
-    if (prevIndex !== -1 && this.isShiftKeyPressed) {
-      const newIndex = inThisFolder.findIndex((item) => item.id === ourItem.id)
-      const toMarkAsChecked = (prevIndex < newIndex ?
-          inThisFolder.slice(prevIndex, newIndex + 1)
-        : inThisFolder.slice(newIndex, prevIndex + 1)
-      ).map((item) => item.id)
-
-      const newlyCheckedItems = newPartialTree
-        .filter((item) => item.type !== 'root' && toMarkAsChecked.includes(item.id)) as (PartialTreeFile | PartialTreeFolderNode)[] 
-
-      newlyCheckedItems.forEach((item) => {
-        if (item.type === 'file') {
-          item.status = this.validateRestrictions(item.data) ? 'unchecked' : 'checked'
-        } else {
-          item.status = 'checked'
-        }
-      })
-
-      newlyCheckedItems.forEach((item) => {
-        percolateDown(item, 'checked')
-      })
-      percolateUp(ourItem)
-    } else {
-      const ourItemInNewTree = newPartialTree.find((item) => item.id === ourItem.id) as (PartialTreeFile | PartialTreeFolderNode)
-      ourItemInNewTree.status = ourItem.status === 'checked' ? 'unchecked' : 'checked'
-      percolateDown(ourItem, ourItemInNewTree.status)
-      percolateUp(ourItem)
-    }
+    const newPartialTree = PartialTreeUtils.getPartialTreeAfterTogglingCheckboxes(partialTree, ourItem, this.validateRestrictions, this.filterItems, currentFolderId, this.isShiftKeyPressed, this.lastCheckbox)
 
     this.plugin.setPluginState({ partialTree: newPartialTree })
     this.lastCheckbox = ourItem.id!
---
+++ packages/@uppy/provider-views/src/utils/PartialTreeUtils.ts
@@ -0,0 +1,87 @@
+import type { PartialTree, PartialTreeFile, PartialTreeFolder, PartialTreeFolderNode } from "@uppy/core/lib/Uppy"
+import type { CompanionFile } from "@uppy/utils/lib/CompanionFile"
+
+const getPartialTreeAfterTogglingCheckboxes = (
+  oldPartialTree: PartialTree,
+  ourItem: PartialTreeFolderNode | PartialTreeFile,
+  validateRestrictions: (file: CompanionFile) => object | null,
+  filterItems : (items: PartialTree) => PartialTree,
+  currentFolderId:  string | null,
+  isShiftKeyPressed: boolean,
+  lastCheckbox: string | undefined
+) : PartialTree => {
+  const newPartialTree : PartialTree = JSON.parse(JSON.stringify(oldPartialTree))
+
+  // if newStatus is "checked" - percolate down "checked"
+  // if newStatus is "unchecked" - percolate down "unchecked"
+  const percolateDown = (clickedItem: PartialTreeFolderNode | PartialTreeFile, status: 'checked' | 'unchecked') => {
+    const children = newPartialTree.filter((item) => item.type !== 'root' && item.parentId === clickedItem.id) as (PartialTreeFolderNode | PartialTreeFile)[]
+    children.forEach((item) => {
+      if (item.type === 'file') {
+        item.status = status === 'checked' && validateRestrictions(item.data) ? 'unchecked' : status
+      } else {
+        item.status = status
+      }
+      percolateDown(item, status)
+    })
+  }
+  // we do something to all of its parents.
+  const percolateUp = (currentItem: PartialTreeFolderNode | PartialTreeFile) => {
+    const parentFolder = newPartialTree.find((item) => item.id === currentItem.parentId)! as PartialTreeFolder
+    if (parentFolder.type === 'root') return
+
+    const parentsChildren = newPartialTree.filter((item) => item.type !== 'root' && item.parentId === parentFolder.id) as (PartialTreeFile | PartialTreeFolderNode)[]
+    const parentsValidChildren = parentsChildren.filter((item) =>
+      !validateRestrictions(item.data)
+    )
+    const areAllChildrenChecked = parentsValidChildren.every((item) => item.status === "checked")
+    const areAllChildrenUnchecked = parentsValidChildren.every((item) => item.status === "unchecked")
+
+    if (areAllChildrenChecked) {
+      parentFolder.status = "checked"
+    } else if (areAllChildrenUnchecked) {
+      parentFolder.status = "unchecked"
+    } else {
+      parentFolder.status = "partial"
+    }
+
+    percolateUp(parentFolder)
+  }
+
+  // Shift-clicking selects a single consecutive list of items
+  // starting at the previous click.
+  const inThisFolder  = filterItems(newPartialTree.filter((item) => item.type !== 'root' && item.parentId === currentFolderId)) as (PartialTreeFile | PartialTreeFolderNode)[]
+  const prevIndex = inThisFolder.findIndex((item) => item.id === lastCheckbox)
+  if (prevIndex !== -1 && isShiftKeyPressed) {
+    const newIndex = inThisFolder.findIndex((item) => item.id === ourItem.id)
+    const toMarkAsChecked = (prevIndex < newIndex ?
+        inThisFolder.slice(prevIndex, newIndex + 1)
+      : inThisFolder.slice(newIndex, prevIndex + 1)
+    ).map((item) => item.id)
+
+    const newlyCheckedItems = newPartialTree
+      .filter((item) => item.type !== 'root' && toMarkAsChecked.includes(item.id)) as (PartialTreeFile | PartialTreeFolderNode)[] 
+
+    newlyCheckedItems.forEach((item) => {
+      if (item.type === 'file') {
+        item.status = validateRestrictions(item.data) ? 'unchecked' : 'checked'
+      } else {
+        item.status = 'checked'
+      }
+    })
+
+    newlyCheckedItems.forEach((item) => {
+      percolateDown(item, 'checked')
+    })
+    percolateUp(ourItem)
+  } else {
+    const ourItemInNewTree = newPartialTree.find((item) => item.id === ourItem.id) as (PartialTreeFile | PartialTreeFolderNode)
+    ourItemInNewTree.status = ourItem.status === 'checked' ? 'unchecked' : 'checked'
+    percolateDown(ourItem, ourItemInNewTree.status)
+    percolateUp(ourItem)
+  }
+
+  return newPartialTree
+}
+
+export default { getPartialTreeAfterTogglingCheckboxes }
