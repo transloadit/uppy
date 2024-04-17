915a563da9e7e3c89cbb0d2ced6a2ad3456cdc56
Merlijn Vos <merlijn@soverin.net>
2024-04-17 12:05:48 +0200

    @uppy/react: remove `useUppy` & reintroduce `useUppyState` (#5059)

---
+++ packages/@uppy/react/src/useUppyState.ts
@@ -0,0 +1,23 @@
+import type { Uppy, State } from '@uppy/core'
+import type { Body, Meta } from '@uppy/utils/lib/UppyFile'
+import { useMemo, useCallback } from 'react'
+import { useSyncExternalStoreWithSelector } from 'use-sync-external-store/with-selector'
+
+export default function useUppyState<
+  M extends Meta = Meta,
+  B extends Body = Body,
+  T = any,
+>(uppy: Uppy<M, B>, selector: (state: State<M, B>) => T): T {
+  const subscribe = useMemo(
+    () => uppy.store.subscribe.bind(uppy.store),
+    [uppy.store],
+  )
+  const getSnapshot = useCallback(() => uppy.store.getState(), [uppy.store])
+
+  return useSyncExternalStoreWithSelector(
+    subscribe,
+    getSnapshot,
+    null,
+    selector,
+  )
+}
