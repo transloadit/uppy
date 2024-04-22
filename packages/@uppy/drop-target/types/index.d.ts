26b9b4edaf75b1858e34c52b4853a221c774accf
Alireza Heydari <a.heydari@catawiki.nl>
2024-04-22 14:47:10 +0200

    @uppy/drop-target: change drop event type to DragEvent

--- packages/@uppy/drop-target/types/index.d.ts
+++ packages/@uppy/drop-target/types/index.d.ts
@@ -2,9 +2,9 @@ import type { PluginOptions, BasePlugin } from '@uppy/core'
 
 interface DropTargetOptions extends PluginOptions {
   target: string | Element
-  onDragOver?: (event: MouseEvent) => void
-  onDrop?: (event: MouseEvent) => void
-  onDragLeave?: (event: MouseEvent) => void
+  onDragOver?: (event: DragEvent) => void
+  onDrop?: (event: DragEvent) => void
+  onDragLeave?: (event: DragEvent) => void
 }
 
 declare class DropTarget extends BasePlugin<DropTargetOptions> {}
