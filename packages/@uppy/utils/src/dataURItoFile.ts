1ed5dc5ee4dd3a5cf9fac59fefcd737a8f723c94
Antoine du Hamel <duhamelantoine1995@gmail.com>
2024-04-23 14:20:21 +0200

    @uppy/utils: improve return type of `dataURItoFile`
    As its name implies, it returns a `File`.

--- packages/@uppy/utils/src/dataURItoFile.ts
+++ packages/@uppy/utils/src/dataURItoFile.ts
@@ -3,6 +3,6 @@ import dataURItoBlob from './dataURItoBlob.ts'
 export default function dataURItoFile(
   dataURI: string,
   opts: { mimeType?: string; name?: string },
-): File | Blob {
+): File {
   return dataURItoBlob(dataURI, opts, true)
 }
