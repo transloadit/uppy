77c6b2512c5eae4bd95a6855d639cc428caba85f
Antoine du Hamel <antoine@transloadit.com>
2024-05-02 22:10:27 +0200

    meta: fix `update-contributors` script (#5137)

--- bin/update-contributors.mjs
+++ bin/update-contributors.mjs
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 
-import { spawn } from 'node:child_process'
+import fetchContrib from 'github-contributors-list/lib/contributors.js'
+import layoutStrategy from 'github-contributors-list/lib/strategies/layout_strategies/json.js'
+import sortStrategy from 'github-contributors-list/lib/strategies/sort_strategies/sort_desc.js'
+import filterStrategy from 'github-contributors-list/lib/strategies/filter_strategies/login.js'
 import { Buffer } from 'node:buffer'
 import fs from 'node:fs/promises'
 import process from 'node:process'
@@ -10,27 +13,37 @@ const README_FILE_NAME = new URL('../README.md', import.meta.url)
 const readme = await fs.open(README_FILE_NAME, 'r+')
 const readmeContent = await readme.readFile()
 
-const githubcontrib = spawn('npx', [
-  'githubcontrib',
-  '--owner', 'transloadit',
-  '--repo', 'uppy',
-  '--cols', '6',
-  '--format', 'md',
-  '--showlogin', 'true',
-  '--sortOrder', 'desc',
-], {
-  stdio: ['ignore', 'pipe', 'inherit'],
-})
-
-githubcontrib.on('error', console.error)
-
 // Detect start of contributors section.
-const START_TAG = Buffer.from('<!--contributors-->\n')
-const START_TAG_POSITION = readmeContent.indexOf(START_TAG) + START_TAG.byteLength
+const START_TAG = Buffer.from('<table id="contributors_table">\n')
+const START_TAG_POSITION =
+  readmeContent.indexOf(START_TAG) + START_TAG.byteLength
 
+const args = {
+  owner: 'transloadit',
+  repository: 'uppy',
+  cols: 6,
+  format: 'json',
+  sortBy: 'contributions',
+  sortOrder: 'desc',
+  filter: [],
+  layoutStrategy,
+  sortStrategy,
+  filterStrategy,
+}
+const { contributors } = await fetchContrib(args).loadAll(
+  args.owner,
+  args.repository,
+  args.authToken || process.env.GITHUB_API_TOKEN,
+  args.fromDate,
+)
 let cursor = START_TAG_POSITION
-for await (const data of githubcontrib.stdout) {
-  const { bytesWritten } = await readme.write(data.toString('utf-8'), cursor, 'utf-8')
+for (const line of contributors) {
+  let row = '<tr>'
+  for (const { html_url, login, avatar_url } of line) {
+    row += `<td><a href=${html_url}><img width="117" alt=${JSON.stringify(login)} src=${JSON.stringify(avatar_url)}></a></td>`
+  }
+  row += '</tr>\n'
+  const { bytesWritten } = await readme.write(row, cursor, 'utf-8')
   cursor += bytesWritten
 }
 
@@ -39,6 +52,8 @@ if (cursor === START_TAG_POSITION) {
   await readme.close()
   process.exit(1)
 }
+
+await readme.truncate(cursor)
 
 // Write the end of the file.
 await readme.write(
