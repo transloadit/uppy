081a415be31358acb20debe4c9144a71d6b1fa53
Antoine du Hamel <duhamelantoine1995@gmail.com>
2024-05-21 15:26:20 +0200

    remove `chalk` from devDeps as well

--- packages/@uppy/companion/test/__tests__/logger.js
+++ packages/@uppy/companion/test/__tests__/logger.js
@@ -1,4 +1,4 @@
-const chalk = require('chalk')
+process.env.FORCE_COLOR = 'false'
 const logger = require('../../src/server/logger')
 
 const maskables = ['ToBeMasked1', 'toBeMasked2', 'toBeMasked(And)?Escaped']
@@ -43,7 +43,7 @@ describe('Test Logger secret mask', () => {
       logger.warn('this warning has ToBeMasked1 and toBeMasked2 and case-insensitive TOBEMasKED2')
     })
 
-    const exptectedMsg = chalk.bold.yellow('this warning has ****** and ****** and case-insensitive ******')
+    const exptectedMsg = 'this warning has ****** and ****** and case-insensitive ******'
 
     expect(loggedMessage).toBeTruthy()
     expect(loggedMessage).toBe(exptectedMsg)
@@ -54,7 +54,7 @@ describe('Test Logger secret mask', () => {
       logger.error(new Error('this error has ToBeMasked1 and toBeMasked2 and case-insensitive TOBEMasKED2'))
     })
 
-    const exptectedMsg = chalk.bold.red('Error: this error has ****** and ****** and case-insensitive ******')
+    const exptectedMsg = 'Error: this error has ****** and ****** and case-insensitive ******'
 
     expect(loggedMessage.startsWith(exptectedMsg)).toBeTruthy()
   })
@@ -65,7 +65,7 @@ describe('Test Logger secret mask', () => {
       logger.error(err, '', '')
     })
 
-    const exptectedMsg = chalk.bold.red('Error: this error has ****** and ****** and case-insensitive ******')
+    const exptectedMsg = 'Error: this error has ****** and ****** and case-insensitive ******'
 
     expect(loggedMessage).toBeTruthy()
     expect(loggedMessage.startsWith(exptectedMsg)).toBe(true)
@@ -79,7 +79,7 @@ describe('Test Logger secret mask', () => {
       logger.warn('this warning has ToBeMasked(And)?Escaped but not toBeMaskedEscaped ')
     })
 
-    const exptectedMsg = chalk.bold.yellow('this warning has ****** but not toBeMaskedEscaped ')
+    const exptectedMsg = 'this warning has ****** but not toBeMaskedEscaped '
 
     expect(loggedMessage).toBeTruthy()
     expect(loggedMessage).toBe(exptectedMsg)
