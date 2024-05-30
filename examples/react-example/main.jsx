c1481392959bfbc3783eeada5df782f2d2e7f278
Merlijn Vos <merlijn@soverin.net>
2024-05-30 12:54:07 +0200

    examples: make React example up-to-date (#5205)

--- examples/react-example/main.jsx
+++
@@ -1,8 +0,0 @@
-/* eslint-disable */
-import React from 'react'
-import { createRoot } from 'react-dom/client';
-import App from './App.jsx'
-
-createRoot(document.querySelector('#app')).render(
-  <App />
-)
