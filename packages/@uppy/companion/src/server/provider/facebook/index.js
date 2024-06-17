db316bf3da0416e0b5c9847f5e6483c98d27d910
Mikael Finstad <finstaden@gmail.com>
2024-06-17 22:29:45 +0200

    implement review feedback

--- packages/@uppy/companion/src/server/provider/facebook/index.js
+++ packages/@uppy/companion/src/server/provider/facebook/index.js
@@ -11,6 +11,48 @@ const { HttpError } = require('../../helpers/utils')
 const got = require('../../got')
 
 
+async function runRequestBatch({ secret, token, requests }) {
+  // https://developers.facebook.com/docs/facebook-login/security/#appsecret
+  // couldn't get `appsecret_time` working, but it seems to be working without it
+  // const time = Math.floor(Date.now() / 1000)
+  const appSecretProof = crypto.createHmac('sha256', secret)
+    // .update(`${token}|${time}`)
+    .update(token)
+    .digest('hex');
+
+  const form = {
+    access_token: token,
+    appsecret_proof: appSecretProof,
+    // appsecret_time: String(time),
+    batch: JSON.stringify(requests),
+  }
+
+  const responsesRaw = await (await got).post('https://graph.facebook.com', { form }).json()
+
+  const responses = responsesRaw.map((response) => ({ ...response, body: JSON.parse(response.body) }))
+
+  const errorResponse = responses.find((response) => response.code !== 200)
+  if (errorResponse) {
+    throw new HttpError({ statusCode: errorResponse.code, responseJson: errorResponse.body })
+  }
+
+  return responses
+}
+
+async function getMediaUrl ({ secret, token, id }) {
+  const [{ body }] = await runRequestBatch({
+    secret,
+    token,
+    requests: [
+      { method: 'GET', relative_url: `${id}?${new URLSearchParams({ fields: 'images' }).toString()}` },
+    ],
+  });
+
+  const sortedImages = sortImages(body.images)
+  return sortedImages[sortedImages.length - 1].source
+}  
+
+
 /**
  * Adapter for API https://developers.facebook.com/docs/graph-api/using-graph-api/
  */
@@ -18,47 +60,6 @@ class Facebook extends Provider {
   static get oauthProvider () {
     return 'facebook'
   }
-
-  async runRequestBatch({ token, requests }) {
-    // https://developers.facebook.com/docs/facebook-login/security/#appsecret
-    // couldn't get `appsecret_time` working, but it seems to be working without it
-    // const time = Math.floor(Date.now() / 1000)
-    const appSecretProof = crypto.createHmac('sha256', this.secret)
-      // .update(`${token}|${time}`)
-      .update(token)
-      .digest('hex');
-  
-    const form = {
-      access_token: token,
-      appsecret_proof: appSecretProof,
-      // appsecret_time: String(time),
-      batch: JSON.stringify(requests),
-    }
-  
-    const responsesRaw = await (await got).post('https://graph.facebook.com', { form }).json()
-
-    const responses = responsesRaw.map((response) => ({ ...response, body: JSON.parse(response.body) }))
-
-    responses.forEach((response) => {
-      if (response.code !== 200) {
-        throw new HttpError({ statusCode: response.code, responseJson: response.body })
-      }
-    })
-
-    return responses
-  }
-  
-  async getMediaUrl ({ token, id }) {
-    const [{ body }] = await this.runRequestBatch({
-      token,
-      requests: [
-        { method: 'GET', relative_url: `${id}?${new URLSearchParams({ fields: 'images' }).toString()}` },
-      ],
-    });
-  
-    const sortedImages = sortImages(body.images)
-    return sortedImages[sortedImages.length - 1].source
-  }  
 
   async list ({ directory, token, query = { cursor: null } }) {
     return this.#withErrorHandling('provider.facebook.list.error', async () => {
@@ -72,7 +73,8 @@ class Facebook extends Provider {
         qs.fields = 'icon,images,name,width,height,created_time'
       }
 
-      const [response1, response2] = await this.runRequestBatch({
+      const [response1, response2] = await runRequestBatch({
+        secret: this.secret,
         token,
         requests: [
           { method: 'GET', relative_url: `me?${new URLSearchParams({ fields: 'email' }).toString()}` },
@@ -88,7 +90,7 @@ class Facebook extends Provider {
 
   async download ({ id, token }) {
     return this.#withErrorHandling('provider.facebook.download.error', async () => {
-      const url = await this.getMediaUrl({ token, id })
+      const url = await getMediaUrl({ secret: this.secret, token, id })
       const stream = (await got).stream.get(url, { responseType: 'json' })
       await prepareStream(stream)
       return { stream }
@@ -104,7 +106,7 @@ class Facebook extends Provider {
 
   async size ({ id, token }) {
     return this.#withErrorHandling('provider.facebook.size.error', async () => {
-      const url = await this.getMediaUrl({ token, id })
+      const url = await getMediaUrl({ secret: this.secret, token, id })
       const { size } = await getURLMeta(url)
       return size
     })
@@ -112,7 +114,8 @@ class Facebook extends Provider {
 
   async logout ({ token }) {
     return this.#withErrorHandling('provider.facebook.logout.error', async () => {
-      await this.runRequestBatch({
+      await runRequestBatch({
+        secret: this.secret,
         token,
         requests: [
           { method: 'DELETE', relative_url: 'me/permissions' },
