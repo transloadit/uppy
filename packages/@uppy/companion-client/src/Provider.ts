d8e2c04320b80ada7d3203df433f9e871cd3b95a
Antoine du Hamel <antoine@transloadit.com>
2024-07-01 15:06:55 +0200

    Update packages/@uppy/companion-client/src/Provider.ts

--- packages/@uppy/companion-client/src/Provider.ts
+++ packages/@uppy/companion-client/src/Provider.ts
@@ -166,7 +166,7 @@ export default class Provider<M extends Meta, B extends Body>
   }): string {
     const params = new URLSearchParams({
       ...query,
-      // todo remove some time in the future, but be careful because old versions of Companion expect this (#5276)
+      // This is only used for Companion instances configured to accept multiple origins.
       state: btoa(JSON.stringify({ origin: getOrigin() })),
       ...this.authQuery({ authFormData }),
     })
