7ed1c825437aec7949a91baf535430356640fdd3
Mikael Finstad <finstaden@gmail.com>
2024-07-15 16:05:50 +0200

    improve logic

--- packages/@uppy/companion/test/__tests__/callback.js
+++ packages/@uppy/companion/test/__tests__/callback.js
@@ -46,7 +46,7 @@ describe('test authentication callback', () => {
       .get(`/dropbox/send-token?uppyAuthToken=${token}`)
       .expect(200)
       .expect((res) => {
-        expect(res.text).toMatch(`window.opener.postMessage({"token":"${token}"}, "http:\\u002F\\u002Flocalhost:3020")`)
+        expect(res.text).toMatch(`postMessage({"token":"${token}"}, "http:\\u002F\\u002Flocalhost:3020")`)
       })
   })
 })
