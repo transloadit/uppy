f2932192fe43e1c4ab5beeffd3fbb03177c68a71
Merlijn Vos <merlijn@soverin.net>
2025-01-13 11:13:54 +0100

    @uppy/unsplash: make utmSource optional (#5601)

--- packages/@uppy/unsplash/src/Unsplash.tsx
+++ packages/@uppy/unsplash/src/Unsplash.tsx
@@ -21,7 +21,7 @@ import locale from './locale.js'
 // @ts-ignore We don't want TS to generate types for the package.json
 import packageJson from '../package.json'
 
-export type UnsplashOptions = { utmSource: string } & CompanionPluginOptions
+export type UnsplashOptions = { utmSource?: string } & CompanionPluginOptions
 
 export default class Unsplash<M extends Meta, B extends Body>
   extends UIPlugin<UnsplashOptions, M, B, UnknownSearchProviderPluginState>
