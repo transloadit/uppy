27492bca8a269f89f35d4f527ab757f8bb55146b
Arnaud Flaesch <ArnaudFlaesch@users.noreply.github.com>
2025-04-09 12:22:11 +0200

    Support Angular 19 (#5709) (#5715)
    Co-authored-by: Murderlon <merlijn@soverin.net>

--- packages/@uppy/angular/projects/uppy/angular/src/lib/components/dashboard-modal/dashboard-modal.stories.ts
+++ packages/@uppy/angular/projects/uppy/angular/src/lib/components/dashboard-modal/dashboard-modal.stories.ts
@@ -1,12 +1,10 @@
 import { DashboardModalDemoComponent } from './dashboard-modal-demo.component';
 import { moduleMetadata } from '@storybook/angular';
-import { UppyAngularDashboardModalModule } from './dashboard-modal.module';
 
 export default {
   title: 'Dashboard',
   decorators: [
     moduleMetadata({
-      imports: [UppyAngularDashboardModalModule],
       declarations: [DashboardModalDemoComponent]
     }),
   ]
--- packages/@uppy/angular/projects/uppy/angular/src/lib/components/drag-drop/drag-drop.stories.ts
+++ packages/@uppy/angular/projects/uppy/angular/src/lib/components/drag-drop/drag-drop.stories.ts
@@ -1,17 +1,15 @@
 import { moduleMetadata } from '@storybook/angular';
-import { UppyAngularDragDropModule } from './drag-drop.module';
 import { DragDropDemoComponent } from './drag-drop-demo.component';
 
 export default {
     title: 'Drag Drop',
     decorators: [
       moduleMetadata({
-        imports: [UppyAngularDragDropModule],
         declarations: [DragDropDemoComponent]
       }),
     ]
   };
-  
+
   export const Default = () => ({
     component: DragDropDemoComponent,
   });
