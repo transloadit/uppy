6bd6dd6ad0e7c063ea11db791f6077e696385fa9
Murderlon <merlijn@soverin.net>
2025-05-08 13:29:54 +0200

    Rename component

--- examples/react/src/App.tsx
+++ examples/react/src/App.tsx
@@ -9,21 +9,69 @@ import {
 } from '@uppy/react'
 import Uppy from '@uppy/core'
 import Tus from '@uppy/tus'
+import UppyWebcam from '@uppy/webcam'
 
 import './app.css'
 import '@uppy/react/dist/styles.css'
 
+function Webcam() {
+  const {
+    getVideoProps,
+    getSnapshotButtonProps,
+    getRecordButtonProps,
+    getStopRecordingButtonProps,
+    getSubmitButtonProps,
+    getDiscardButtonProps,
+  } = useWebcam()
+
+  const ref = useRef<HTMLVideoElement>(null)
+
+  return (
+    <div className="">
+      <video
+        className="border-2 w-full"
+        {...getVideoProps(ref.current)}
+        ref={ref}
+      />
+      <div className="flex gap-4">
+        <button className="disabled:opacity-50" {...getSnapshotButtonProps()}>
+          Snapshot
+        </button>
+        <button className="disabled:opacity-50" {...getRecordButtonProps()}>
+          Record
+        </button>
+        <button
+          className="disabled:opacity-50"
+          {...getStopRecordingButtonProps()}
+        >
+          Stop
+        </button>
+        <button className="disabled:opacity-50" {...getSubmitButtonProps()}>
+          Submit
+        </button>
+        <button className="disabled:opacity-50" {...getDiscardButtonProps()}>
+          Discard
+        </button>
+      </div>
+    </div>
+  )
+}
+
 function App() {
   const [uppy] = useState(() =>
-    new Uppy().use(Tus, {
-      endpoint: 'https://tusd.tusdemo.net/files/',
-    }),
+    new Uppy()
+      .use(Tus, {
+        endpoint: 'https://tusd.tusdemo.net/files/',
+      })
+      .use(UppyWebcam),
   )
 
   return (
     <UppyContextProvider uppy={uppy}>
       <main className="p-5 max-w-xl mx-auto">
         <h1 className="text-4xl font-bold">Welcome to React.</h1>
+
+        <Webcam />
 
         <article>
           <h2 className="text-2xl my-4">With list</h2>
