5fd37a8c1b761acf7b26c655c3738bafc2734e23
Murderlon <merlijn@soverin.net>
2025-05-26 11:02:51 +0200

    Fix Vue useDropzone

--- packages/@uppy/vue/src/useDropzone.ts
+++ packages/@uppy/vue/src/useDropzone.ts
@@ -1,22 +1,59 @@
 import {
   createDropzone,
   type DropzoneOptions,
-  type DropzoneReturn,
   type NonNullableUppyContext,
 } from '@uppy/components'
 import { useUppyContext } from './headless/useUppyContext.js'
 
-export function useDropzone(
-  options?: DropzoneOptions,
-): DropzoneReturn<DragEvent, Event> {
+export type VueDropzoneReturn = {
+  getRootProps: () => {
+    ondragenter: (event: DragEvent) => void
+    ondragover: (event: DragEvent) => void
+    ondragleave: (event: DragEvent) => void
+    ondrop: (event: DragEvent) => void
+    onclick: () => void
+  }
+  getInputProps: () => {
+    id: string
+    type: 'file'
+    multiple: boolean
+    onchange: (event: Event) => void
+  }
+}
+
+export function useDropzone(options?: DropzoneOptions): VueDropzoneReturn {
   const ctx = useUppyContext()
 
   if (!ctx?.uppy) {
     throw new Error('useDropzone must be called within a UppyContextProvider')
   }
 
-  return createDropzone(
+  const dropzone = createDropzone(
     ctx as NonNullableUppyContext, // covered by the if statement above
     options,
   )
+
+  return {
+    // Vue.js uses lowercase event names when using v-bind so we need to remap them
+    ...dropzone,
+    getRootProps: () => {
+      const { onDragEnter, onDragOver, onDragLeave, onDrop, onClick, ...rest } =
+        dropzone.getRootProps()
+      return {
+        ...rest,
+        ondragenter: onDragEnter,
+        ondragover: onDragOver,
+        ondragleave: onDragLeave,
+        ondrop: onDrop,
+        onclick: onClick,
+      }
+    },
+    getInputProps: () => {
+      const { onChange, ...rest } = dropzone.getInputProps()
+      return {
+        ...rest,
+        onchange: onChange,
+      }
+    },
+  }
 }
--- packages/@uppy/vue/src/useFileInput.ts
+++ packages/@uppy/vue/src/useFileInput.ts
@@ -1,7 +1,6 @@
 import { inject } from 'vue'
 import {
   type FileInputProps,
-  type FileInputFunctions,
   type NonNullableUppyContext,
 } from '@uppy/components'
 import { createFileInput } from '@uppy/components'
@@ -10,17 +9,48 @@ import {
   type UppyContext,
 } from './headless/context-provider.js'
 
-export function useFileInput(
-  props?: FileInputProps,
-): FileInputFunctions<Event> {
+export type VueFileInputFunctions = {
+  getInputProps: () => {
+    id: string
+    type: 'file'
+    multiple: boolean
+    accept?: string
+    onchange: (event: Event) => void
+  }
+  getButtonProps: () => {
+    type: 'button'
+    onclick: () => void
+  }
+}
+
+export function useFileInput(props?: FileInputProps): VueFileInputFunctions {
   const ctx = inject<UppyContext>(UppyContextSymbol)
 
   if (!ctx?.uppy) {
     throw new Error('useFileInput must be called within a UppyContextProvider')
   }
 
-  return createFileInput<Event>(
+  const fileinput = createFileInput<Event>(
     ctx as NonNullableUppyContext, // covered by the if statement above
     props,
   )
+
+  return {
+    // Vue.js uses lowercase event names when using v-bind so we need to remap them
+    ...fileinput,
+    getButtonProps: () => {
+      const { onClick, ...rest } = fileinput.getButtonProps()
+      return {
+        ...rest,
+        onclick: onClick,
+      }
+    },
+    getInputProps: () => {
+      const { onChange, ...rest } = fileinput.getInputProps()
+      return {
+        ...rest,
+        onchange: onChange,
+      }
+    },
+  }
 }
