231a291a3a9c52ce921739ee7ca9c0fe03b878c1
Prakash <qxprakash@gmail.com>
2025-05-28 15:53:41 +0530

    more checks

--- packages/@uppy/core/src/Uppy.ts
+++ packages/@uppy/core/src/Uppy.ts
@@ -927,7 +927,9 @@ export class Uppy<
       this.emit('restriction-failed', file, error)
       return false
     }
-    this.setFileState(file.id, { missingRequiredMetaFields: [] })
+    if (missingFields.length === 0 && file.missingRequiredMetaFields) {
+      this.setFileState(file.id, { missingRequiredMetaFields: [] })
+    }
     return true
   }
 
