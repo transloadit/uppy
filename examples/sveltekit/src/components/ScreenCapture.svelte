84ff344c0bb1fa8be374640b50bc02bcac7daff1
Prakash <qxprakash@gmail.com>
2025-06-16 12:35:08 +0530

    style fixes and added deps

--- examples/sveltekit/src/components/ScreenCapture.svelte
+++ examples/sveltekit/src/components/ScreenCapture.svelte
@@ -36,7 +36,7 @@
     class="border-2 w-full rounded-lg"
     {...screenCaptureStore.getMediaProps()}
   ></video>
-  <div class="flex gap-4 mt-4">
+  <div class="flex flex-wrap gap-2.5 mt-4">
     <button
       class="bg-blue-500 text-white px-4 py-2 rounded-md disabled:opacity-50 disabled:bg-blue-300"
       {...screenCaptureStore.getScreenshotButtonProps()}
