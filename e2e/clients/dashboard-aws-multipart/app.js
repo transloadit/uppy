81c111ddec48ab1a3860d6088c7a39283f4b82db
Murderlon <merlijn@soverin.net>
2025-06-25 14:50:43 +0200

    Fix import in e2e tests

--- e2e/clients/dashboard-aws-multipart/app.js
+++ e2e/clients/dashboard-aws-multipart/app.js
@@ -1,6 +1,6 @@
 import { Uppy } from '@uppy/core'
 import Dashboard from '@uppy/dashboard'
-import AwsS3Multipart from '@uppy/aws-s3-multipart'
+import AwsS3Multipart from '@uppy/aws-s3'
 
 import '@uppy/core/dist/style.css'
 import '@uppy/dashboard/dist/style.css'
