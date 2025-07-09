c7b0d645ce2a8b4bc5a15f8a632db77622214fde
Murderlon <merlijn@soverin.net>
2025-07-09 17:32:48 +0200

    Undo

--- examples/react/test/index.test.tsx
+++
@@ -1,9 +0,0 @@
-import { expect, test } from 'vitest'
-import { render } from 'vitest-browser-react'
-import App from '../src/App'
-
-test('loads and displays greeting', async () => {
-  const screen = render(<App />)
-  const heading = screen.getByText('Welcome to React.')
-  await expect.element(heading).toHaveTextContent('Welcome to React.')
-})
