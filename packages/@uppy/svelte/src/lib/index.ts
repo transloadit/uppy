3bfd22cc1db01012b3a17845236b2d6fd9740492
Prakash <qxprakash@gmail.com>
2025-07-13 02:33:38 +0530

    implemented and tested export maps with all three frameworks

--- packages/@uppy/svelte/src/lib/index.ts
+++ packages/@uppy/svelte/src/lib/index.ts
@@ -1,11 +1,6 @@
-export { default as Dashboard } from "./components/Dashboard.svelte";
-export { default as DashboardModal } from "./components/DashboardModal.svelte";
-export { default as DragDrop } from "./components/DragDrop.svelte";
 export * from "./components/headless/generated/index.js";
 // Headless components
 export { default as UppyContextProvider } from "./components/headless/UppyContextProvider.svelte";
-export { default as ProgressBar } from "./components/ProgressBar.svelte";
-export { default as StatusBar } from "./components/StatusBar.svelte";
 
 // Hooks
 export * from "./useDropzone.js";
