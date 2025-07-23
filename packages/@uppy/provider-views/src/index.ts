c8297b21b5d3c7a74da04dab8ab552c25cac7f12
Prakash <qxprakash@gmail.com>
2025-07-23 13:38:26 +0530

    apply feedback uppy/provider-views/package.json

--- packages/@uppy/provider-views/src/index.ts
+++ packages/@uppy/provider-views/src/index.ts
@@ -6,3 +6,7 @@ export {
 
 export { default as SearchInput } from './SearchInput.js'
 export { default as SearchProviderViews } from './SearchProviderView/index.js'
+
+// Export Google Picker utilities and icons for clean API
+export { GoogleDriveIcon, GooglePhotosIcon } from './GooglePicker/icons.js'
+export type { PickedItem } from './GooglePicker/googlePicker.js'
