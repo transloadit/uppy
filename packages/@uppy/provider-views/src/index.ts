2d48b97b286bf840e4be65e057b98949012d0a27
Prakash <qxprakash@gmail.com>
2025-07-23 13:41:23 +0530

    lint fix

--- packages/@uppy/provider-views/src/index.ts
+++ packages/@uppy/provider-views/src/index.ts
@@ -1,12 +1,10 @@
 export { default as GooglePickerView } from './GooglePicker/GooglePickerView.js'
+export type { PickedItem } from './GooglePicker/googlePicker.js'
+// Export Google Picker utilities and icons for clean API
+export { GoogleDriveIcon, GooglePhotosIcon } from './GooglePicker/icons.js'
 export {
   default as ProviderViews,
   defaultPickerIcon,
 } from './ProviderView/index.js'
-
 export { default as SearchInput } from './SearchInput.js'
 export { default as SearchProviderViews } from './SearchProviderView/index.js'
-
-// Export Google Picker utilities and icons for clean API
-export { GoogleDriveIcon, GooglePhotosIcon } from './GooglePicker/icons.js'
-export type { PickedItem } from './GooglePicker/googlePicker.js'
