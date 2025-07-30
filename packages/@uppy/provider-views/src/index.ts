62f48c726a64268b5f1591c0f6d044d74210d68f
Prakash <qxprakash@gmail.com>
2025-07-30 21:37:21 +0530

    add missing exports in provider-views

--- packages/@uppy/provider-views/src/index.ts
+++ packages/@uppy/provider-views/src/index.ts
@@ -1,6 +1,24 @@
 export { default as GooglePickerView } from './GooglePicker/GooglePickerView.js'
-export type { PickedItem } from './GooglePicker/googlePicker.js'
-// Export Google Picker utilities and icons for clean API
+export type {
+  MediaItem,
+  MediaItemBase,
+  PhotoMediaItem,
+  PickedDriveItem,
+  PickedItem,
+  PickedItemBase,
+  PickedPhotosItem,
+  PickingSession,
+  UnspecifiedMediaItem,
+  VideoMediaItem,
+} from './GooglePicker/googlePicker.js'
+export {
+  authorize,
+  ensureScriptsInjected,
+  logout,
+  pollPickingSession,
+  showDrivePicker,
+  showPhotosPicker,
+} from './GooglePicker/googlePicker.js'
 export { GoogleDriveIcon, GooglePhotosIcon } from './GooglePicker/icons.js'
 export {
   default as ProviderViews,
