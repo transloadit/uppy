2b3c783df0d2aab4c7fe82077fa312f475a5ff20
Murderlon <merlijn@soverin.net>
2025-08-05 12:47:18 +0200

    format

--- packages/@uppy/status-bar/src/StatusBarOptions.ts
+++ packages/@uppy/status-bar/src/StatusBarOptions.ts
@@ -1,6 +1,6 @@
 import type { UIPluginOptions } from '@uppy/core/lib/UIPlugin.js'
-import type StatusBarLocale from './locale.js'
 import type { LocaleStrings } from '@uppy/utils/lib/Translator'
+import type StatusBarLocale from './locale.js'
 
 export interface StatusBarOptions extends UIPluginOptions {
   showProgressDetails?: boolean
