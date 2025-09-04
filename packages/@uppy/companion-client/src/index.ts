95b1471a28b3cec621410ee1eda0f7c353884c47
Prakash <qxprakash@gmail.com>
2025-09-04 16:33:12 +0530

    lint fix

--- packages/@uppy/companion-client/src/index.ts
+++ packages/@uppy/companion-client/src/index.ts
@@ -2,11 +2,10 @@
  * Manages communications with Companion
  */
 
+export { default as AuthError } from './AuthError.js'
 export type { CompanionPluginOptions } from './CompanionPluginOptions.js'
 export { default as getAllowedHosts } from './getAllowedHosts.js'
 export { default as Provider } from './Provider.js'
 export { default as RequestClient } from './RequestClient.js'
 export { default as SearchProvider } from './SearchProvider.js'
 export * as tokenStorage from './tokenStorage.js'
-
-export { default as AuthError } from './AuthError.js'
