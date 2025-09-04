f997d8b401224ca17661d7553c800366412ad73d
Prakash <qxprakash@gmail.com>
2025-09-04 22:29:21 +0530

    Revert "add missing exports" (#5951)
    Reverts transloadit/uppy#5944

--- packages/@uppy/core/src/index.ts
+++ packages/@uppy/core/src/index.ts
@@ -10,7 +10,6 @@ export { default as BasePlugin } from './BasePlugin.js'
 export { default as EventManager } from './EventManager.js'
 export { debugLogger } from './loggers.js'
 export type { Restrictions, ValidateableFile } from './Restricter.js'
-export { RestrictionError } from './Restricter.js'
 export type { PluginTarget, UIPluginOptions } from './UIPlugin.js'
 export { default as UIPlugin } from './UIPlugin.js'
 export type {
