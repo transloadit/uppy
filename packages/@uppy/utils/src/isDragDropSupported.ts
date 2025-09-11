343cfdd6af6907ba5810af98a07ca034ec683976
Mikael Finstad <finstaden@gmail.com>
2025-09-11 23:00:42 +0200

    Enhance drag and drop support check

--- packages/@uppy/utils/src/isDragDropSupported.ts
+++ packages/@uppy/utils/src/isDragDropSupported.ts
@@ -2,10 +2,14 @@
  * Checks if the browser supports Drag & Drop (not supported on mobile devices, for example).
  */
 export default function isDragDropSupported(): boolean {
+  if (typeof window === 'undefined') {
+    return false
+  }
+
   const body = document.body
 
   // sometimes happens in the wild: https://github.com/transloadit/uppy/issues/5953
-  if (body == null || typeof window === 'undefined' || window == null) {
+  if (body == null || window == null) {
     return false
   }
 
