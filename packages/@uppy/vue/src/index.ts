682e9a1c3a4061978b5dfee001c97ed7179f5115
Mikael Finstad <finstaden@gmail.com>
2025-09-12 23:45:08 +0200

    Reorder exports in index.ts for consistency

--- packages/@uppy/vue/src/index.ts
+++ packages/@uppy/vue/src/index.ts
@@ -1,5 +1,8 @@
-export { UppyContextProvider, UppyContextSymbol } from './headless/context-provider.js'
 export type { UppyContext } from './headless/context-provider.js'
+export {
+  UppyContextProvider,
+  UppyContextSymbol,
+} from './headless/context-provider.js'
 export * from './headless/generated/index.js'
 
 export * from './useDropzone.js'
