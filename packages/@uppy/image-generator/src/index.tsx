cd9ba5d19cb2e56053cff88bfc4d88218a0261e0
Murderlon <merlijn@soverin.net>
2025-09-16 15:51:40 +0200

    WIP

--- packages/@uppy/image-generator/src/index.tsx
+++ packages/@uppy/image-generator/src/index.tsx
@@ -1,7 +1,6 @@
 import type { Body, Meta, UIPluginOptions, Uppy } from '@uppy/core'
 import { UIPlugin } from '@uppy/core'
 import { SearchInput } from '@uppy/provider-views'
-// eslint-disable-next-line import/no-extraneous-dependencies
 import type Transloadit from '@uppy/transloadit'
 import type { AssemblyResult } from '@uppy/transloadit'
 import locale from './locale.js'
@@ -69,7 +68,7 @@ export default class ImageGenerator<
       this.setPluginState({ loading: true })
       await this.uppy.upload()
     } catch {
-      // TODO: inform the user
+      // TODO: inform the user?
     } finally {
       // Normally uppy.upload() is only used to upload your files
       // but here we need it to _get_ the results from the AI image generator Robot.
@@ -120,7 +119,9 @@ export default class ImageGenerator<
             <input
               type="text"
               id="uppy-image-generator-prompt"
+              className="uppy-c-textInput"
               value={prompt}
+              data-uppy-super-focusable
               onInput={(e) =>
                 this.setPluginState({
                   prompt: (e.target as HTMLInputElement).value,
@@ -128,6 +129,26 @@ export default class ImageGenerator<
               }
             />
             <label for="uppy-image-generator-prompt">Prompt</label>
+            <button
+              type="button"
+              className="uppy-u-reset"
+              data-uppy-super-focusable
+            >
+              <span className="sr-only">Regenerate</span>
+              <svg
+                aria-hidden
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                class="uppy-c-icon"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M4.755 10.059a7.5 7.5 0 0 1 12.548-3.364l1.903 1.903h-3.183a.75.75 0 1 0 0 1.5h4.992a.75.75 0 0 0 .75-.75V4.356a.75.75 0 0 0-1.5 0v3.18l-1.9-1.9A9 9 0 0 0 3.306 9.67a.75.75 0 1 0 1.45.388Zm15.408 3.352a.75.75 0 0 0-.919.53 7.5 7.5 0 0 1-12.548 3.364l-1.902-1.903h3.183a.75.75 0 0 0 0-1.5H2.984a.75.75 0 0 0-.75.75v4.992a.75.75 0 0 0 1.5 0v-3.18l1.9 1.9a9 9 0 0 0 15.059-4.035.75.75 0 0 0-.53-.918Z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </button>
           </div>
 
           <ul className="uppy-ImageGenerator-grid">
