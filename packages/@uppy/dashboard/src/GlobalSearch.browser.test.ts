0faad6f2e1170b1ed2b8586537b9d780a78fe1c2
Prakash <qxprakash@gmail.com>
2025-11-04 23:30:14 +0530

    lint fix

--- packages/@uppy/dashboard/src/GlobalSearch.browser.test.ts
+++ packages/@uppy/dashboard/src/GlobalSearch.browser.test.ts
@@ -315,8 +315,8 @@ describe('ProviderView Search E2E', () => {
     await clearSearchButton.click()
 
     await expect
-    .element(page.getByRole('button', { name: 'first' }))
-    .toBeVisible()
+      .element(page.getByRole('button', { name: 'first' }))
+      .toBeVisible()
 
     const firstFolderItem = Array.from(
       document.querySelectorAll('.uppy-ProviderBrowserItem'),
@@ -324,7 +324,7 @@ describe('ProviderView Search E2E', () => {
       (item) =>
         item.textContent?.includes('first') && item.querySelector('button'),
     )
-    console.log("logging first folder item inside test -->", firstFolderItem);
+    console.log('logging first folder item inside test -->', firstFolderItem)
     expect(firstFolderItem).toBeTruthy()
 
     expect(
