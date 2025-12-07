ef2b46584feb1c289c5e496214361b809f077a7d
Prakash <qxprakash@gmail.com>
2025-12-08 01:44:03 +0530

    fix imports in examples

--- examples/xhr-bundle/main.js
+++ examples/xhr-bundle/main.js
@@ -2,8 +2,8 @@ import Uppy from '@uppy/core'
 import Dashboard from '@uppy/dashboard'
 import XHRUpload from '@uppy/xhr-upload'
 
-import '@uppy/core/dist/style.css'
-import '@uppy/dashboard/dist/style.css'
+import '@uppy/core/css/style.css'
+import '@uppy/dashboard/css/style.css'
 
 const uppy = new Uppy({
   debug: true,
--- examples/xhr-node/main.js
+++ examples/xhr-node/main.js
@@ -4,7 +4,7 @@ import Webcam from '@uppy/webcam'
 import XHRUpload from '@uppy/xhr-upload'
 
 import '@uppy/core/dist/style.css'
-import '@uppy/dashboard/dist/style.css'
+import '@uppy/dashboard/css/style.css'
 import '@uppy/webcam/dist/style.css'
 
 const uppy = new Uppy({
