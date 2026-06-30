6ebe5603a3b850342df3eca1f2377e29c6099b3c
prakash <qxprakash@gmail.com>
2026-06-30 16:32:41 +0530

    remove dangling import

--- examples/companion-custom-provider/client/MyCustomProvider.jsx
+++ examples/companion-custom-provider/client/MyCustomProvider.jsx
@@ -1,6 +1,6 @@
 /** @jsx h */
 
-import { getAllowedHosts, Provider, tokenStorage } from '@uppy/companion-client'
+import { getAllowedHosts, Provider, tokenStorage } from '@uppy/core/companion-client'
 import { UIPlugin } from '@uppy/core'
 import { ProviderViews } from '@uppy/core/provider-views'
 
