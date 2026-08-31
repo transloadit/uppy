43c8f7646ebb2915f86e9d412f8996b9bd3d8232
Remco Haszing <remcohaszing@gmail.com>
2026-08-31 11:03:55 +0200

    Make TypeScript setup more consistent
    This changes the TypeScript setup so it’s consistent across all
    packages. Example are excluded.
    
    This introduces a new private package named `@uppy-dev/tsconfig`. This
    contains two tsconfig presets.
    
    Every package now has 2 tsconfig files. `tsconfig.build.json` extends
    `@uppy-dev/tsconfig/build`. This packages typechecks and builds the
    `src` folder of each package into `lib`. `tsconfig.json` typechecks
    everything except `src`, and references the `tsconfig.build.json` file.
    
    Files that should not be published, such as tests, are moved into a
    separate `test` directory. This was already the situation sometimes, but
    not always.
    
    The `tsconfig.json` in the project root is a solution file. Running
    `tsc --build` from the project root, builds and checks all TypeScript
    projects. Likewise, each project can be built or type checked
    individually.
    
    Tests have been added to `@uppy-dev/tsconfig` to make sure this setup
    stays consistent.
    
    The packages `@uppy/angular` and `@uppy/svelte` have been excluded,
    because they need a special build step.

---
+++ packages/@uppy/compressor/test/index.test.ts
@@ -0,0 +1,70 @@
+import CompressorPlugin from '@uppy/compressor'
+import Core from '@uppy/core'
+import { getFileNameAndExtension } from '@uppy/core/utils'
+import { describe, expect, it } from 'vitest'
+import sampleImage from '../fixtures/image.jpg'
+
+async function getSampleImage(name: string): Promise<File> {
+  const response = await fetch(sampleImage)
+  const blob = await response.blob()
+  return new File([blob], name, { type: blob.type })
+}
+
+// Compressor uses browser canvas API, so need to mock compress()
+// @ts-expect-error mocked
+CompressorPlugin.prototype.compress = async (blob: File) => {
+  return {
+    name: `${getFileNameAndExtension(blob.name).name}.webp`,
+    type: 'image/webp',
+    data: blob,
+    size: 123,
+  }
+}
+
+describe('CompressorPlugin', () => {
+  it('should change update extension in file.name and file.meta.name', async () => {
+    const uppy = new Core()
+    uppy.use(CompressorPlugin, {
+      quality: 0.85,
+      mimeType: 'image/webp',
+    })
+
+    uppy.addFile({
+      source: 'test',
+      name: 'image-1.jpeg',
+      type: 'image/jpeg',
+      data: await getSampleImage('image-1.jpeg'),
+    })
+    uppy.addFile({
+      source: 'test',
+      name: 'yolo',
+      type: 'image/jpeg',
+      data: await getSampleImage('yolo'),
+    })
+    uppy.addFile({
+      source: 'test',
+      name: 'my.file.is.weird.png',
+      type: 'image/png',
+      data: await getSampleImage('my.file.is.weird.png'),
+    })
+
+    // User changed file.meta.name
+    uppy.setFileMeta(uppy.getFiles()[0].id, { name: 'new-name.jpeg' })
+
+    return uppy.upload().then(() => {
+      const files = uppy.getFiles()
+
+      expect(files[0].meta.name).toEqual('new-name.webp')
+      expect(files[0].name).toEqual('image-1.webp')
+      expect(files[0].meta.type).toEqual('image/webp')
+
+      expect(files[1].meta.name).toEqual('yolo.webp')
+      expect(files[1].meta.type).toEqual('image/webp')
+      expect(files[1].name).toEqual('yolo.webp')
+
+      expect(files[2].meta.name).toEqual('my.file.is.weird.webp')
+      expect(files[2].meta.type).toEqual('image/webp')
+      expect(files[2].name).toEqual('my.file.is.weird.webp')
+    })
+  })
+})
---
+++ private/tsconfig/test/tsconfig.test.ts
@@ -0,0 +1,60 @@
+import { readdir, readFile } from 'node:fs/promises'
+import { expect, test } from 'vitest'
+
+const ignore = ['angular', 'svelte']
+
+const projectRoot = new URL('../../../', import.meta.url)
+
+const packages = (await readdir(new URL('packages/@uppy/', projectRoot)))
+  .filter((name) => !ignore.includes(name))
+  .sort()
+
+async function readJSON(path: string): Promise<any> {
+  return JSON.parse(await readFile(new URL(path, projectRoot), 'utf8'))
+}
+
+test('tsconfig.json', async () => {
+  const tsconfig = await readJSON('tsconfig.json')
+  expect(tsconfig).toStrictEqual({
+    files: [],
+    references: packages.map((name) => ({
+      path: `./packages/@uppy/${name}/tsconfig.json`,
+    })),
+  })
+})
+
+test.each(packages)('packages/@uppy/%s/tsconfig.build.json', async (name) => {
+  const pkg = await readJSON(`packages/@uppy/${name}/package.json`)
+  const tsconfigBuild = await readJSON(
+    `packages/@uppy/${name}/tsconfig.build.json`,
+  )
+
+  let expected: unknown[] | undefined
+  if (pkg.dependencies || pkg.devDependencies || pkg.peerDependencies) {
+    const deps = Object.keys({
+      ...pkg.dependencies,
+      ...pkg.devDependencies,
+      ...pkg.peerDependencies,
+    })
+      .filter((dep) => dep.startsWith('@uppy'))
+      .sort()
+
+    if (deps.length) {
+      expected = deps.map((dep) => ({
+        path: `../${dep.replace(/^@uppy\//, '')}/tsconfig.build.json`,
+      }))
+    }
+  }
+
+  expect(tsconfigBuild.extends).toBe('@uppy-dev/tsconfig/build')
+  expect(tsconfigBuild.references).toStrictEqual(expected)
+})
+
+test.each(packages)('packages/@uppy/%s/tsconfig.json', async (name) => {
+  const tsconfigBuild = await readJSON(`packages/@uppy/${name}/tsconfig.json`)
+
+  expect(tsconfigBuild.extends).toBe('@uppy-dev/tsconfig')
+  expect(tsconfigBuild.references).toStrictEqual([
+    { path: './tsconfig.build.json' },
+  ])
+})
