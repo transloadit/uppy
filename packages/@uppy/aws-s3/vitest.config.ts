80a50e4f18d375cc79938e78ac122cbd70d0b4a1
Remco Haszing <remcohaszing@gmail.com>
2026-09-07 16:06:51 +0200

    Restore aws-s3 globalSetup script

--- packages/@uppy/aws-s3/vitest.config.ts
+++ packages/@uppy/aws-s3/vitest.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     testTimeout: 120_000,
+    globalSetup: ['test/s3-client/setup.ts'],
     browser: {
       enabled: true,
       headless: true,
