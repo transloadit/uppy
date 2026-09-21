4dd92cf9ea16e7102509a5c8293f08a287fc7f39
Remco Haszing <remcohaszing@gmail.com>
2026-09-21 12:32:31 +0200

    Use a JSDoc comment to describe stripTrailingSlash

--- packages/@uppy/core/src/utils/stripTrailingSlash.ts
+++ packages/@uppy/core/src/utils/stripTrailingSlash.ts
@@ -1,4 +1,6 @@
-// Remove trailing slashes so we can always safely append /xyz.
+/**
+ * Remove trailing slashes so we can always safely append /xyz.
+ */
 export default function stripTrailingSlash(url: string): string {
   return url.replace(/\/+$/, '')
 }
