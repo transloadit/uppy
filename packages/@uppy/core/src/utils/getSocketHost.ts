1a020fc6ef1d7fcc2921a23d94946af5f659edc0
Mikael Finstad <finstaden@gmail.com>
2026-09-24 17:56:48 +0200

    Refactor getSocketHost to simplify host extraction

--- packages/@uppy/core/src/utils/getSocketHost.ts
+++ packages/@uppy/core/src/utils/getSocketHost.ts
@@ -3,8 +3,7 @@ import stripTrailingSlash from './stripTrailingSlash.js'
 export default function getSocketHost(url: string): string {
   // get the host domain
   const regex = /^(?:https?:\/\/|\/\/)?(?:[^@\n]+@)?([^\n]+)/i
-  const match = regex.exec(url)?.[1]
-  const host = match == null ? match : stripTrailingSlash(match)
+  const host = regex.exec(stripTrailingSlash(url))?.[1]
   const socketProtocol = /^http:\/\//i.test(url) ? 'ws' : 'wss'
 
   return `${socketProtocol}://${host}`
